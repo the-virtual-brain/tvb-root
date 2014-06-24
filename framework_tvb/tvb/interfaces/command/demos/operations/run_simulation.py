2977aaccca5623efea056b43e808d84fe40e51f6
liadomide <lia_domide@yahoo.com>
2014-06-24 11:51:00 +0000

    TVB-1471 Add a simple set of instructions for running simulations on the cluster from the command profile
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@6280 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- framework_tvb/tvb/interfaces/command/demos/operations/run_simulation.py
+++ framework_tvb/tvb/interfaces/command/demos/operations/run_simulation.py
@@ -35,6 +35,7 @@ Find a TS in current project (by Subject) and later run an analyzer on it
 """
 
 import tvb.interfaces.command.prepare
+import sys
 from time import sleep
 from tvb.basic.logger.builder import get_logger
 from tvb.core.adapters.abcadapter import ABCAdapter
@@ -71,6 +72,9 @@ if __name__ == "__main__":
         launch_args[f["name"]] = str(f["default"]) if 'default' in f else None
     launch_args["connectivity"] = connectivity.gid
     launch_args["model_parameters_option_Generic2dOscillator_variables_of_interest"] = 'V'
+
+    if len(sys.argv) > 1:
+        launch_args["model_parameters_option_Generic2dOscillator_tau"] = sys.argv[1]
 
     ## launch an operation and have the results stored both in DB and on disk
     launched_operation = flow_service.fire_operation(adapter_instance, project.administrator,
