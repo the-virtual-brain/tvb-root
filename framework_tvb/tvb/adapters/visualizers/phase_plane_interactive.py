42d3f0c7629244a50328d416b34e18c8dbc88da6
mihandrei <mihai.daniel.psmg@gmail.com>
2014-10-13 09:37:10 +0000

    TVB-1024 Removed deprecated mplh5 renderer. Use vectorized trajectory computation and remove scalar one.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@6702 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- framework_tvb/tvb/adapters/visualizers/phase_plane_interactive.py
+++ framework_tvb/tvb/adapters/visualizers/phase_plane_interactive.py
@@ -31,11 +31,7 @@
 .. moduleauthor:: Ionel Ortelecan <ionel.ortelecan@codemart.ro>
 .. moduleauthor:: Mihai Andrei <mihai.andrei@codemart.ro>
 """
-import threading
 import numpy
-import pylab
-import colorsys
-from tvb.basic.profile import TvbProfile
 from tvb.basic.logger.builder import get_logger
 from matplotlib import _cntr
 
@@ -111,32 +107,6 @@ class PhasePlane(object):
             self.log.error("NaN")
 
 
-    def _calc_phase_plane(self):
-        """ Calculate the vector field. """
-        svx_ind = self.model.state_variables.index(self.svx)
-        svy_ind = self.model.state_variables.index(self.svy)
-
-        #Calculate the vector field discretely sampled at a grid of points
-        grid_point = self.default_sv.copy()
-        self.U = numpy.zeros((NUMBEROFGRIDPOINTS, NUMBEROFGRIDPOINTS, self.model.number_of_modes))
-        self.V = numpy.zeros((NUMBEROFGRIDPOINTS, NUMBEROFGRIDPOINTS, self.model.number_of_modes))
-        for ii in xrange(NUMBEROFGRIDPOINTS):
-            grid_point[svy_ind] = self.Y[ii]
-            for jj in xrange(NUMBEROFGRIDPOINTS):
-                #import pdb; pdb.set_trace()
-                grid_point[svx_ind] = self.X[jj]
-                d = self.model.dfun(grid_point, self.no_coupling)
-
-                for kk in range(self.model.number_of_modes):
-                    self.U[ii, jj, kk] = d[svx_ind, 0, kk]
-                    self.V[ii, jj, kk] = d[svy_ind, 0, kk]
-
-        #self.UVmag = numpy.sqrt(self.U**2 + self.V**2)
-        #import pdb; pdb.set_trace()
-        if numpy.isnan(self.U).any() or numpy.isnan(self.V).any():
-            self.log.error("NaN")
-
-
     def get_axes_ranges(self, sv):
         lo, hi = self.model.state_variable_range[sv]
         default_range = hi - lo
@@ -180,186 +150,7 @@ class PhasePlane(object):
         if numpy.isnan(trajs).any():
             self.log.warn("NaN in trajectories")
 
-        return trajs[:, svx_ind, :, self.mode], trajs[:, svy_ind, :, self.mode]
-
-
-    def _compute_trajectory(self, x, y):
-        """
-        Calculate a sample trajectory, starting at the position x,y in the phase-plane.
-        """
-        svx_ind = self.model.state_variables.index(self.svx)
-        svy_ind = self.model.state_variables.index(self.svy)
-
-        state = self.default_sv.copy()
-        state[svx_ind] = x
-        state[svy_ind] = y
-        scheme = self.integrator.scheme
-        traj = numpy.zeros((TRAJ_STEPS + 1, self.model.nvar, 1, self.model.number_of_modes))
-        traj[0, :] = state
-
-        for step in xrange(TRAJ_STEPS):
-            state = scheme(state, self.model.dfun, self.no_coupling, 0.0, 0.0)
-            traj[step + 1, :] = state
-
-        if numpy.isnan(traj).any():
-            self.log.warn("NaN in trajectory")
-
-        return traj
-
-
-
-class PhasePlaneInteractive(PhasePlane):
-    """
-    An interactive phase-plane plot generated from a TVB model object.
-
-    A TVB integrator object will be use for generating sample trajectories
-    -- not the phase-plane. This is mainly interesting for visualising
-    the effect of noise on a trajectory.
-    """
-
-
-    def __init__(self, model, integrator):
-        PhasePlane.__init__(self, model, integrator)
-        self.ipp_fig = None
-        self.pp_splt = None
-        # Concurrent access to the drawing routines is breaking this viewer
-        # Concurrency happens despite the GIL because the drawing calls do socket io
-        # Lock must be re-entrant because refresh is used in event handling
-        self.lock = threading.RLock()
-
-
-    @staticmethod
-    def get_color(num_colours):
-        for hue in range(num_colours):
-            hue = 1.0 * hue / num_colours
-            col = [int(x) for x in colorsys.hsv_to_rgb(hue, 1.0, 230)]
-            yield "#{0:02x}{1:02x}{2:02x}".format(*col)
-
-
-    def reset(self, model, integrator):
-        """
-        Resets the state associated with the model and integrator.
-        Redraws plot.
-        """
-        with self.lock:
-            PhasePlane.__init__(self, model, integrator)
-            self.draw_phase_plane()
-
-
-    def refresh(self):
-        with self.lock:
-            self._set_mesh_grid()
-            self._calc_phase_plane()
-            self._update_phase_plane()
-
-
-    def draw_phase_plane(self):
-        """Generate the interactive phase-plane figure."""
-        with self.lock:
-            self.log.debug("Plot started...")
-
-            model_name = self.model.__class__.__name__
-            msg = "Generating an interactive phase-plane plot for %s"
-            self.log.info(msg % model_name)
-
-            self._set_state_vector()
-
-            #TODO: see how we can get the figure size from the UI to better 'fit' the encompassing div
-            if self.ipp_fig is None:
-                self.ipp_fig = pylab.figure(figsize=(10, 8))
-                # add mouse handler for trajectory clicking
-                self.ipp_fig.canvas.mpl_connect('button_press_event', self._click_trajectory)
-
-            pylab.clf()
-            self.pp_ax = self.ipp_fig.add_axes([0.07, 0.2, 0.9, 0.75])
-
-            self.pp_splt = self.ipp_fig.add_subplot(212)
-            self.ipp_fig.subplots_adjust(left=0.07, bottom=0.03, right=0.97, top=0.3, wspace=0.1, hspace=None)
-            self.pp_splt.set_color_cycle(self.get_color(self.model.nvar))
-            self.pp_splt.plot(numpy.arange(TRAJ_STEPS + 1) * self.integrator.dt,
-                              numpy.zeros((TRAJ_STEPS + 1, self.model.nvar)))
-            if hasattr(self.pp_splt, 'autoscale'):
-                self.pp_splt.autoscale(enable=True, axis='y', tight=True)
-            self.pp_splt.legend(self.model.state_variables)
-
-            #Calculate the phase plane
-            self._set_mesh_grid()
-            self._calc_phase_plane()
-
-            #Plot phase plane
-            self._plot_phase_plane()
-
-            self.ipp_fig.canvas.draw()
-
-            return dict(mplh5ServerURL=TvbProfile.current.web.MPLH5_SERVER_URL,
-                        figureNumber=self.ipp_fig.number, showFullToolbar=False)
-
-
-    ##------------------- Functions for updating the figure ------------------##
-
-    def _update_phase_plane(self):
-        """ Clear the axes and redraw the phase-plane. """
-        self.pp_ax.clear()
-        self.pp_splt.clear()
-        self.pp_splt.set_color_cycle(self.get_color(self.model.nvar))
-        self.pp_splt.plot(numpy.arange(TRAJ_STEPS + 1) * self.integrator.dt,
-                          numpy.zeros((TRAJ_STEPS + 1, self.model.nvar)))
-        if hasattr(self.pp_splt, 'autoscale'):
-            self.pp_splt.autoscale(enable=True, axis='y', tight=True)
-        self.pp_splt.legend(self.model.state_variables)
-        self._plot_phase_plane()
-
-
-    def _plot_phase_plane(self):
-        """ Plot the vector field and its nullclines. """
-        # Set title and axis labels
-        model_name = self.model.__class__.__name__
-        self.pp_ax.set(title=model_name + " mode " + str(self.mode))
-        self.pp_ax.set(xlabel="State Variable " + self.svx)
-        self.pp_ax.set(ylabel="State Variable " + self.svy)
-        #import pdb; pdb.set_trace()
-        #Plot a discrete representation of the vector field
-        if numpy.all(self.U[:, :, self.mode] + self.V[:, :, self.mode] == 0):
-            self.pp_ax.set(title=model_name + " mode " + str(self.mode) + ": NO MOTION IN THIS PLANE")
-            X, Y = numpy.meshgrid(self.X, self.Y)
-            self.pp_quivers = self.pp_ax.scatter(X, Y, s=8, marker=".", c="k")
-        else:
-            self.pp_quivers = self.pp_ax.quiver(self.X, self.Y,
-                                                self.U[:, :, self.mode],
-                                                self.V[:, :, self.mode],
-                                                #self.UVmag[:, :, self.mode],
-                                                width=0.001, headwidth=8)
-
-        #Plot the nullclines
-        self.nullcline_x = self.pp_ax.contour(self.X, self.Y, self.U[:, :, self.mode], [0], colors="r")
-        self.nullcline_y = self.pp_ax.contour(self.X, self.Y, self.V[:, :, self.mode], [0], colors="g")
-        self.ipp_fig.canvas.draw()
-
-
-    def _plot_trajectory(self, x, y):
-        """
-        Plot a sample trajectory, starting at the position x,y in the phase-plane.
-        """
-        svx_ind = self.model.state_variables.index(self.svx)
-        svy_ind = self.model.state_variables.index(self.svy)
-
-        traj = self._compute_trajectory(x, y)
-
-        self.pp_ax.scatter(x, y, s=42, c='g', marker='o', edgecolor=None)
-        self.pp_ax.plot(traj[:, svx_ind, 0, self.mode], traj[:, svy_ind, 0, self.mode])
-
-        #Plot the selected state variable trajectories as a function of time
-        self.pp_splt.plot(numpy.arange(TRAJ_STEPS + 1) * self.integrator.dt, traj[:, :, 0, self.mode])
-
-        pylab.draw()
-
-
-    def _click_trajectory(self, event):
-        with self.lock:
-            if event.inaxes is self.pp_ax:
-                x, y = event.xdata, event.ydata
-                self.log.info('trajectory starting at (%f, %f)', x, y)
-                self._plot_trajectory(x, y)
+        return trajs
 
 
 
@@ -411,7 +202,8 @@ class PhasePlaneD3(PhasePlane):
     def trajectory(self, x, y):
         svx_ind = self.model.state_variables.index(self.svx)
         svy_ind = self.model.state_variables.index(self.svy)
-        traj = self._compute_trajectory(x, y)
+
+        traj = self._compute_trajectories([x], [y])
 
         signal_x = numpy.arange(TRAJ_STEPS + 1) * self.integrator.dt
 
