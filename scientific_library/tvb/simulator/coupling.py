6ed2e9bba89f64c235187f390db914e09ab3297c
pausz <sanzleon.paula@gmail.com>
2014-11-09 18:33:05 +0000

    Coupling: correct mistake I introduced when changing the axis of the explicit summation in the coupling term. This implementation respects now the convention described in the docstring.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@6841 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- scientific_library/tvb/simulator/coupling.py
+++ scientific_library/tvb/simulator/coupling.py
@@ -237,8 +237,10 @@ class Linear(Coupling):
 
 
         """
-        coupled_input = (g_ij * x_j).sum(axis=2)
+        coupled_input = (g_ij.transpose((2, 1, 0, 3)) * x_j).sum(axis=0)
+
         return self.a * coupled_input + self.b
+
 
     device_info = coupling_device_info(
         pars = ['a', 'b'],
@@ -290,7 +292,7 @@ class Scaling(Coupling):
 
 
         """
-        coupled_input = (g_ij * x_j).sum(axis=2)
+        coupled_input = (g_ij.transpose((2, 1, 0, 3)) * x_j).sum(axis=2)
         return self.a * coupled_input
 
     device_info = coupling_device_info(
@@ -365,11 +367,11 @@ class HyperbolicTangent(Coupling):
             #NOTE: normalising by the strength or degrees may yield NaNs, so fill these values with inf
             in_strength = g_ij.sum(axis=2)[:, :, numpy.newaxis, :]
             in_strength[in_strength==0] = numpy.inf
-            temp *= (g_ij / in_strength) #region mode normalisation
+            temp *= (g_ij.transpose((2, 1, 0, 3)) / in_strength) #region mode normalisation
 
-            coupled_input = temp.mean(axis=2)
+            coupled_input = temp.mean(axis=0)
         else:
-            coupled_input = (g_ij*temp).mean(axis=2)
+            coupled_input = (g_ij.transpose((2, 1, 0, 3))*temp).mean(axis=0)
 
         return coupled_input
 
@@ -515,7 +517,7 @@ class SigmoidalJansenRit(Sigmoidal):
         diff_input = x_j[:, 0, numpy.newaxis, :, :] - x_j[:, 1, numpy.newaxis, :, :]        
         temp       = self.r * (self.midpoint - (diff_input))
         sigm_y1_y2 = numpy.where(temp > magic_exp_number, self.cmax / (1.0 + numpy.exp(temp)), self.cmax / (1.0 + numpy.exp(temp)))     
-        coupled_input = self.a * ( g_ij * sigm_y1_y2).sum(axis=2)
+        coupled_input = self.a * ( g_ij.transpose((2, 1, 0, 3)) * sigm_y1_y2).sum(axis=0)
 
         return coupled_input
 
@@ -625,7 +627,7 @@ class PreSigmoidal(Coupling):
         A_j = self.H * (self.Q + numpy.tanh(self.G * (self.P * x_j \
             - self.theta[self.sliceT,numpy.newaxis])))
         
-        return (g_ij * A_j).sum(axis=0)
+        return (g_ij.transpose((2, 1, 0, 3)) * A_j).sum(axis=0)
 
 
     def call_dynamic(self, g_ij, x_i, x_j):
@@ -636,7 +638,7 @@ class PreSigmoidal(Coupling):
         A_j = self.H * (self.Q + numpy.tanh(self.G * (self.P * x_j[:,0,:,:] \
             - x_j[:,1,self.sliceT,:])[:,numpy.newaxis,:,:]))
         
-        c_0 = (g_ij[:,0] * A_j[:,0]).sum(axis=2)
+        c_0 = (g_ij[:,0] * A_j[:,0]).sum(axis=0)
         c_1 = self.meanOrNot(A_j)
         return numpy.array([c_0, c_1])
 
@@ -662,7 +664,7 @@ class Difference(Coupling):
 
         """
 
-        return self.a*(g_ij*(x_j - x_i)).sum(axis=2)
+        return self.a*(g_ij.transpose((2, 1, 0, 3))*(x_j - x_i)).sum(axis=0)
 
     device_info = coupling_device_info(
         pars = ['a'],
@@ -704,7 +706,7 @@ class Kuramoto(Coupling):
         """
         number_of_regions = g_ij.shape[0]
 
-        return (self.a / number_of_regions)*(g_ij*sin(x_j-x_i)).sum(axis=2)
+        return (self.a / number_of_regions)*(g_ij.transpose((2, 1, 0, 3))*sin(x_j-x_i)).sum(axis=0)
 
     device_info = coupling_device_info(
         pars = ['a'],
