028f1b1c62cc81c000ee3b21414a9a53cdceac5e
pausz <sanzleon.paula@gmail.com>
2015-01-15 10:50:55 +0000

    TVB-1467: update test for models, enable comparison of initial condition arrays.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7017 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- scientific_library/tvb/tests/library/simulator/models_test.py
+++ scientific_library/tvb/tests/library/simulator/models_test.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
 #
-#  TheVirtualBrain-Scientific Package. This package holds all simulators, and 
+#  TheVirtualBrain-Scientific Package. This package holds all simulators, and
 # analysers necessary to run brain-simulations. You can use it stand alone or
 # in conjunction with TheVirtualBrain-Framework Package. See content of the
 # documentation-folder for more details. See also http://www.thevirtualbrain.org
 #
 # (c) 2012-2013, Baycrest Centre for Geriatric Care ("Baycrest")
 #
-# This program is free software; you can redistribute it and/or modify it under 
+# This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License version 2 as published by the Free
 # Software Foundation. This program is distributed in the hope that it will be
-# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
-# License for more details. You should have received a copy of the GNU General 
+# License for more details. You should have received a copy of the GNU General
 # Public License along with this program; if not, you can download it here
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0
 #
@@ -38,7 +38,7 @@ Test for tvb.simulator.models module
 if __name__ == "__main__":
     from tvb.tests.library import setup_test_console_env
     setup_test_console_env()
-    
+
 import unittest
 import numpy
 from numpy.testing import assert_array_almost_equal as ArrayAlmostEqual
@@ -47,9 +47,9 @@ from tvb.tests.library.base_testcase import BaseTestCase
 from tvb.simulator import models
 
 
-# NOTE: Comparing arrays of floating point numbers is, to say the least, 
+# NOTE: Comparing arrays of floating point numbers is, to say the least,
 #        akward and generally ill posed. Numpy provides a testing suite to compare
-#        arrays. 
+#        arrays.
 # TODO: use numpy.testing.ssert_array_almost_equal_nulp or assert_array_max_ulp
 # which are built for floating point comparisons.
 
@@ -57,15 +57,15 @@ dt = 2**-4
 
 class ModelsTest(BaseTestCase):
     """
-    Define test cases for coupling:
+    Define test cases for models:
         - initialise each class
-        - check default parameters 
-         (should correspond to those used in the original work with the aim to 
+        - check default parameters
+         (should correspond to those used in the original work with the aim to
          reproduce at least one figure)
         - check that initial conditions are always the same. Fails if seed != 42
           or sv ranges are changed or intial method is overwritten... mmm
-          
-          
+
+
     """
 
     def test_wilson_cowan(self):
@@ -76,16 +76,15 @@ class ModelsTest(BaseTestCase):
         history_shape = (1, model._nvar, 1, model.number_of_modes)
         model_ic = model.initial(dt, history_shape)
         self.assertEqual(model._nvar, 2)
-        # self.assertTrue(ArrayAlmostEqual(model_ic, numpy.array([[[[ 0.4527827 ]],
-        #                                                          [[ 0.30644616]]]])) 
-        #                                                          is None)
-        
+        self.assertTrue(ArrayAlmostEqual(model_ic, numpy.array([[[[ 0.49023095]],
+                                                           [[ 0.49023095]]]])) is None)
+
     def test_g2d(self):
         """
         Default parameters:
-            
-        +---------------------------+     
-        |  SanzLeonetAl  2013       | 
+
+        +---------------------------+
+        |  SanzLeonetAl  2013       |
         +--------------+------------+
         |Parameter     |  Value     |
         +==============+============+
@@ -103,15 +102,17 @@ class ModelsTest(BaseTestCase):
         |* intrinsic frequency is   |
         |  approx 10 Hz             |
         +---------------------------+
-        
-        
+
+
         """
         model = models.Generic2dOscillator()
         history_shape = (1, model._nvar, 1, model.number_of_modes)
         model_ic = model.initial(dt, history_shape)
         self.assertEqual(model._nvar, 2)
-        #self.assertTrue(ArrayAlmostEqual(model_ic, numpy.array([[[[ 1.70245989]], [[ 0.2765286 ]]]])) is None)
-        
+        self.assertTrue(ArrayAlmostEqual(model_ic, numpy.array([[[[ 0.97607082]],
+                                                                 [[-0.03384097]]]]))
+                                                                 is None)
+
 
     def test_jansen_rit(self):
         """
@@ -120,12 +121,15 @@ class ModelsTest(BaseTestCase):
         history_shape = (1, model._nvar, 1, model.number_of_modes)
         model_ic = model.initial(dt, history_shape)
         self.assertEqual(model._nvar, 6)
-        # self.assertTrue(ArrayAlmostEqual(model_ic, numpy.array([[[[ 0.40556541]],[[ 2.52434922]],
-        #                                                          [[ 3.73943152]],[[ 3.04605971]],
-        #                                                          [[ 0.85500724]],[[ 4.27473643]]]])) 
-        #                                                          is None)
-        
-        
+        self.assertTrue(ArrayAlmostEqual(model_ic, numpy.array([[[[-0.01381552]],
+                                                                 [[-0.30892439]],
+                                                                 [[-0.09769047]],
+                                                                 [[-0.03384097]],
+                                                                 [[-0.06178488]],
+                                                                 [[-0.30892439]]]]))
+                                                                  is None)
+
+
     def test_sj2d(self):
         """
         """
@@ -134,13 +138,13 @@ class ModelsTest(BaseTestCase):
         model_ic = model.initial(dt, history_shape)
         self.assertEqual(model._nvar, 4)
         self.assertEqual(model.number_of_modes, 3)
-        # self.assertTrue(ArrayAlmostEqual(model_ic, numpy.array([[[[ 0.81113082, 0.22578466, 1.05767095]],
-        #                                                          [[ 2.15388948, 0.33114288, 0.33111966]],
-        #                                                          [[ 2.57884373, 1.25321566, 0.76664846]],
-        #                                                          [[ 0.76729577, 0.65537159, 0.65864133]]]])) 
-        #                                                          is None)
-        
-        
+        self.assertTrue(ArrayAlmostEqual(model_ic, numpy.array([[[[-0.02763104,  0.14269546,  0.0534609 ]],
+                                                                 [[-0.02392918,  0.12357789,  0.0462985 ]],
+                                                                 [[-0.02763104,  0.14269546,  0.0534609 ]],
+                                                                 [[-0.02392918,  0.12357789,  0.0462985 ]]]]))
+                                                                 is None)
+
+
     def test_sj3d(self):
         """
         """
@@ -149,14 +153,14 @@ class ModelsTest(BaseTestCase):
         model_ic = model.initial(dt, history_shape)
         self.assertEqual(model._nvar, 6)
         self.assertEqual(model.number_of_modes, 3)
-        # self.assertTrue(ArrayAlmostEqual(model_ic, numpy.array([[[[ 0.81113082, 0.22578466, 1.05767095]],
-        #                                                          [[ 3.39866927, -1.59312788, -1.59319147]],
-        #                                                          [[ 8.57884373, 7.25321566, 6.76664846]],
-        #                                                          [[ 0.88599684, 0.75675792, 0.7605335 ]],
-        #                                                          [[ 0.88352129, 6.98631166, 6.29850938]],
-        #                                                          [[ 6.91821169, 7.65394629, 6.51316375]]]])) 
-        #                                                          is None)
-    
+        self.assertTrue(ArrayAlmostEqual(model_ic, numpy.array([[[[-0.02763104,  0.14269546,  0.0534609 ]],
+                                                                 [[-2.56553276, -2.16156801, -2.37320634]],
+                                                                 [[ 5.97236896,  6.14269546,  6.0534609 ]],
+                                                                 [[-0.02763104,  0.14269546,  0.0534609 ]],
+                                                                 [[-0.06178488,  0.31907674,  0.1195422 ]],
+                                                                 [[ 5.97236896,  6.14269546,  6.0534609 ]]]]))
+                                                                 is None)
+
     def test_reduced_wong_wang(self):
         """
         """
@@ -164,8 +168,30 @@ class ModelsTest(BaseTestCase):
         history_shape = (1, model._nvar, 1, model.number_of_modes)
         model_ic = model.initial(dt, history_shape)
         self.assertEqual(model._nvar, 1)
-        #self.assertTrue(ArrayAlmostEqual(model_ic, numpy.array([[[[ 0.78677805]]]])) is None)
-                                        
+        self.assertTrue(ArrayAlmostEqual(model_ic, numpy.array([[[[ 0.49023095]]]])) is None)
+
+
+    def test_zetterberg_jansen(self):
+            """
+            """
+            model = models.ZetterbergJansen()
+            history_shape = (1, model._nvar, 1, model.number_of_modes)
+            model_ic = model.initial(dt, history_shape)
+            self.assertEqual(model._nvar, 12)
+            self.assertTrue(ArrayAlmostEqual(model_ic, numpy.array([[[[ -0.13815519]],
+                                                                     [[ -0.30892439]],
+                                                                     [[-25.1196459 ]],
+                                                                     [[-47.10057849]],
+                                                                     [[-47.10057849]],
+                                                                     [[-47.10057849]],
+                                                                     [[-40.10701455]],
+                                                                     [[-40.10701455]],
+                                                                     [[-40.10701455]],
+                                                                     [[ -0.30892439]],
+                                                                     [[-40.10701455]],
+                                                                     [[-40.10701455]]]]))
+                                                                     is None)
+
 
 def suite():
     """
@@ -180,4 +206,4 @@ if __name__ == "__main__":
     #So you can run tests from this package individually.
     TEST_RUNNER = unittest.TextTestRunner()
     TEST_SUITE = suite()
-    TEST_RUNNER.run(TEST_SUITE) 
+    TEST_RUNNER.run(TEST_SUITE)
