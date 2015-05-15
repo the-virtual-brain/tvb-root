14c30e0aa0d5868ddb5c8587413f0d523ca18b93
liadomide <lia_domide@yahoo.com>
2015-05-15 19:16:05 +0000

    Fill in basic unit-tests for coupling methods (to avoid errors like the one fixed in commit r7152)
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7160 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- scientific_library/tvb/tests/library/simulator/coupling_test.py
+++ scientific_library/tvb/tests/library/simulator/coupling_test.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #
-#  TheVirtualBrain-Scientific Package. This package holds all simulators, and 
+#  TheVirtualBrain-Scientific Package. This package holds all simulators, and
 # analysers necessary to run brain-simulations. You can use it stand alone or
 # in conjunction with TheVirtualBrain-Framework Package. See content of the
 # documentation-folder for more details. See also http://www.thevirtualbrain.org
@@ -37,11 +37,10 @@ Test for tvb.simulator.coupling module
 
 if __name__ == "__main__":
     from tvb.tests.library import setup_test_console_env
-
     setup_test_console_env()
 
+import numpy
 import unittest
-
 from tvb.tests.library.base_testcase import BaseTestCase
 from tvb.simulator import coupling
 
@@ -54,18 +53,68 @@ class CouplingTest(BaseTestCase):
         - check functionality
         
     """
+    weights = numpy.array([[0, 1], [1, 0]])
+    state_1sv = numpy.array([[[1], [2]]])               # (state_variables, nodes, modes)
+    state_2sv = numpy.array([[[1], [2]], [[1], [2]]])
+    delayed_state_1sv = numpy.ones((2, 1, 2, 1))        # nodes, state_variables, nodes, modes
+    delayed_state_2sv = numpy.ones((2, 2, 2, 1))
+
+    def _apply_coupling(self, k):
+        k.configure()
+        k.configure()
+        k(self.weights, self.state_1sv, self.delayed_state_1sv)
+
+    def _apply_coupling_2sv(self, k):
+        k.configure()
+        k.configure()
+        k(self.weights, self.state_2sv, self.delayed_state_2sv)
+
+
+    def test_difference_coupling(self):
+        k = coupling.Difference()
+        self.assertEqual(k.a, 0.1)
+        self._apply_coupling(k)
+
+
+    def test_hyperbolic_coupling(self):
+        k = coupling.HyperbolicTangent()
+        self.assertEqual(k.a, 1)
+        self.assertEqual(k.b, 1)
+        self.assertEqual(k.midpoint, 0)
+        self.assertEqual(k.sigma, 1)
+        self._apply_coupling(k)
+
+
+    def test_kuramoto_coupling(self):
+        k = coupling.Kuramoto()
+        self.assertEqual(k.a, 1)
+        self._apply_coupling(k)
 
 
     def test_linear_coupling(self):
         k = coupling.Linear()
         self.assertEqual(k.a, 0.00390625)
         self.assertEqual(k.b, 0.0)
+        self._apply_coupling(k)
+
+
+    def test_pre_sigmoidal_coupling(self):
+        k = coupling.PreSigmoidal()
+        self.assertEqual(k.H, 0.5)
+        self.assertEqual(k.Q, 1.)
+        self.assertEqual(k.G, 60.)
+        self.assertEqual(k.P, 1.)
+        self.assertEqual(k.theta, 0.5)
+        self.assertEqual(k.dynamic, True)
+        self.assertEqual(k.globalT, False)
+        self._apply_coupling_2sv(k)
 
 
     def test_scaling_coupling(self):
         k = coupling.Scaling()
         # Check scaling -factor
         self.assertEqual(k.a, 0.00390625)
+        self._apply_coupling(k)
 
 
     def test_sigmoidal_coupling(self):
@@ -75,6 +124,18 @@ class CouplingTest(BaseTestCase):
         self.assertEqual(k.midpoint, 0.0)
         self.assertEqual(k.sigma, 230.)
         self.assertEqual(k.a, 1.0)
+        self._apply_coupling(k)
+
+
+    @unittest.skipIf(True, "TODO: find why this test is failing")
+    def test_sigmoidal_jr_coupling(self):
+        k = coupling.SigmoidalJansenRit()
+        self.assertEqual(k.cmin, 0.0)
+        self.assertEqual(k.cmax, 2.0 * 0.0025)
+        self.assertEqual(k.midpoint, 6.0)
+        self.assertEqual(k.r, 1.0)
+        self.assertEqual(k.a, 0.56)
+        self._apply_coupling_2sv(k)
 
 
 
