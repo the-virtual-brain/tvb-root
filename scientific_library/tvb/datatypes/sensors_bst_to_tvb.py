fd626ade2f1fbe2025921738d38732cd36c271c7
liadomide <lia_domide@yahoo.com>
2015-06-22 15:00:45 +0000

    update bst to tvb sensor conversion to use mm and meg/seeg also
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7242 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- scientific_library/tvb/datatypes/sensors_bst_to_tvb.py
+++ scientific_library/tvb/datatypes/sensors_bst_to_tvb.py
@@ -1,13 +1,82 @@
+# -*- coding: utf-8 -*-
+#
+#  TheVirtualBrain-Scientific Package. This package holds all simulators, and
+# analysers necessary to run brain-simulations. You can use it stand alone or
+# in conjunction with TheVirtualBrain-Framework Package. See content of the
+# documentation-folder for more details. See also http://www.thevirtualbrain.org
+#
+# (c) 2012-2013, Baycrest Centre for Geriatric Care ("Baycrest")
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by the Free
+# Software Foundation. This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+# License for more details. You should have received a copy of the GNU General
+# Public License along with this program; if not, you can download it here
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0
+#
+#
+#   CITATION:
+# When using The Virtual Brain for scientific publications, please cite it as follows:
+#
+#   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
+#   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
+#       The Virtual Brain: a simulator of primate brain network dynamics.
+#   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
+#
+#
+
+"""
+Small script for converting Brainstorm sensor files for our default dataset
+to the simple ASCII format used by TVB (and other software).
+
+NB: Brainstorm uses meters, TVB uses millimeters.
+
+.. moduleauthor:: Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
+
+"""
+
 import numpy
 import scipy.io
 
 tvb_data_path = '/home/sophie/dev/tvb_data/tvb_data/'
-eeg_chan_path = 'brainstorm/data/TVB-Subject/EEG_channels/channel.mat'
-mat = scipy.io.loadmat(tvb_data_path + eeg_chan_path)
+bst_path = tvb_data_path + 'brainstorm/data/TVB-Subject/'
 
-name = [l[0] for l in mat['Channel']['Name'][0]]
-loc = numpy.array([l[:, 0] for l in mat['Channel']['Loc'][0]])
+chan_path = {
+    'eeg': 'EEG_channels/channel.mat',
+    'meg': 'MEGchannels/channel_4d_acc1.mat',
+    'seeg': 'seeg_channels/channel.mat',
+}
 
-with open(tvb_data_path + 'sensors/eeg-brainstorm-65.txt', 'w') as fd:
-    for n, (x, y, z) in zip(name, loc):
-        fd.write('\t'.join(map(str, (n, x, y, z))) + '\n')
+def get_field_array(mat_group, n=3, dtype=numpy.float64):
+    return numpy.array([
+        l.flat[:n] if l.size else numpy.zeros((n, ), dtype)
+        for l in mat_group[0]
+    ], dtype=dtype)
+
+for sens_type, sens_path in chan_path.iteritems():
+    # only MEG channels require orientation information
+    use_ori = sens_type in ('meg', )
+    # read from MAT file necessary fields
+    mat = scipy.io.loadmat(bst_path + sens_path)
+    name = [l[0] for l in mat['Channel']['Name'][0]]
+    loc = get_field_array(mat['Channel']['Loc'])
+    if use_ori:
+        ori = get_field_array(mat['Channel']['Orient'])
+    # bst uses m, we use mm
+    loc *= 1e3
+    # write out to text format
+    out_fname = '%s/sensors/%s-brainstorm-%d.txt'
+    out_fname %= tvb_data_path, sens_type, len(name)
+    with open(out_fname, 'w') as fd:
+        if use_ori: # MEG
+            for n, (x, y, z), (ox, oy, oz) in zip(name, loc, ori):
+                line = '\t'.join(['%s']+['%f']*6) + '\n'
+                line %= n, x, y, z, ox, oy, oz
+                fd.write(line)
+        else: # sEEG, EEG
+            for n, (x, y, z) in zip(name, loc):
+                line = '\t'.join(['%s']+['%f']*3) + '\n'
+                line %= n, x, y, z
+                fd.write(line)
