a2f3cd9d24e9ef9bb330490283dcf2d01a1bac9c
maedoc <mw@eml.cc>
2015-06-24 14:27:08 +0000

    TVB-1756 use transform expressions instead of variables of interest (but will show vois if no expressions given)
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7252 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- framework_tvb/tvb/adapters/visualizers/time_series.py
+++ framework_tvb/tvb/adapters/visualizers/time_series.py
@@ -77,7 +77,11 @@ class TimeSeries(ABCDisplayer):
         if preview and shape[0] > self.MAX_PREVIEW_DATA_LENGTH:
             shape[0] = self.MAX_PREVIEW_DATA_LENGTH
 
-        state_variables = time_series.labels_dimensions.get(time_series.labels_ordering[1], [])
+        if time_series.user_tag_1:
+            variables = time_series.user_tag_1.split(';')
+        else:
+            variables = time_series.labels_dimensions.get(time_series.labels_ordering[1], [])
+
         labels = time_series.get_space_labels()
 
         # when surface-result, the labels will be empty, so fill some of them,
@@ -91,7 +95,8 @@ class TimeSeries(ABCDisplayer):
                 'ts_title': time_series.title, 'preview': preview, 'figsize': figsize,
                 'shape': repr(shape), 't0': ts[0],
                 'dt': ts[1] - ts[0] if len(ts) > 1 else 1,
-                'labelsStateVar': state_variables, 'labelsModes': range(shape[3])
+                'labelsStateVar': variables,
+                'labelsModes': range(shape[3])
                 }
         pars.update(self.build_template_params_for_subselectable_datatype(time_series))
 
