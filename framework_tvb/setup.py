b8af9532613f747a94f50de1ac7d8417b20cde8f
liadomide <lia_domide@yahoo.com>
2015-06-24 17:26:20 +0000

    When using h5y the latest version we get segfault, fix the version needed for TVB-framework
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7256 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- framework_tvb/setup.py
+++ framework_tvb/setup.py
@@ -41,13 +41,13 @@ import shutil
 import setuptools
 
 
-VERSION = "1.1"
+VERSION = "1.3"
 
-TVB_TEAM = "Lia Domide, Ionel Ortelecan, Bogdan Neacsa, Calin Pavel, "
+TVB_TEAM = "Mihai Andrei, Lia Domide, Ionel Ortelecan, Bogdan Neacsa, Calin Pavel, "
 TVB_TEAM += "Stuart Knock, Marmaduke Woodman, Paula Sansz Leon, "
 
 TVB_INSTALL_REQUIREMENTS = ["apscheduler", "beautifulsoup", "cherrypy", "genshi", "cfflib", "formencode==1.3.0a1",
-                            "h5py", "lxml", "minixsv", "mod_pywebsocket", "networkx", "nibabel", "numpy",
+                            "h5py==2.3.0", "lxml", "minixsv", "mod_pywebsocket", "networkx", "nibabel", "numpy",
                             "numexpr", "psutil", "scikit-learn", "scipy", "simplejson", "PIL>=1.1.7",
                             "sqlalchemy==0.7", "sqlalchemy-migrate==0.7.2", "matplotlib==1.2.1"]
 
