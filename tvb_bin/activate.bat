341adff0ed42218a2ace3e1975e990bac6251732
maedoc <mw@eml.cc>
2015-06-26 21:33:36 +0000

    add 'activate' style scripts for win/*nix to add TVB libs to path
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7274 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

---
+++ tvb_bin/activate.bat
@@ -0,0 +1,2 @@
+
+set PYTHONPATH=%CD%\scientific_library;%CD%\framework_tvb;%CD%\externals\mplh5canvas;%CD%\tvb_data;%CD%;%PYTHONPATH%
---
+++ tvb_bin/activate.sh
@@ -0,0 +1,2 @@
+
+export PYTHONPATH=$(pwd)/scientific_library:$(pwd)/framework_tvb:$(pwd)/externals/mplh5canvas:$(pwd)/tvb_data:$(pwd):$PYTHONPATH
