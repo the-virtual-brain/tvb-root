99812308d6c7c47361b552e7bcb95155c553bc7e
maedoc <mw@eml.cc>
2015-06-26 21:14:48 +0000

    use call in batch file, equiv to sh source
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7273 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- build_resources/make_conda_env.bat
+++ build_resources/make_conda_env.bat
@@ -2,7 +2,10 @@ REM build env with pre-built binaries from conda repos
 conda create -y -n tvb-build matplotlib scipy networkx scikit-learn cython h5py pip pil numexpr psutil coverage 
 
 REM use environment
-activate tvb-build
+call activate tvb-build
 
 REM add locally built or pure Python packages
 pip install gdist sqlalchemy==0.7.8 sqlalchemy-migrate minixsv formencode genshi simplejson mod_pywebsocket cherrypy cfflib nibabel
+
+REM exit environment
+deactivate
