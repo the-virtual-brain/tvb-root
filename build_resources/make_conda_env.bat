e9c2ab2f45e7d183fee3accb41bf3f688a62bd27
maedoc <mw@eml.cc>
2015-06-26 20:29:23 +0000

    add conda build script for Win, assuming VC++ for Py 2.7 installed
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7272 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

---
+++ build_resources/make_conda_env.bat
@@ -0,0 +1,8 @@
+REM build env with pre-built binaries from conda repos
+conda create -y -n tvb-build matplotlib scipy networkx scikit-learn cython h5py pip pil numexpr psutil coverage 
+
+REM use environment
+activate tvb-build
+
+REM add locally built or pure Python packages
+pip install gdist sqlalchemy==0.7.8 sqlalchemy-migrate minixsv formencode genshi simplejson mod_pywebsocket cherrypy cfflib nibabel
