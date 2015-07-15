4eaf7d8aa1e31ee9a5291b3a3445672664ca1cd6
liadomide <lia_domide@yahoo.com>
2015-07-15 10:10:10 +0000

    TVB-1178 Explicitly list IPython dependencies.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7382 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- setup_mac.py
+++ setup_mac.py
@@ -86,8 +86,9 @@ PY2APP_PACKAGES = ['cherrypy', 'email', 'h5py', 'idlelib', 'migrate', 'minixsv',
                    'numpy', 'scipy', 'sklearn', 'tables', 'tvb']
 
 PY2APP_INCLUDES = ['apscheduler', 'apscheduler.scheduler', 'cfflib', 'cmath', 'contextlib', 'formencode',
-                   'gdist', 'genshi', 'genshi.template', 'genshi.template.loader', 'logging.config', 'lxml.etree',
-                   'lxml._elementpath', 'matplotlib', 'minixsv', 'mod_pywebsocket', 'mplh5canvas.backend_h5canvas',
+                   'gdist', 'genshi', 'genshi.template', 'genshi.template.loader',
+                   'jinja2', 'jsonschema', 'logging.config', 'lxml.etree', 'lxml._elementpath',
+                   'markupsafe', 'matplotlib', 'minixsv', 'mod_pywebsocket', 'mplh5canvas.backend_h5canvas',
                    'mpl_toolkits.axes_grid', 'nibabel', 'numexpr', 'os', 'psycopg2', 'runpy', 'sqlite3', 'sqlalchemy',
                    'sqlalchemy.dialects.sqlite', 'sqlalchemy.dialects.postgresql', 'simplejson', 'StringIO', 'tornado',
                    'xml.dom', 'xml.dom.minidom', 'zlib', 'zmq']
