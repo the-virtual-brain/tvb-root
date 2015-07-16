22f5621a22ea69e53ce15daa17898a18f6eb3f94
mihandrei <mihai.daniel.psmg@gmail.com>
2015-07-16 14:47:24 +0000

    Exclude tvb_documentor from the rst search
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7419 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb_documentation_new/conf.py
+++ tvb_documentation_new/conf.py
@@ -74,7 +74,7 @@ release = TvbProfile.current.version.CURRENT_VERSION
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'tvb_documentor']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
