a35694bfc599c93a78462e96bad16d190d95801a
mihandrei <mihai.daniel.psmg@gmail.com>
2015-07-16 15:30:48 +0000

    Remove obsolete tutorials and readme-s. Exclude more folders from the build
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7422 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb_documentation_new/conf.py
+++ tvb_documentation_new/conf.py
@@ -74,7 +74,13 @@ release = TvbProfile.current.version.CURRENT_VERSION
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'tvb_documentor']
+exclude_patterns = [
+    '_build',
+
+    'tvb_documentor',
+    'handouts',
+    'requirements'
+]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
