c233d54a43cabacc8b2a9533701f1e32a306c220
mihandrei <mihai.daniel.psmg@gmail.com>
2015-08-01 09:44:46 +0000

    TVB-1893 Add time to file open/close trace
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7471 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb_bin/debug/diagnose_file_trace.py
+++ tvb_bin/debug/diagnose_file_trace.py
@@ -38,6 +38,10 @@ Once the trace is collected run  python diagnose_file_trace.py [filter_path_pref
 
 import __builtin__
 import traceback
+from datetime import datetime
+
+def timestamp():
+    return datetime.now().strftime("%H:%M:%S.%f")
 
 def monkey_patch_file_to_trace(trace_file='file_open_trace'):
 
@@ -52,7 +56,7 @@ def monkey_patch_file_to_trace(trace_file='file_open_trace'):
     class newfile(oldfile):
         def __init__(self, *args):
             self.x = args[0]
-            _file_open_trace.write("OPENING %s\n" % str(self.x))
+            _file_open_trace.write("$$ %s OPENING %s\n" % ( timestamp(), str(self.x)))
             # traceback module will open python files to get the trace. We must exclude them from tracing otherwise stackoverflow
             if not self.x.endswith('py'):
                 _file_open_trace.write(''.join(traceback.format_stack()))
@@ -60,7 +64,7 @@ def monkey_patch_file_to_trace(trace_file='file_open_trace'):
             # openfiles.add(self)
 
         def close(self):
-            _file_open_trace.write("CLOSING %s\n" % str(self.x))
+            _file_open_trace.write("$$ %s CLOSING %s\n" % ( timestamp(), str(self.x)))
             if not self.x.endswith('py'):
                 _file_open_trace.write(''.join(traceback.format_stack()))
             oldfile.close(self)
@@ -85,13 +89,18 @@ def parse_trace():
 
     with open('file_open_trace') as f:
         for line in f:
-            if line.startswith("OPENING") or line.startswith("CLOSING"):
-                idx = line.find(' ')
-                op = line[:idx]
-                pth = line[idx+1:-1]
-                result.append((pth, op, []))
+            if line.startswith("$$"):
+                idx1 = line.find(' ')
+                idx2 = line.find(' ', idx1 + 1)
+                idx3 = line.find(' ', idx2 + 1)
+
+                time = line[idx1+1:idx2]
+                op = line[idx2+1:idx3]
+                pth = line[idx3+1:-1]
+
+                result.append((pth, op, time, []))
             else:
-                result[-1][2].append(line)
+                result[-1][3].append(line)
 
     return result
 
@@ -104,7 +113,7 @@ def analyse_trace(filter_path_prefix=None):
 
     result = {}
 
-    for pth, op, stack in trace:
+    for pth, op, time, stack in trace:
         if pth not in result:
             result[pth] = 0
         if op == "OPENING":
@@ -112,23 +121,37 @@ def analyse_trace(filter_path_prefix=None):
         else:
             result[pth] -= 1
 
+    print
+    print '  List of files with unbalanced open close calls '
+    print '================================================='
+    print
+
+    for guilty_pth in result:
+        if result[guilty_pth] != 0:
+            print '%s %d' % (guilty_pth, result[guilty_pth])
+    print
+    print ' TRACES '
+    print '========'
+    print
+
     for guilty_pth in result:
         if result[guilty_pth] != 0:
             print
-            print '===================='
-            print 'this file is guilty %s %d' % (guilty_pth, result[guilty_pth])
+            print '=' * 80
+            print 'file: %s ' % guilty_pth
+            print 'open-close: %d' % result[guilty_pth]
             print 'printing access log'
             print
 
             count = 0
-            for pth, op, stack in trace:
+            for pth, op, time, stack in trace:
                 if pth == guilty_pth:
                     if op == "OPENING":
                         count += 1
                     else:
                         count -= 1
 
-                    print '---------------- %s %d' %(op, count)
+                    print '%s  %s %d -----------' %(time, op, count)
                     for st in stack:
                         print st,
 
