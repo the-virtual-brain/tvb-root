9f84f5e3ae3ce6cbfe9aad5c568e957bc5312e42
liadomide <lia_domide@yahoo.com>
2015-08-27 09:09:24 +0000

    TVB-1918 Update mplh5canvas version
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7553 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- externals/mplh5canvas/setup.py
+++ externals/mplh5canvas/setup.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python
+
 """
 Install an HTML5 backcend for Maplotlib
 """
+
 from setuptools import setup, find_packages
-from matplotlib import __version__
-import sys
 
-if tuple([int(x) for x in __version__.split(".")[:2]]) < (0, 99, 1):
-    print "The HTML 5 Canvas Backend requires matplotlib 0.99.1.1 or newer. \
-           Your version (%s) appears older than this. Unable to continue..." \
-           % __version__
-    sys.exit(0)
 
-setup (
-    name = "mplh5canvas",
-    version = "trunk",
-    description = "Matplotlib HTML 5 Canvas Backend",
-    author = "Simon Ratcliffe, Ludwig Schwardt",
-    packages = find_packages(),
-    scripts = [],
-    zip_safe = False,
+setup(
+    name="mplh5canvas",
+    version="1.0tvb",
+    description="Matplotlib HTML 5 Canvas Backend",
+    author="Simon Ratcliffe, Ludwig Schwardt",
+    packages=find_packages(),
+    scripts=[],
+    zip_safe=False,
+    install_requires=["matplotlib>=1.2.1"]
 )
