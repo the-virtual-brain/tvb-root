8d6a57257ba1808a051a911b42be121594ff5b0f
liadomide <lia_domide@yahoo.com>
2015-09-09 05:56:18 +0000

    don't forget to configure sensors
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7573 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- scientific_library/tvb/simulator/monitors.py
+++ scientific_library/tvb/simulator/monitors.py
@@ -749,6 +749,9 @@ class Projection(Monitor):
         if hasattr(self, 'sensors'):
             self.sensors.configure()
 
+        if hasattr(self, 'sensors'):
+            self.sensors.configure()
+
         # setup convenient locals
         surf = simulator.surface
         conn = simulator.connectivity
