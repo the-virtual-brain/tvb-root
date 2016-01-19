2344397dabb96bdaf860b9f3eaa9b49caf7df22a
liadomide <lia_domide@yahoo.com>
2016-01-19 11:07:28 +0000

    Cleanup unused external files
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7717 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- externals/Berlin-Petra/MRImaging_ArtefactCorrectionForEEG/ff_600hzTVB_BaselineCorr.m
+++
@@ -1,5 +0,0 @@
-function [out,base]=ff_600hzTVB_baselinecorr(in,basetime)
- 
-    base=mean(in(:,basetime),2);
-    base_matrix=repmat(base,1,size(in,2));
-    out=in-base_matrix;
--- externals/Berlin-Petra/MRImaging_ArtefactCorrectionForEEG/ff_600hzTVB_Meta.m
+++
@@ -1,8 +0,0 @@
-%add path to scripts
-addpath('\\naas\synchrony\_Projects\TVB\SkripteFrank\600hzTVB')
-
-%set parameters, paths, filenames
-ff_600hzTVB_Init;
-
-%execute MR artifact correction 
-[EEG,EEG_corr,Xx] = ff_600hzTVB_MRCorr(Xx);
