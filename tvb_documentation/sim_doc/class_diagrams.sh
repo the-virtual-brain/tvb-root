4eebde26011ac489513e6176640c4b14e8419779
liadomide <lia_domide@yahoo.com>
2016-06-15 13:53:55 +0000

    TVB-2144 Update Simulator classes diagrams before release
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7981 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb_documentation/sim_doc/class_diagrams.sh
+++ tvb_documentation/sim_doc/class_diagrams.sh
@@ -5,65 +5,66 @@ set -o verbose
 # Reverse engineer the classes of tvb.simulator, show only public attributes #
 ##############################################################################
 
+SIM_ROOT=../../scientific_library/tvb/simulator
 
 ## Simulator ##
-pyreverse --project=simulator ../simulator.py
+pyreverse --project=simulator $SIM_ROOT/simulator.py
 
 dot -Tsvg classes_simulator.dot > img/classes_simulator.svg
 dot -Tfig classes_simulator.dot > img/classes_simulator.fig
 
 
 ## Integrators ##
-pyreverse --project=integrators ../integrators.py
+pyreverse --project=integrators $SIM_ROOT/integrators.py
 
 dot -Tsvg classes_integrators.dot > img/classes_integrators.svg
 dot -Tfig classes_integrators.dot > img/classes_integrators.fig
 
 
 ## Models ##
-pyreverse --project=models ../models.py
+pyreverse --project=models $SIM_ROOT/models
 
 dot -Tsvg classes_models.dot > img/classes_models.svg
 dot -Tfig classes_models.dot > img/classes_models.fig
 
 
 ## Monitors ##
-pyreverse --project=monitors ../monitors.py
+pyreverse --project=monitors $SIM_ROOT/monitors.py
 
 dot -Tsvg classes_monitors.dot > img/classes_monitors.svg
 dot -Tfig classes_monitors.dot > img/classes_monitors.fig
 
 
 ## Couplings ##
-pyreverse --project=coupling ../coupling.py
+pyreverse --project=coupling $SIM_ROOT/coupling.py
 
 dot -Tsvg classes_coupling.dot > img/classes_coupling.svg
 dot -Tfig classes_coupling.dot > img/classes_coupling.fig
 
 
 ## Noise ##
-pyreverse --project=noise ../noise.py
+pyreverse --project=noise $SIM_ROOT/noise.py
 
 dot -Tsvg classes_noise.dot > img/classes_noise.svg
 dot -Tfig classes_noise.dot > img/classes_noise.fig
 
 
 ## Phase-Plane Interactive ##
-pyreverse --project=phase_plane_interactive ../plot/phase_plane_interactive.py
+pyreverse --project=phase_plane_interactive $SIM_ROOT/plot/phase_plane_interactive.py
 
 dot -Tsvg classes_phase_plane_interactive.dot > img/classes_phase_plane_interactive.svg
 dot -Tfig classes_phase_plane_interactive.dot > img/classes_phase_plane_interactive.fig
 
 
 ## Power-Spectra Interactive ##
-pyreverse --project=power_spectra_interactive ../plot/power_spectra_interactive.py
+pyreverse --project=power_spectra_interactive $SIM_ROOT/plot/power_spectra_interactive.py
 
 dot -Tsvg classes_power_spectra_interactive.dot > img/classes_power_spectra_interactive.svg
 dot -Tfig classes_power_spectra_interactive.dot > img/classes_power_spectra_interactive.fig
 
 
 ## Time-Series Interactive ##
-pyreverse --project=timeseries_interactive ../plot/timeseries_interactive.py
+pyreverse --project=timeseries_interactive $SIM_ROOT/plot/timeseries_interactive.py
 
 dot -Tsvg classes_timeseries_interactive.dot > img/classes_timeseries_interactive.svg
 dot -Tfig classes_timeseries_interactive.dot > img/classes_timeseries_interactive.fig
@@ -71,4 +72,4 @@ dot -Tfig classes_timeseries_interactive.dot > img/classes_timeseries_interactiv
 
 ## Clean up ##
 rm *dot
-#rm img/*.fig
+rm img/*.fig
