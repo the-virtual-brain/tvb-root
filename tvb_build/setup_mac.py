072319a7960f5ef7b2f8d9af94cf2bae028ca872
liadomide <lia_domide@yahoo.com>
2016-06-15 11:42:13 +0000

    TVB-2144 Add numba to the Mac package
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@7976 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb_build/setup_mac.py
+++ tvb_build/setup_mac.py
@@ -65,7 +65,7 @@ EXCLUDED_DYNAMIC_LIBS = []
 
 # --------------------------- PY2APP specific configurations--------------------------------------------
 
-PY2APP_PACKAGES = ['cherrypy', 'email', 'h5py', 'IPython', 'idlelib', 'migrate', 'minixsv',
+PY2APP_PACKAGES = ['cherrypy', 'email', 'h5py', 'IPython', 'idlelib', 'migrate', 'minixsv', 'numba',
                    'numpy', 'PIL', 'pkg_resources', 'scipy', 'sklearn', 'tables', 'tornado', 'tvb']
 
 PY2APP_INCLUDES = ['apscheduler', 'apscheduler.scheduler', 'cfflib', 'cmath', 'contextlib', 'formencode', 'gdist',
