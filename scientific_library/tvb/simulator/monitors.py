0f091e0b84b08388b76bafbc86eb6da3f98e3b85
liadomide <lia_domide@yahoo.com>
2016-06-21 06:21:41 +0000

    Solve merge conflict - bis
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@8024 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- scientific_library/tvb/simulator/monitors.py
+++ scientific_library/tvb/simulator/monitors.py
@@ -447,12 +447,6 @@ class Projection(Monitor):
         if hasattr(self, 'sensors'):
             self.sensors.configure()
 
-        if hasattr(self, 'sensors'):
-            self.sensors.configure()
-
-        if hasattr(self, 'sensors'):
-            self.sensors.configure()
-
         # setup convenient locals
         surf = simulator.surface
         conn = simulator.connectivity
