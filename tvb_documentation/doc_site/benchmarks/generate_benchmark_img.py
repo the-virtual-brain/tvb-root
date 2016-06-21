0e9d2d98db0688d70eb90b125c83e3aec0336366
liadomide <lia_domide@yahoo.com>
2016-06-21 13:59:00 +0000

    TVB-2023 Update benchmark 1.5 as an average over multiple runs
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@8031 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb_documentation/doc_site/benchmarks/generate_benchmark_img.py
+++ tvb_documentation/doc_site/benchmarks/generate_benchmark_img.py
@@ -58,19 +58,19 @@ x_labels = ["1.4", "1.4.1", "1.5"]
 figure = pyplot.figure()
 
 plot(131, "Generic2dOscillator 1000sec", 20,
-     [14.5, 17.9, 6.3], "nodes=192 dt=0.05",
-     [9.5, 11.0, 6.1], "nodes=96 dt=0.05",
+     [14.5, 17.9, 6.6], "nodes=192 dt=0.05",
+     [9.5, 11.0, 6.2], "nodes=96 dt=0.05",
      [5, 5.9, 3.5], "nodes=96 dt=0.1")
 
 plot(132, "Epileptor 1000sec", 30,
-     [21.8, 25.5, 10.3], "nodes=192 dt=0.05",
-     [12.8, 16.2, 9.1], "nodes=96 dt=0.05",
-     [6.7, 7.5, 4.3], "nodes=96 dt=0.1")
+     [21.8, 25.5, 9.1], "nodes=192 dt=0.05",
+     [12.8, 16.2, 8.7], "nodes=96 dt=0.05",
+     [6.7, 7.5, 4.6], "nodes=96 dt=0.1")
 
 plot(133, "LarterBreakspear 10000sec", 180,
-     [159.6, 145.4, 89.5], "nodes=192 dt=0.1",
-     [88.6, 89.8, 78.8], "nodes=96 dt=0.1",
-     [40.7, 39.0, 43.9], "nodes=96 dt=0.2")
+     [159.6, 145.4, 82.8], "nodes=192 dt=0.1",
+     [88.6, 89.8, 71.1], "nodes=96 dt=0.1",
+     [40.7, 39.0, 39.1], "nodes=96 dt=0.2")
 
 pyplot.show()
 figure.savefig("../../_static/benchmarks-evolution-mac.png")
