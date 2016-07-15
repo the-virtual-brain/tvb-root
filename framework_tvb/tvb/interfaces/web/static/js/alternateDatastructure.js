573b60bdecf6bc7e0bcbbe889ce7582fc26d7827
DevinBayly <baylyd@email.arizona.edu>
2016-07-15 00:10:43 +0000

    TVB-2177 expand upon the API skeleton for the new structure. Add function for adding new results, updating the step arrays, and computing distances in the geometric selection.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@8074 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- framework_tvb/tvb/interfaces/web/static/js/alternateDatastructure.js
+++ framework_tvb/tvb/interfaces/web/static/js/alternateDatastructure.js
@@ -9,26 +9,55 @@
  */
 
 //this function will take in the original 1 dimensional array (seriesArray likely) and generate a matrix (sparse or condensed) that will be accessed upon plotting, filtering, and generating overlays.
-function createStructure(data) {
+// **I might be leaning towards using a CSR (compressed sparse Row) structure here to actually accomplish the reasonable sized storing**
+function createStructure(data, xlabels, ylabels) {
 
 }
+
+//very important to be able to add new results into the current structure whatever winds up being selected
+//this could be a good place to update a list that is storing the step values that have been used so far. Probably the call to update the coordinate arrays
+function mergeResults(newData, data) {
+
+    //this function will be necessary because there will need to be a single array comprised of the parameter values used to generate the results.
+    // Think of arrays of the xparameter and yparameter values used as coordinates to position results in the canvas; these arrays need updating when more results are added.
+    function updateCoordinateArrays(oldArr, NewArr) {
+
+    }
+
+}
+
+
 
 //this function will gather information about the specifications that the user has selected above, and the logical relation between them (AND,OR) and will iteratively progress through the results one by one adding them to a return if they fit the search criteria
 function filterResults(data) {
 
+
 }
 
 //this function will calculate the difference between each result and its neighbors upon a selected metric. This difference will be compared to a user input value, and will dictate whether there are colored contour lines added inbetween results to help visualize the change in results based on parameter change.
-function compareToNeighbors(data) {
+//don't forget that this will need to have some sort of threshold for the number of results that are coming back from the geometric selection before proceeding
+//      if there happens to be too many decrease the distance value to the next smallest step value (x&y step arrays) that exists from the calculations.
+function compareToNeighbors(data, xSteps, ySteps) {
+
+    //this function is a helper for compareToNeighbors which will generate an object composed of other results within a specific distance from the result of interest (one currently being investigated out of all).
+    //the current idea here is to go with a circular geometric area from which we will select all the results within the radius distance from the central result.
+    //the other option which might eventually be easier is to try to use a square shape in the parameter space.***I think this is what I will lean towards***
+    function geometricSelection(data, centralResult, distance) {
+
+    }
+
+    //this function will take whatever number of results have been selected within the distance from the focusObj (result under investigation), and will now attach distance values to each object in the geometric selection specific to the x or y parameter
+    function assignDistance(focusObj, geoSelected) {
+
+    }
 
 }
 
-/*
- I have this commented out because it isn't a generic api aspect, I mean it could work, but it doesn't fit a barebones kind of necessity in the way that the other functions so far do.
- //this function is a helper for compareToNeighbors which will generate an object composed of other results within a specific distance from the result of interest (one currently being investigated out of all).
- //the current idea here is to go with a circular geometric area from which we will select all the results within the radius distance from the central result.
- //the other option which might eventually be easier is to try to use a square shape in the parameter space.***I think this is what I will lean towards***
- function returnGeometricSelection(data,centralResult,distance) {
 
- }*/
+
+
+
+
+
+
 
