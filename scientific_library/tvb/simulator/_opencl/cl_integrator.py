3bec00bb951d1fcf2fe89cdaf59caed6e2910eca
Vio8023 <huwy.vio@gmail.com>
2016-08-13 08:05:17 +0000

    Add integrators
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@8120 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- scientific_library/tvb/simulator/_opencl/cl_integrator.py
+++ scientific_library/tvb/simulator/_opencl/cl_integrator.py
@@ -2,7 +2,7 @@ import pyopencl
 import pyopencl.array
 import numpy
 from models import CLComponent, CLModel
-from ..integrators import HeunDeterministic, HeunStochastic, EulerDeterministic, EulerStochastic, Identity
+from ..integrators import HeunDeterministic, HeunStochastic, EulerDeterministic, EulerStochastic, RungeKutta4thOrderDeterministic , Identity
 from util import *
 
 grw = lambda arr, ctx: pyopencl.Buffer(ctx, pyopencl.mem_flags.COPY_HOST_PTR, hostbuf=arr)
@@ -22,51 +22,61 @@ class CLIntegrator(CLComponent):
     }
     }"""
 
-    def cl_integrate(self, numOfCoefficient, lenOfVec, vectors, coefficients, ans):
+    def cl_integrate(self, numOfCoefficient, lenOfVec, vectors, coefficients, ):
+        n_states = vectors.shape[0]
+        n_nodes = lenOfVec
+        n_mode = vectors.shape[2]
+
         size = numpy.int32(numOfCoefficient)
         lenOfVec = numpy.int32(lenOfVec)
         vec_cl = grw(vectors, self._context)
         co_cl = grw(coefficients, self._context)
+        ans = numpy.zeros((n_states,n_nodes,n_mode))
         ans_cl = grw(ans, self._context)
         args = size,lenOfVec,vec_cl,co_cl,ans_cl
-        self._kernel(self._queuequeue, (int(lenOfVec),), None, *args)
+        self._kernel(self._queue, (int(lenOfVec),), None, *args)
 
-        tmp = numpy.zeros((lenOfVec, 1), 'f')
-        pyopencl.enqueue_copy( self._queue, tmp , ans_cl)
-        return tmp
 
-    def _alloc_opencl(self, n_nodes, n_states=1, n_mode=1):
+        pyopencl.enqueue_copy( self._queue, ans , ans_cl)
+
+        return ans
+
+    def _alloc_opencl(self, n_nodes, n_states=1, n_mode=1 , stochasticVariant = False):
         if not hasattr(self, '_queue'):
             msg = "OpenCL components must be configured via the `configure_opencl` method prior to use."
             raise RuntimeError(msg)
 
         arrays = {'state': (n_states, n_nodes, n_mode), 'coupling': (1, n_nodes), 'deriv': (n_states, n_nodes, n_mode)}
+        self._arrays = {}
+        if stochasticVariant:
+            arrays['noise'] = (n_states, n_nodes, n_mode)
 
         for name, shape in arrays.items():
             self._arrays[name] = pyopencl.array.Array(self._queue, shape, 'f')
+            #print "Name: type:",isinstance(self._arrays[name],pyopencl.Buffer)
+            #print "name: %s, shape %s"%(name,shape)
 
-        pass
     def scheme(self,  X , dfun, coupling, local_coupling = 0.0, stimulus = 0.0):
+        n_states = X.shape[0]
+        n_nodes = X.shape[1]
+        n_mode = X.shape[2]
+        print "X shape:",X.shape
+        # check for openclArray buffer
+        if not hasattr(self, '_arrays'):
+            self._alloc_opencl( n_nodes,n_states = n_states, n_mode = n_mode , stochasticVariant = True )
 
-        X_cl = pyopencl.array.Array(self._queue, X.shape ,'f')
-        coupling_cl = pyopencl.array.Array(self._queue, coupling.shape ,'f')
-        X_next = dfun(X_cl, coupling_cl)
+        print "self: %s %s X: %s %s"%(self._arrays['state'].dtype, self._arrays['state'].size, X.dtype, X.size)
+        self._arrays['state'].set( X )
 
-        return X_next
-    #TODO pass ans, st, to model; call kernel function of integrator
+        self._arrays['coupling'].set( coupling )
 
-    def scheme_cl(self, X, clModel, coupling, local_coupling=0.0, stimulus=0.0):
-        if not isinstance(clModel, CLModel ):
-            msg = "the datatype of clModel should be openclModel"
-            raise RuntimeError(msg)
-        clModel.configure_opencl(self._context,self._queue)
-
-        X_cl = pyopencl.array.Array(self._queue, X.shape, 'f')
-        coupling_cl = pyopencl.array.Array(self._queue, coupling.shape, 'f')
-
-        X_next = clModel.dfunKernel(X_cl, coupling_cl)
+        #X_cl = pyopencl.array.Array(self._queue, X.shape ,'f')
+        #coupling_cl = pyopencl.array.Array(self._queue, coupling.shape ,'f')
+        X_next = dfun(self._arrays['state'], self._arrays['coupling'])
 
         return X_next
+
+
 
     def setUp(self):
         self._context, self._queue = context_and_queue(create_cpu_context())
@@ -82,76 +92,186 @@ class CL_EulerDeterministic( CLIntegrator, EulerDeterministic ):
     # opencl_program_source = ""
 
     def scheme(self, X, dfun, coupling, local_coupling=0.0, stimulus=0.0):
-        X_cl, coupling_cl = pyopencl.array.Array(self._queue, X.shape, 'f'), \
-                            pyopencl.array.Array(self._queue, coupling.shape, 'f')
-        self.dX = dfun(X_cl, coupling_cl)
-        vectors = numpy.vstack((X_cl.data,self.dX))
+        n_states = X.shape[0]
+        n_nodes = X.shape[1]
+        n_mode = X.shape[2]
+        print "X shape:", X.shape
+        # check for openclArray buffer
+        if not hasattr(self, '_arrays'):
+            self._alloc_opencl(n_nodes, n_states=n_states, n_mode=n_mode, stochasticVariant=True)
 
-        X_next = X_cl + self.dt * (self.dX + stimulus)
-
+        #print "self: %s %s X: %s %s" % (self._arrays['state'].dtype, self._arrays['state'].size, X.dtype, X.size)
+        self._arrays['state'].set(X)
+        self._arrays['coupling'].set(coupling)
+        self.dX = dfun(self._arrays['state'], self._arrays['coupling'])
+        #X_next = self.dX + stimulus
+        X_next = self._arrays['state'] + self.dt * (self.dX + stimulus)
         return X_next
 
-    def scheme_cl(self, X, clModel, coupling, local_coupling=0.0, stimulus=0.0):
-
-        if not isinstance(clModel, CLModel):
-            msg = "the datatype of clModel should be openclModel"
-            raise RuntimeError(msg)
-        clModel.configure_opencl(self._context, self._queue)
-
-        X_cl, coupling_cl = pyopencl.array.Array(self._queue, X.shape, 'f'), \
-                            pyopencl.array.Array(self._queue, coupling.shape, 'f')
-
-        X_next = clModel.dfunKernel(X_cl, coupling_cl, local_coupling) + stimulus
-        return X_next
 
 class CL_Identity(CLIntegrator, Identity):
 
     def scheme(self, X, dfun, coupling=None, local_coupling=0.0, stimulus=0.0):
-        X_cl, coupling_cl = pyopencl.array.Array(self._queue, X.shape, 'f'),\
-                            pyopencl.array.Array(self._queue, coupling.shape, 'f')
-        return dfun(X, coupling, local_coupling) + stimulus
+        n_states = X.shape[0]
+        n_nodes = X.shape[1]
+        n_mode = X.shape[2]
+        print "X shape:", X.shape
+        # check for openclArray buffer
+        if not hasattr(self, '_arrays'):
+            self._alloc_opencl(n_nodes, n_states=n_states, n_mode=n_mode, stochasticVariant=True)
 
+        self._arrays['state'].set(X)
 
-    def scheme_cl(self, X, clModel, coupling, local_coupling=0.0, stimulus=0.0):
-        if not isinstance(clModel, CLModel):
-            msg = "the datatype of clModel should be openclModel"
-            raise RuntimeError(msg)
-        clModel.configure_opencl(self._context, self._queue)
-        X_cl, coupling_cl = pyopencl.array.Array(self._queue, X.shape, 'f'), \
-                            pyopencl.array.Array(self._queue, coupling.shape, 'f')
+        self._arrays['coupling'].set(coupling)
+        self.dX = dfun(self._arrays['state'], self._arrays['coupling'])
 
-        self.dx = clModel.dfunKernel( X_cl, coupling_cl,local_coupling )
-        vectors = self.dx + stimulus
-        coefficients = [1,1]
-        ans = pyopencl.array.Array(self._queue, (len(X)), 'f')
-        
-        return
+        return self.dX + stimulus
+
 
 
 class CL_HeunDeterministic(CLIntegrator, HeunDeterministic):
+    def _alloc_opencl(self, n_nodes, n_states=1, n_mode=1):
+        super(CL_HeunDeterministic, self)._alloc_opencl(n_nodes, n_states, n_mode, stochasticVariant=True)
+
+        self._arrays['inter'] = pyopencl.array.Array(self._queue, (n_states, n_nodes, n_mode), 'f')
 
     def scheme(self, X, dfun, coupling= None, local_coupling=0.0, stimulus=0.0):
-        X_cl, coupling_cl = pyopencl.array.Array(self._queue, X.shape, 'f'),\
-                            pyopencl.array.Array(self._queue, coupling.shape, 'f')
-        m_dx_tn = dfun(X_cl, coupling_cl, local_coupling)
-        inter = X_cl + self.dt * (m_dx_tn + stimulus)
+        n_states = X.shape[0]
+        n_nodes = X.shape[1]
+        n_mode = X.shape[2]
+        print "X shape:", X.shape
+        # check for openclArray buffer
+        if not hasattr(self, '_arrays'):
+            self._alloc_opencl(n_nodes, n_states=n_states, n_mode=n_mode)
+
+        self._arrays['state'].set(X)
+
+        self._arrays['coupling'].set(coupling)
+        self._arrays['inter'] = dfun(self._arrays['state'], self._arrays['coupling'])
+
+
+        inter = self._arrays['state'] + self.dt * (self._arrays['inter'] + stimulus)
+        #print "shceme: m_dx_tn",m_dx_tn.shape ,"inter",inter.shape
         self.clamp_state(inter)
 
-        dX = (m_dx_tn + dfun(inter, coupling_cl, local_coupling)) * self.dt / 2.0
+        dX = (self._arrays['inter'] + dfun(inter, self._arrays['coupling'])) * self.dt / 2.0
+        #print "scheme dX.shape: X_cl shape self.dt stimulus ",dX.shape, X_cl.shape, self.dt,stimulus
+        X_next = self._arrays['state'] + dX + self.dt * stimulus
 
-        X_next = X_cl + dX + self.dt * stimulus
         self.clamp_state(X_next)
 
         return X_next
 
-    def scheme_cl(self, X, clModel, coupling, local_coupling=0.0, stimulus=0.0):
-        if not isinstance(clModel, CLModel):
-            msg = "the datatype of clModel should be openclModel"
-            raise RuntimeError(msg)
-        clModel.configure_opencl(self._context, self._queue)
-        X_cl, coupling_cl = pyopencl.array.Array(self._queue, X.shape, 'f'), \
-                            pyopencl.array.Array(self._queue, coupling.shape, 'f')
 
-        X_next = clModel.dfunKernel(X_cl, coupling_cl, local_coupling) + stimulus
-        vector = X_cl + coupling_cl
+class CL_EulerStochastic(CLIntegrator, EulerStochastic):
+    def _alloc_opencl(self, n_nodes, n_states=1, n_mode=1 ):
+        super( CL_EulerStochastic, self)._alloc_opencl(n_nodes, n_states, n_mode, stochasticVariant= True )
+
+        self._arrays['inter'] = pyopencl.array.Array(self._queue, (n_states, n_nodes, n_mode), 'f')
+
+    def scheme(self, X, dfun, coupling, local_coupling = 0.0, stimulus = 0.0):
+        n_states = X.shape[0]
+        n_nodes = X.shape[1]
+        n_mode = X.shape[2]
+        print "X shape:", X.shape
+        # check for openclArray buffer
+        if not hasattr(self, '_arrays'):
+            self._alloc_opencl( n_nodes, n_states=n_states, n_mode=n_mode )
+
+        self._arrays['state'].set(X)
+        self._arrays['coupling'].set(coupling)
+        noise = self.noise.generate(X.shape).astype(numpy.float32)
+        self._arrays['noise'].set(noise)
+
+        dX = dfun(self._arrays['state'], self._arrays['coupling']) * self.dt
+        noise_gfun = self.noise.gfun(X)
+        X_next =  self._arrays['state'] + dX + noise_gfun * self._arrays['noise'] + self.dt * stimulus
+        self.clamp_state(X_next)
         return X_next
+
+
+
+class CL_HeunStochastic( CLIntegrator, HeunStochastic ):
+    def _alloc_opencl(self, n_nodes, n_states=1, n_mode=1):
+        super(CL_HeunStochastic, self)._alloc_opencl(n_nodes, n_states, n_mode, stochasticVariant=True)
+
+        self._arrays['inter'] = pyopencl.array.Array(self._queue, (n_states, n_nodes, n_mode), 'f')
+
+    def scheme(self,  X , dfun, coupling, local_coupling = 0.0, stimulus = 0.0):
+        n_states = X.shape[0]
+        n_nodes = X.shape[1]
+        n_mode = X.shape[2]
+        print "X shape:", X.shape
+        # check for openclArray buffer
+        if not hasattr(self, '_arrays'):
+            self._alloc_opencl(n_nodes, n_states=n_states, n_mode=n_mode )
+
+        self._arrays['state'].set(X)
+        self._arrays['coupling'].set(coupling)
+
+        noise = self.noise.generate(X.shape).astype(numpy.float32)
+        self._arrays['noise'].set(noise)
+
+        noise_gfun = self.noise.gfun(X)
+
+        if (noise_gfun.shape != (1,) and noise.shape[0] != noise_gfun.shape[0]):
+            msg = str("Got shape %s for noise but require %s."
+                      " You need to reconfigure noise after you have changed your model." % (
+                          noise_gfun.shape, (noise.shape[0], noise.shape[1])))
+            raise Exception(msg)
+
+        m_dx_tn = dfun(self._arrays['state'], self._arrays['coupling'])
+
+        noise *= noise_gfun
+
+        self._arrays['inter'] = self._arrays['state'] + self.dt * m_dx_tn + self._arrays['noise'] + self.dt * stimulus
+        self.clamp_state(self._arrays['inter'])
+
+        dX = (m_dx_tn + dfun(self._arrays['inter'], self._arrays['coupling'] )) * self.dt / 2.0
+
+        X_next = self._arrays['state'] + dX + self._arrays['noise'] + self.dt * stimulus
+        self.clamp_state(X_next)
+
+        return X_next
+
+
+
+
+class CL_RungeKutta4thOrderDeterministic( CLIntegrator, RungeKutta4thOrderDeterministic):
+    def scheme(self,  X , dfun, coupling, local_coupling = 0.0, stimulus = 0.0):
+
+        n_states = X.shape[0]
+        n_nodes = X.shape[1]
+        n_mode = X.shape[2]
+        print "X shape:", X.shape
+        # check for openclArray buffer
+        if not hasattr(self, '_arrays'):
+            self._alloc_opencl(n_nodes, n_states=n_states, n_mode=n_mode)
+
+        self._arrays['state'].set(X)
+        self._arrays['coupling'].set(coupling)
+
+        dt = self.dt
+        dt2 = dt / 2.0
+        dt6 = dt / 6.0
+
+        k1 = dfun(self._arrays['state'], self._arrays['coupling'])
+        self._arrays['inter'] = self._arrays['state'] + dt2 * k1
+
+        self.clamp_state(self._arrays['inter'])
+        k2 = dfun(self._arrays['inter'], self._arrays['coupling'])
+        self._arrays['inter'] = self._arrays['state'] + dt2 * k2
+
+        self.clamp_state( self._arrays['inter'])
+        k3 = dfun( self._arrays['inter'], self._arrays['coupling'])
+        self._arrays['inter'] = self._arrays['state'] + dt * k3
+        self.clamp_state(self._arrays['inter'])
+        k4 = dfun(self._arrays['inter'], self._arrays['coupling'])
+
+        dX = dt6 * (k1 + 2.0 * k2 + 2.0 * k3 + k4)
+
+        X_next = self._arrays['state'] + dX + self.dt * stimulus
+        self.clamp_state(X_next)
+        return X_next
+
+
