107cf9a2656e5a27dc842252705c30d581be08cb
liadomide <lia_domide@yahoo.com>
2017-06-26 07:47:28 +0000

    TVB-2246 - solved the color-scheme redrawing when color options are selected. Solved the redrawing of axis on window resize. First fully functional version of the viewer.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@8278 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- framework_tvb/tvb/interfaces/web/templates/genshi/visualizers/wavelet/wavelet.js
+++ framework_tvb/tvb/interfaces/web/templates/genshi/visualizers/wavelet/wavelet.js
@@ -2,17 +2,80 @@
  * Created by Dan Pop on 5/24/2017.
  */
 
-function wavelet_spectrogram_view(matrix_data, matrix_shape, start_time, end_time, freq_lo, freq_hi, vmin, vmax) {
-    interpolateTerrain = function (t) {
-        return t < 0.5 ? i0(t * 2) : i1((t - 0.5) * 2);
-    }
+var WaveletSpect = {
+    data: null,
+    n: null,
+    m: null,
+    xAxisScale: null,
+    yAxisScale: null,
+    xAxis: null,
+    yAxis: null,
+    xAxisGroup: null,
+    yAxisGroup: null,
+    vmin: null,
+    vmax: null
+};
+
+function wavelet_spectrogram_init(matrix_data, matrix_shape, start_time, end_time, freq_lo, freq_hi, vmin, vmax) {
+
     ColSch_initColorSchemeComponent(vmin, vmax);
-    ColSch_initColorSchemeGUI(vmin, vmax);
+    ColSch_initColorSchemeGUI(vmin, vmax, drawCanvas);
+
     var data = $.parseJSON(matrix_data);
     var dimensions = $.parseJSON(matrix_shape);
     var n = dimensions[0];
     var m = dimensions[1];
-    var canvas = d3.select("canvas")
+    WaveletSpect.data = data;
+    WaveletSpect.n = n;
+    WaveletSpect.m = m;
+    WaveletSpect.vmin = vmin;
+    WaveletSpect.vmax = vmax;
+
+    drawCanvas();
+
+    var canvas = d3.select("canvas");
+    var context = canvas.node().getContext("2d");
+    var cWidth = context.canvas.clientWidth;
+    var cHeight = context.canvas.clientHeight;
+    var svgContainer = d3.select("#svg-container");
+    var xAxisScale = d3.scale.linear()
+        .domain([start_time, end_time])
+        .range([0, cWidth]);
+
+    var xAxis = d3.svg.axis()
+        .orient("bot")
+        .scale(xAxisScale);
+    var xAxisGroup = svgContainer.append("g")
+        .attr("transform", "translate(35, " + cHeight + ")");
+
+    var yAxisScale = d3.scale.linear()
+        .domain([freq_lo, freq_hi])
+        .range([cHeight, 0]);
+    var yAxis = d3.svg.axis()
+        .scale(yAxisScale)
+        .orient("left")
+        .ticks(5);
+    var yAxisGroup = svgContainer.append("g")
+        .attr("transform", "translate(35,0)");
+
+    WaveletSpect.xAxisScale = xAxisScale;
+    WaveletSpect.yAxisScale = yAxisScale;
+    WaveletSpect.xAxis = xAxis;
+    WaveletSpect.yAxis = yAxis;
+    WaveletSpect.xAxisGroup = xAxisGroup;
+    WaveletSpect.yAxisGroup = yAxisGroup;
+
+    drawAxis(start_time, end_time, freq_lo, freq_hi);
+}
+
+function drawCanvas() {
+    var data = WaveletSpect.data;
+    var n = WaveletSpect.n;
+    var m = WaveletSpect.m;
+    var vmin = WaveletSpect.vmin;
+    var vmax = WaveletSpect.vmax;
+
+    var canvas = d3.select("#main-canvas")
         .attr("width", m)
         .attr("height", n);
     var context = canvas.node().getContext("2d"),
@@ -32,34 +95,8 @@ function wavelet_spectrogram_view(matrix_data, matrix_shape, start_time, end_tim
             image.data[l + 3] = 255;
         }
     }
-
-    var cWidth = context.canvas.clientWidth;
-    var cHeight = context.canvas.clientHeight;
-    var svgContainer = d3.select("#svg-container");
-    var xAxisScale = d3.scale.linear()
-        .domain([start_time, end_time])
-
-        .range([0, cWidth]);
-
-    var xAxis = d3.svg.axis()
-        .orient("bot")
-        .scale(xAxisScale);
-    var xAxisGroup = svgContainer.append("g")
-        .attr("transform", "translate(35, " + cHeight + ")")
-        .call(xAxis);
-
-
-    var yAxisScale = d3.scale.linear()
-        .domain([freq_lo, freq_hi])
-        .range([cHeight, 0]);
-    var yAxis = d3.svg.axis()
-        .scale(yAxisScale)
-        .orient("left")
-        .ticks(5);
-    var yAxisGroup = svgContainer.append("g")
-        .attr("transform", "translate(35,0)")
-        .call(yAxis);
     context.putImageData(image, 0, 0);
+    updateLegend(vmin, vmax);
 }
 
 function updateLegend(minColor, maxColor) {
@@ -69,4 +106,26 @@ function updateLegend(minColor, maxColor) {
     tableContainer = d3.select("#table-colorWeightsLegend");
     ColSch_updateLegendColors(legendContainer.node(), legendHeight * 95 / 100);
     ColSch_updateLegendLabels(tableContainer.node(), minColor, maxColor, "95%");
+}
+
+function drawAxis() {
+    var canvas = d3.select("#main-canvas");
+    var context = canvas.node().getContext("2d");
+    var cWidth = context.canvas.clientWidth;
+    var cHeight = context.canvas.clientHeight;
+
+    var xAxisScale = WaveletSpect.xAxisScale;
+    var yAxisScale = WaveletSpect.yAxisScale;
+    xAxisScale.range([0, cWidth]);
+    yAxisScale.range([cHeight, 0]);
+
+    var xAxis = WaveletSpect.xAxis.scale(xAxisScale);
+    WaveletSpect.xAxisGroup
+        .attr("transform", "translate(35, " + cHeight + ")")
+        .call(xAxis);
+
+    var yAxis = WaveletSpect.yAxis.scale(yAxisScale);
+    WaveletSpect.yAxisGroup
+        .attr("transform", "translate(35,0)")
+        .call(yAxis);
 }
