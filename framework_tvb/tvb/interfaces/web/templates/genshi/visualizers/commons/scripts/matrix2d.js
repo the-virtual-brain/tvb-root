b642bc1048f0d32f01964f56154fb9699734f85c
liadomide <lia_domide@yahoo.com>
2017-10-12 06:35:57 +0000

    Replace usages of VAR
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@8415 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- framework_tvb/tvb/interfaces/web/templates/genshi/visualizers/commons/scripts/matrix2d.js
+++ framework_tvb/tvb/interfaces/web/templates/genshi/visualizers/commons/scripts/matrix2d.js
@@ -43,10 +43,10 @@ var Matrix2d = {
 
 function matrix2d_init(canvasName, xAxisName, yAxisName, matrix_data, matrix_shape, x_min, x_max, y_min, y_max, vmin, vmax) {
 
-    var dimensions = $.parseJSON(matrix_shape);
-    var n = dimensions[0];
-    var m = dimensions[1];
-    var canvas = d3.select("#main-canvas-2d")
+    const dimensions = $.parseJSON(matrix_shape);
+    const n = dimensions[0];
+    const m = dimensions[1];
+    const canvas = d3.select("#main-canvas-2d")
         .attr("width", m)
         .attr("height", n);
     if (matrix_data) {
@@ -59,39 +59,39 @@ function matrix2d_init(canvasName, xAxisName, yAxisName, matrix_data, matrix_sha
     Matrix2d.m = m;
     Matrix2d.canvas = canvas;
 
-    var context = canvas.node().getContext("2d");
-    var cHeight = context.canvas.clientHeight;
-    var xLabelHeight = cHeight + 50;
-    var yLabelHeight = cHeight / 2;
-    var xLabelWidth = context.canvas.clientWidth / 2 + 90;
-    var yLabelWidth = 50;
-    var svgContainer = d3.select("#svg-container");
-    var xAxisLabel = svgContainer.append("text")
+    const context = canvas.node().getContext("2d");
+    const cHeight = context.canvas.clientHeight;
+    const xLabelHeight = cHeight + 50;
+    const yLabelHeight = cHeight / 2;
+    const xLabelWidth = context.canvas.clientWidth / 2 + 90;
+    const yLabelWidth = 50;
+    let svgContainer = d3.select("#svg-container");
+    let xAxisLabel = svgContainer.append("text")
         .attr("text-anchor", "middle")
         .attr("transform", "translate(" + xLabelWidth + ", " + xLabelHeight + ")")
         .text(xAxisName);
-    var canvasTitle = svgContainer.append("text")
+    let canvasTitle = svgContainer.append("text")
         .attr("text-anchor", "middle")
         .attr("transform", "translate(" + xLabelWidth + ",15)")
         .text(canvasName);
-    var yAxisLabel = svgContainer.append("text")
+    let yAxisLabel = svgContainer.append("text")
         .attr("text-anchor", "middle")
         .attr("transform", "translate(" + yLabelWidth + ", " + yLabelHeight + ")rotate(-90)")
         .text(yAxisName);
-    var xAxisScale = d3.scale.linear()
+    let xAxisScale = d3.scale.linear()
         .domain([x_min, x_max]);
-    var xAxis = d3.svg.axis()
+    let xAxis = d3.svg.axis()
         .orient("bot")
         .scale(xAxisScale);
-    var xAxisGroup = svgContainer.append("g")
+    let xAxisGroup = svgContainer.append("g")
         .attr("transform", "translate(90, " + cHeight + ")");
-    var yAxisScale = d3.scale.linear()
+    let yAxisScale = d3.scale.linear()
         .domain([y_min, y_max]);
-    var yAxis = d3.svg.axis()
+    let yAxis = d3.svg.axis()
         .scale(yAxisScale)
         .orient("left")
         .ticks(5);
-    var yAxisGroup = svgContainer.append("g")
+    let yAxisGroup = svgContainer.append("g")
         .attr("transform", "translate(90,0)");
 
     Matrix2d.xAxisScale = xAxisScale;
@@ -112,35 +112,29 @@ function matrix2d_init(canvasName, xAxisName, yAxisName, matrix_data, matrix_sha
 }
 
 function updateLegend2D(minColor, maxColor, viewerName) {
-    var legendContainer, legendHeight, tableContainer;
-    legendContainer = d3.select("#colorWeightsLegend"+viewerName);
+    let legendContainer, legendHeight, tableContainer;
+    legendContainer = d3.select("#colorWeightsLegend" + viewerName);
     legendHeight = legendContainer.node().getBoundingClientRect().height;
-    tableContainer = d3.select("#table-colorWeightsLegend"+viewerName);
+    tableContainer = d3.select("#table-colorWeightsLegend" + viewerName);
     ColSch_updateLegendColors(legendContainer.node(), legendHeight * 95 / 100);
     ColSch_updateLegendLabels(tableContainer.node(), minColor, maxColor, "95%");
 }
 
 function drawCanvas() {
 
-    var data = Matrix2d.data;
-    var n = Matrix2d.n;
-    var m = Matrix2d.m;
-    var vmin = Matrix2d.vmin;
-    var vmax = Matrix2d.vmax;
-    var canvas = Matrix2d.canvas;
-    canvas.attr("width", m)
-        .attr("height", n);
-    var context = canvas.node().getContext("2d"),
-        image = context.createImageData(m, n);
-    for (var i = n - 1; i >= 0; i--) {
-        for (var j = 0; j < m; j++) {
-            var k = m * i + j;
-            var l = (m * (n - i - 1) + j) * 4;
-            if (data[k] > vmax)
-                data[k] = vmax;
-            if (data[k] < vmin)
-                data[k] = vmin;
-            var c = ColSch_getColor(data[k]);
+    Matrix2d.canvas.attr("width", Matrix2d.m)
+        .attr("height", Matrix2d.n);
+    const context = Matrix2d.canvas.node().getContext("2d");
+    let image = context.createImageData(Matrix2d.m, Matrix2d.n);
+    for (let i = Matrix2d.n - 1; i >= 0; i--) {
+        for (let j = 0; j < Matrix2d.m; j++) {
+            const k = Matrix2d.m * i + j;
+            const l = (Matrix2d.m * (Matrix2d.n - i - 1) + j) * 4;
+            if (Matrix2d.data[k] > Matrix2d.vmax)
+                Matrix2d.data[k] = Matrix2d.vmax;
+            if (Matrix2d.data[k] < Matrix2d.vmin)
+                Matrix2d.data[k] = Matrix2d.vmin;
+            const c = ColSch_getColor(Matrix2d.data[k]);
             image.data[l] = c[0] * 255;
             image.data[l + 1] = c[1] * 255;
             image.data[l + 2] = c[2] * 255;
@@ -148,68 +142,70 @@ function drawCanvas() {
         }
     }
     context.putImageData(image, 0, 0);
-    updateLegend2D(vmin, vmax, Matrix2d.viewerType);
+    updateLegend2D(Matrix2d.vmin, Matrix2d.vmax, Matrix2d.viewerType);
 }
 
 function drawAxis() {
-    var canvas = Matrix2d.canvas;
+    const canvas = Matrix2d.canvas;
     if (canvas === null) {
         console.warn("We won't draw axes because canvas has not yet been drawn!");
         return;
     }
-    var context = canvas.node().getContext("2d");
-    var cWidth = context.canvas.clientWidth;
-    var cHeight = context.canvas.clientHeight;
-    var canvasParent=document.getElementById("canvasParent");
-    var xOffset=Math.floor((canvasParent.clientWidth*8)/100);
-    var yOffset=Math.floor((canvasParent.clientHeight*6)/100);
-    var xLabelHeight = cHeight + 2.5*yOffset;
-    var yLabelHeight = cHeight / 2 + yOffset;
-    var xLabelWidth = context.canvas.clientWidth / 2 + xOffset;
-    var yLabelWidth = xOffset/2;
+    const context = canvas.node().getContext("2d");
+    const cWidth = context.canvas.clientWidth;
+    const cHeight = context.canvas.clientHeight;
+    const canvasParent = document.getElementById("canvasParent");
+    const xOffset = Math.floor((canvasParent.clientWidth * 8) / 100);
+    const yOffset = Math.floor((canvasParent.clientHeight * 6) / 100);
+    const xLabelHeight = cHeight + 2.5 * yOffset;
+    const yLabelHeight = cHeight / 2 + yOffset;
+    const xLabelWidth = context.canvas.clientWidth / 2 + xOffset;
+    const yLabelWidth = xOffset / 2;
 
-    var xAxisScale = Matrix2d.xAxisScale;
-    var yAxisScale = Matrix2d.yAxisScale;
+    let xAxisScale = Matrix2d.xAxisScale;
+    let yAxisScale = Matrix2d.yAxisScale;
     xAxisScale.range([0, cWidth]);
     yAxisScale.range([cHeight, 0]);
 
-    var xAxis = Matrix2d.xAxis.scale(xAxisScale);
+    let xAxis = Matrix2d.xAxis.scale(xAxisScale);
     Matrix2d.xAxisGroup
-        .attr("transform", "translate("+xOffset+", " + (cHeight + yOffset) + ")")
+        .attr("transform", "translate(" + xOffset + ", " + (cHeight + yOffset) + ")")
         .call(xAxis);
 
-    var yAxis = Matrix2d.yAxis.scale(yAxisScale);
+    let yAxis = Matrix2d.yAxis.scale(yAxisScale);
     Matrix2d.yAxisGroup
-        .attr("transform", "translate("+xOffset+","+yOffset+")")
+        .attr("transform", "translate(" + xOffset + "," + yOffset + ")")
         .call(yAxis);
 
     Matrix2d.xAxisLabel
         .attr("transform", "translate(" + xLabelWidth + ", " + xLabelHeight + ")");
     Matrix2d.canvasTitle
-        .attr("transform", "translate(" + xLabelWidth + ","+yOffset/2+")");
+        .attr("transform", "translate(" + xLabelWidth + "," + yOffset / 2 + ")");
     Matrix2d.yAxisLabel
         .attr("transform", "translate(" + yLabelWidth + ", " + yLabelHeight + ")rotate(-90)")
 }
 
 function interpolateMatrix(cWidth, cHeight) {
-    var dataMatrix = [];
-    for (var i = 0; i < Matrix2d.n; i++) {
+    let i;
+    let j;
+    let dataMatrix = [];
+    for (i = 0; i < Matrix2d.n; i++) {
         dataMatrix[i] = [];
-        for (var j = 0; j < Matrix2d.m; j++) {
+        for (j = 0; j < Matrix2d.m; j++) {
             dataMatrix[i][j] = undefined;
         }
     }
-    var oldMatrix = Matrix2d.data;
+    const oldMatrix = Matrix2d.data;
     for (i = 0; i < Matrix2d.n; i++)
         for (j = 0; j < Matrix2d.m; j++)
             dataMatrix[i][j] = oldMatrix[i * Matrix2d.m + j];
-    var ratioC = Math.floor((cWidth / 10) / Matrix2d.m);
-    var ratioL = Math.floor((cHeight / 10) / Matrix2d.n);
-    var spaceC = Math.floor((Matrix2d.m * ratioC) / (Matrix2d.m - 1));
-    var spaceL = Math.floor((Matrix2d.n * ratioL) / (Matrix2d.n - 1));
-    var n = (spaceL) * (Matrix2d.n - 1) + Matrix2d.n;
-    var m = (spaceC) * (Matrix2d.m - 1) + Matrix2d.m;
-    var newMatrix = [];
+    const ratioC = Math.floor((cWidth / 10) / Matrix2d.m);
+    const ratioL = Math.floor((cHeight / 10) / Matrix2d.n);
+    const spaceC = Math.floor((Matrix2d.m * ratioC) / (Matrix2d.m - 1));
+    const spaceL = Math.floor((Matrix2d.n * ratioL) / (Matrix2d.n - 1));
+    const n = (spaceL) * (Matrix2d.n - 1) + Matrix2d.n;
+    const m = (spaceC) * (Matrix2d.m - 1) + Matrix2d.m;
+    let newMatrix = [];
     for (i = 0; i < n; i++) {
         newMatrix[i] = [];
         for (j = 0; j < m; j++) {
@@ -217,26 +213,26 @@ function interpolateMatrix(cWidth, cHeight) {
         }
     }
     for (i = 0; i < Matrix2d.n; i++) {
-        var line = i * spaceL;
-        var col = 0;
+        const line = i * spaceL;
+        let col = 0;
         newMatrix[line][col] = dataMatrix[i][0];
-        for (var k = 1; k < Matrix2d.m; k++) {
+        for (let k = 1; k < Matrix2d.m; k++) {
             col += spaceC;
             newMatrix[line][col] = dataMatrix[i][k];
         }
     }
     for (j = 0; j < Matrix2d.m; j++) {
-        var column = [];
+        let column = [];
         for (i = 0; i < Matrix2d.n; i++) {
             column.push(dataMatrix[i][j])
         }
-        interpolatedColumn = interpolateArray(column, n);
+        const interpolatedColumn = interpolateArray(column, n);
         for (i = 0; i < n; i++) {
             newMatrix[i][j * spaceC] = interpolatedColumn[i];
         }
     }
     for (i = 0; i < n; i++) {
-        var intermediateLine = newMatrix[i].filter(function (element) {
+        let intermediateLine = newMatrix[i].filter(function (element) {
             return element !== undefined;
         });
         newMatrix[i] = interpolateArray(intermediateLine, m);
@@ -251,14 +247,14 @@ function linearInterpolate(before, after, atPoint) {
 }
 
 function interpolateArray(data, fitCount) {
-    var newData = [];
-    var springFactor = (data.length - 1) / (fitCount - 1);
+    let newData = [];
+    const springFactor = (data.length - 1) / (fitCount - 1);
     newData[0] = data[0]; // for new allocation
-    for (var i = 1; i < fitCount - 1; i++) {
-        var tmp = i * springFactor;
-        var before = Math.floor(tmp).toFixed();
-        var after = Math.ceil(tmp).toFixed();
-        var atPoint = tmp - before;
+    for (let i = 1; i < fitCount - 1; i++) {
+        const tmp = i * springFactor;
+        const before = Math.floor(tmp).toFixed();
+        const after = Math.ceil(tmp).toFixed();
+        const atPoint = tmp - before;
         newData[i] = this.linearInterpolate(data[before], data[after], atPoint);
     }
     newData[fitCount - 1] = data[data.length - 1]; // for new allocation
@@ -266,11 +262,11 @@ function interpolateArray(data, fitCount) {
 }
 
 function matrixToArray(matrixData) {
-    var n = matrixData.length;
-    var m = matrixData[0].length;
-    var array = [];
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
+    const n = matrixData.length;
+    const m = matrixData[0].length;
+    let array = [];
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
             array[i * m + j] = matrixData[i][j];
         }
     }
