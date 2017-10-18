bb54659516a7ca0292c2e04aeb241a242044ae6f
liadomide <lia_domide@yahoo.com>
2017-10-18 07:01:45 +0000

    Merge changes from SVN
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@8480 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- externals/geodesic_distance/setup.py
+++ externals/geodesic_distance/setup.py
@@ -48,14 +48,13 @@ from Cython.Distutils import build_ext
 
 GEODESIC_NAME = "gdist"
 
-GEODESIC_MODULE = [setuptools.Extension(name=GEODESIC_NAME,  # Name of extension
-                                        sources=["gdist.pyx"],  # Filename of Cython source
-                                        language="c++",  # Cython create C++ source
-                                        define_macros=[
+GEODESIC_MODULE = [setuptools.Extension(name=GEODESIC_NAME,          # Name of extension
+                             sources=["gdist.pyx"], # Filename of Cython source
+                             language="c++",       # Cython create C++ sourcedefine_macros=[
                                             ('NDEBUG', 1)])]  # Disable assertions; one is failing geodesic_mesh.h:405
 
-INCLUDE_DIRS = [numpy.get_include(),  # NumPy dtypes
-                "geodesic_library"]  # geodesic distance, C++ library.
+INCLUDE_DIRS = [numpy.get_include(), # NumPy dtypes
+                       "geodesic_library"]  # geodesic distance, C++ library.
 
 TEAM = "Danil Kirsanov, Gaurav Malhotra and Stuart Knock"
 
