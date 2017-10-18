7efad763d6a106935ad306c9058336c9aeaf1529
liadomide <lia_domide@yahoo.com>
2017-10-18 11:31:15 +0000

    TVB-2114 Fix generation of Pypi files for gdist also
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@8483 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- package_for_pip.sh
+++ package_for_pip.sh
@@ -1,17 +1,34 @@
 #!/usr/bin/env bash
+
 rm -Rf dist
 mkdir dist
 
-for pipPackage in "tvb_data" "framework_tvb" "scientific_library"; do
+for pipPackage in "tvb_data" "framework_tvb" "scientific_library" "externals/geodesic_distance"; do
+
+    echo "============================="
     echo "Packing: " $pipPackage
-    echo "========================"
+    echo "============================="
+
     cd $pipPackage
     python setup.py sdist
     python setup.py bdist_wheel
-    mv dist/* ../dist/
+
+    if [ -d "../dist/" ]
+    then
+        mv dist/* ../dist/
+    else
+        mv dist/* ../../dist/
+    fi
+
     rm -R dist
     rm -R build
-    cd ..
+
+    if [ -d "../dist/" ]
+    then
+        cd ..
+    else
+        cd ../..
+    fi
 done
 
 ## After manual check, do the actual deploy on Pypi
