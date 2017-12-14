615eb290fc6eb45bc17998565ecdb91c85c9dbe8
liadomide <lia_domide@yahoo.com>
2017-12-14 12:05:11 +0000

    TVB-1073 Add example of running coverage and excluding tests
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@8611 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb_bin/run_tests.bat
+++ tvb_bin/run_tests.bat
@@ -14,6 +14,10 @@ pytest --pyargs tvb.tests.framework --junitxml=TEST_OUTPUT\TEST-RESULTS.xml > TE
 echo 'Starting TVB Scientific Library tests'
 pytest --pyargs tvb.tests.library --junitxml=TEST_OUTPUT\TEST-LIBRARY-RESULTS.xml > TEST_OUTPUT\TEST-LIBRARY.out 2>&1
 
+REM echo 'Run Coverage'
+REM cd ..\scientific_library
+REM py.test --cov-config .coveragerc --cov=tvb tvb\tests --cov-branch --cov-report xml:TEST_OUTPUT\coverage_library.xml --junitxml=TEST_OUTPUT\TEST-LIBRARY-RESULTS.xml  1>TEST_OUTPUT\TEST-LIBRARY.out 2>&1
+
 echo 'Tests done.'
 
 exit 0
