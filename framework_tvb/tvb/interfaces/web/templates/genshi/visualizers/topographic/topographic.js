9ab2db290abd5d1b399a4407af279d7e76230fbf
liadomide <lia_domide@yahoo.com>
2017-12-19 14:06:53 +0000

    TVB-2319 Review Topo JS code
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@8642 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- framework_tvb/tvb/interfaces/web/templates/genshi/visualizers/topographic/topographic.js
+++ framework_tvb/tvb/interfaces/web/templates/genshi/visualizers/topographic/topographic.js
@@ -18,37 +18,29 @@
  **/
 
 
-var Topographic = {
-    topographic_container:[],
-    vmin: null,
-    vmax: null,
+var TOPO_Data = {
+    headsData:[],
+    valueMin: null,
+    valueMax: null,
 };
 
-function topographic_init(matrix_data, matrix_shape, vmin, vmax, index) {
-
-    var dimensions = $.parseJSON(matrix_shape);
-    var n = dimensions[0];
-    var m = dimensions[1];
-    var canvas = d3.select("#canvas-" + (index+1));
-
-    var Topographic_element ={
-        canvas: null,
-        data: null,
-        n: null,
-        m: null
-    }
+function TOPO_Init(matrix_data, matrix_shape, vmin, vmax, index) {
 
     if (matrix_data) {
-        Topographic_element.data = ($.parseJSON(matrix_data));
-        Topographic_element.n = n;
-        Topographic_element.m = m;
+        var dimensions = $.parseJSON(matrix_shape);
+        var canvas = d3.select("#canvas-" + (index+1));
+        var head = {
+            canvas: canvas,
+            data: ($.parseJSON(matrix_data)),
+            n: dimensions[0],
+            m: dimensions[1]
+        }
+        TOPO_Data.headsData.push(head);
     }
-    Topographic_element.canvas=canvas;
-    Topographic.topographic_container.push(Topographic_element);
 
-    if(index==0){
-        Topographic.vmin = vmin;
-        Topographic.vmax = vmax;
+    if(index == 0) {
+        TOPO_Data.valueMin = vmin;
+        TOPO_Data.valueMax = vmax;
         ColSch_initColorSchemeGUI(vmin, vmax, drawAllCanvases);
     }
 
@@ -64,23 +56,22 @@ function topographic_init(matrix_data, matrix_shape, vmin, vmax, index) {
 }
 
 function drawAllCanvases(){
-    for(var i =0; i<Topographic.topographic_container.length;i++){
+    for(var i =0; i < TOPO_Data.headsData.length; i++){
         drawCanvas(i);
     }
 }
 
 function drawCanvas(index) {
-    var vmin = Topographic.vmin;
-    var vmax = Topographic.vmax;
-    var n = Topographic.topographic_container[index].n;
-    var m = Topographic.topographic_container[index].m;
-    var data = Topographic.topographic_container[index].data;
-    var canvas = Topographic.topographic_container[index].canvas;
+    var vmin = TOPO_Data.valueMin;
+    var vmax = TOPO_Data.valueMax;
+    var n = TOPO_Data.headsData[index].n;
+    var m = TOPO_Data.headsData[index].m;
+    var data = TOPO_Data.headsData[index].data;
+    var canvas = TOPO_Data.headsData[index].canvas;
 
-    canvas.attr("width", m)
-        .attr("height", n);
-    var context = canvas.node().getContext("2d"),
-        image = context.createImageData(m, n);
+    canvas.attr("width", m).attr("height", n);
+    var context = canvas.node().getContext("2d");
+    var image = context.createImageData(m, n);
     for (var i = n - 1; i >= 0; i--) {
         for (var j = 0; j < m; j++) {
             var k = m * i + j;
@@ -89,20 +80,28 @@ function drawCanvas(index) {
             image.data[l] = c[0] * 255;
             image.data[l + 1] = c[1] * 255;
             image.data[l + 2] = c[2] * 255;
-            if (data[k] === vmin - 1)
+            if (data[k] === vmin - 1) {
                 image.data[l + 3] = 0;
-            else
+            } else {
                 image.data[l + 3] = 255;
+            }
         }
     }
     context.putImageData(image, 0, 0);
-    updateLegend2D(vmin, vmax);
+
+    // updateLegend2D
+    var legendContainer = d3.select("#colorWeightsLegend");
+    var legendHeight = legendContainer.node().getBoundingClientRect().height;
+    var tableContainer = d3.select("#table-colorWeightsLegend");
+    ColSch_updateLegendColors(legendContainer.node(), legendHeight * 95 / 100);
+    ColSch_updateLegendLabels(tableContainer.node(), vmin, vmax, "95%");
+
     drawContour(data, index);
 }
 
-function drawContours() {
-    for (var i = 0; i < Topographic.topographic_container.length; i++) {
-        drawContour(Topographic.topographic_container[i].data, i);
+function TOPO_DrawContours() {
+    for (var i = 0; i < TOPO_Data.headsData.length; i++) {
+        drawContour(TOPO_Data.headsData[i].data, i);
     }
 }
 
@@ -113,10 +112,10 @@ function drawContour(data, index) {
         'height': cw + 'px'
     });
 
-    var n = Topographic.topographic_container[index].n;
-    var m = Topographic.topographic_container[index].m;
-    var vmin = Topographic.vmin;
-    var vmax = Topographic.vmax;
+    var n = TOPO_Data.headsData[index].n;
+    var m = TOPO_Data.headsData[index].m;
+    var vmin = TOPO_Data.valueMin;
+    var vmax = TOPO_Data.valueMax;
     var svg = d3.select("#svg-container-" + (index+1));
     var width = svg["0"]["0"].clientWidth;
     var height = svg["0"]["0"].clientHeight;
@@ -135,17 +134,6 @@ function drawContour(data, index) {
         .attr("overflow", "hidden")
         .attr("transform", "translate(" + width * (1 - scale) / 2 + "," + (height * 0.15) + ")")
         .attr("fill", "none");
-
-    // TODO Add controls and ajax calls
-}
-
-function updateLegend2D(minColor, maxColor) {
-    var legendContainer, legendHeight, tableContainer;
-    legendContainer = d3.select("#colorWeightsLegend");
-    legendHeight = legendContainer.node().getBoundingClientRect().height;
-    tableContainer = d3.select("#table-colorWeightsLegend");
-    ColSch_updateLegendColors(legendContainer.node(), legendHeight * 95 / 100);
-    ColSch_updateLegendLabels(tableContainer.node(), minColor, maxColor, "95%");
 }
 
 function flipArray(array, n, m) {
