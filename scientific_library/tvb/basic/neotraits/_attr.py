4f97aa2e307fb126470d5aa54c8a5a94e0a2d1b2
liadomide <lia_domide@yahoo.com>
2019-10-15 21:56:28 +0000

    TVB-2495 Add Copyright and citation text to neotraits modules.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@9062 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- scientific_library/tvb/basic/neotraits/_attr.py
+++ scientific_library/tvb/basic/neotraits/_attr.py
@@ -1,3 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+#
+#  TheVirtualBrain-Scientific Package. This package holds all simulators, and
+# analysers necessary to run brain-simulations. You can use it stand alone or
+# in conjunction with TheVirtualBrain-Framework Package. See content of the
+# documentation-folder for more details. See also http://www.thevirtualbrain.org
+#
+# (c) 2012-2017, Baycrest Centre for Geriatric Care ("Baycrest") and others
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this
+# program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#   CITATION:
+# When using The Virtual Brain for scientific publications, please cite it as follows:
+#
+#   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
+#   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
+#       The Virtual Brain: a simulator of primate brain network dynamics.
+#   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
+#
+#
+
 """
 This private module implements concrete declarative attributes
 """
@@ -319,7 +349,7 @@ class Int(_Number):
     """
 
     def __init__(
-        self, field_type=int, default=0, doc='', label='', required=True, final=False, choices=None
+            self, field_type=int, default=0, doc='', label='', required=True, final=False, choices=None
     ):
         super(_Number, self).__init__(
             field_type=field_type,
@@ -354,7 +384,7 @@ class Float(_Number):
     """
 
     def __init__(
-        self, field_type=float, default=0, doc='', label='', required=True, final=False, choices=None
+            self, field_type=float, default=0, doc='', label='', required=True, final=False, choices=None
     ):
         super(_Number, self).__init__(
             field_type=field_type,
@@ -402,15 +432,15 @@ class NArray(Attr):
     """
 
     def __init__(
-        self,
-        default=None,
-        required=True,
-        doc='',
-        label='',
-        dtype=numpy.float,
-        shape=None,
-        dim_names=(),
-        domain=None,
+            self,
+            default=None,
+            required=True,
+            doc='',
+            label='',
+            dtype=numpy.float,
+            shape=None,
+            dim_names=(),
+            domain=None,
     ):
         # type: (numpy.ndarray, bool, str, str, typing.Union[numpy.dtype, type, str], typing.Optional[typing.Tuple[Dim, ...]], typing.Tuple[str, ...], typing.Container) -> None
         """
