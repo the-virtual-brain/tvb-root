12ef3e841f34dd637948a90fef705423182e5575
liadomide <lia_domide@yahoo.com>
2019-10-15 21:53:12 +0000

    neotraits: add summary info
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@8995 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- scientific_library/tvb/basic/neotraits/api.py
+++ scientific_library/tvb/basic/neotraits/api.py
@@ -3,5 +3,5 @@ The public api of the neotraits package.
 """
 
 from ._core import HasTraits, traitproperty
-from .info import narray_describe
+from .info import narray_describe, narray_summary_info
 from ._attr import Attr, Int, Float, NArray, Const, List, Range, LinspaceRange
--- scientific_library/tvb/basic/neotraits/info.py
+++ scientific_library/tvb/basic/neotraits/info.py
@@ -5,33 +5,38 @@ Some of these functions are here so that they won't clutter the core trait imple
 """
 
 import numpy
+import typing
 
 
 def auto_docstring(cls):
-    # type: (HasTraits) -> str
     """ generate a docstring for the new class in which the Attrs are documented """
+    header = 'Traited class [{}.{}]'.format(cls.__module__, cls.__name__)
+
     doc = [
-        'Traited class [{}.{}]'.format(cls.__module__, cls.__name__),
+        header,
+        len(header) * '^',
         '',
-        '  Attributes declared',
-        '  -------------------',
+        'Attributes declared',
+        '"""""""""""""""""""',
         ''
     ]
-
+    # a rst definition list for all attributes
     for attr_name in cls.declarative_attrs:
         attr = getattr(cls, attr_name)
         # the standard repr of the attribute
-        doc.append('  {} : {}'.format(attr_name, str(attr)))
+        doc.append('{} : {}'.format(attr_name, str(attr)))
         # and now the doc property
         for line in attr.doc.splitlines():
             doc.append('    ' + line.lstrip())
+        doc.append('')
 
-    doc.extend([
-        '',
-        '  Properties declared',
-        '  -------------------',
-        ''
-    ])
+    if cls.declarative_props:
+        doc.extend([
+            '',
+            'Properties declared',
+            '"""""""""""""""""""',
+            ''
+        ])
 
     for prop_name in cls.declarative_props:
         prop = getattr(cls, prop_name)
@@ -52,40 +57,57 @@ def auto_docstring(cls):
         return doc
 
 
-def narray_describe(ar):
+def narray_summary_info(ar, ar_name='', omit_shape=False):
+    # type: (numpy.ndarray, str, bool) -> typing.Dict[str, str]
+    """
+    A 2 column table represented as a dict of str->str
+    """
     if ar is None:
-        return 'None'
-    ret = [
-        'shape    {}'.format(ar.shape),
-        'dtype    {}'.format(ar.dtype),
-    ]
+        return {'is None': 'True'}
+
+    ret = {}
+    if not omit_shape:
+        ret.update({
+            'shape': str(ar.shape),
+            'dtype': str(ar.dtype),
+        })
 
     if ar.size == 0:
-        ret.append('is empty')
-        return '\n'.join(ret)
+        ret['is empty'] = 'True'
+        return ret
 
     if ar.dtype.kind in 'iufc':
-        ret += [
-            'has NaN  {}'.format(numpy.isnan(ar).any()),
-            'maximum  {}'.format(ar.max()),
-            'minimum  {}'.format(ar.min())
-        ]
+        has_nan = numpy.isnan(ar).any()
+        if has_nan:
+            ret['has NaN'] = 'True'
+        ret['[min, median, max]'] = '[{:g}, {:g}, {:g}]'.format(ar.min(), numpy.median(ar), ar.max())
+
+    if ar_name:
+        return {ar_name + ' ' + k: v for k, v in ret.iteritems()}
+    else:
+        return ret
+
+
+def narray_describe(ar):
+    # type: (numpy.ndarray) -> str
+    summary = narray_summary_info(ar)
+    ret = []
+    for k in sorted(summary):
+        ret.append('{:<12}{}'.format(k, summary[k]))
     return '\n'.join(ret)
 
 
+# these are here and not on HasTraits just so that that class is not
+# complicated by irrelevant string formatting
+
 def trait_object_str(self):
     cls = type(self)
-    result = ['{} ('.format(self.__class__.__name__)]
-    for aname in cls.declarative_attrs:
-        attr_field = getattr(self, aname)
-        if isinstance(attr_field, numpy.ndarray):
-            attr_repr = ['array'] + narray_describe(attr_field).splitlines()
-        else:
-            # str would be pretty. but recursive types will stack overflow then
-            attr_repr = repr(attr_field).splitlines()
-        attr_repr = attr_repr[:1] + ['      ' + s for s in attr_repr[1:]]
-        attr_repr = '\n'.join(attr_repr)
-        result.append('  {} = {},'.format(aname, attr_repr))
+    summary = self.summary_info()
+    result = ['{} ('.format(cls.__name__)]
+    maxlenk = max(len(k) for k in summary)
+
+    for k in sorted(summary):
+        result.append('  {:.<{}} {}'.format(k + ' ', maxlenk, summary[k]))
     result.append(')')
     return '\n'.join(result)
 
@@ -94,23 +116,16 @@ def trait_object_repr_html(self):
     cls = type(self)
     result = [
         '<table>',
-        '<h3>{}</h3>'.format(self.__class__.__name__),
-        '<thead><tr><th>attribute</th><th style="text-align:left;width:40%">value</th><th>about</th></tr></thead>',
+        '<h3>{}</h3>'.format(cls.__name__),
+        '<thead><tr><th></th><th style="text-align:left;width:40%">value</th></tr></thead>',
         '<tbody>']
 
-    for aname in cls.declarative_attrs:
-        attr_field = getattr(self, aname)
-        attr_doc = getattr(cls, aname).doc
-        row_fmt = '<tr><td>{}</td><td style="text-align:left;"><pre>{}</pre></td><td style="text-align:left;">{}</td>'
-        if isinstance(attr_field, numpy.ndarray):
-            attr_repr = narray_describe(attr_field).splitlines()
-        else:
-            attr_repr = repr(attr_field).splitlines()
-        attr_repr = '\n'.join(attr_repr)
-        result.append(row_fmt.format(aname, attr_repr, attr_doc))
+    summary = self.summary_info()
+
+    for k in sorted(summary):
+        row_fmt = '<tr><td>{}</td><td style="text-align:left;"><pre>{}</pre></td>'
+        result.append(row_fmt.format(k, summary[k]))
 
     result += ['</tbody></table>']
 
     return '\n'.join(result)
-
-
