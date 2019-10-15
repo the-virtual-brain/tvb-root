8b95c652c3ec7a1bd9f11ff054c50646d7733ad6
liadomide <lia_domide@yahoo.com>
2019-10-15 21:50:40 +0000

    Small fix after variables_of_interest type change
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@8934 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- scientific_library/tvb/simulator/simulator.py
+++ scientific_library/tvb/simulator/simulator.py
@@ -642,11 +642,11 @@ class Simulator(core.Type):
 
             else:
                 stock_shape = (monitor.hrf_length * monitor._stock_sample_rate,
-                               self.model.variables_of_interest.shape[0],
+                               len(self.model.variables_of_interest),
                                number_of_nodes,
                                self.model.number_of_modes)
                 interim_stock_shape = (1.0 / (2.0 ** -2 * self.integrator.dt),
-                                       self.model.variables_of_interest.shape[0],
+                                       len(self.model.variables_of_interest),
                                        number_of_nodes,
                                        self.model.number_of_modes)
                 memreq += numpy.prod(stock_shape) * bits_64
