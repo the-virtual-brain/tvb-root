735e575fd0eaab7f7f5da0b9fb25588d795e503a
liadomide <lia_domide@yahoo.com>
2019-10-16 09:36:22 +0000

    Testing store/load for simple datatypes.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@9135 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

---
+++ framework_tvb/tvb/tests/framework/core/neotraits/datatypes.py
@@ -0,0 +1,88 @@
+import numpy
+from tvb.datatypes.connectivity import Connectivity
+from tvb.datatypes.projections import ProjectionMatrix
+from tvb.datatypes.surfaces import Surface, CorticalSurface
+from tvb.datatypes.region_mapping import RegionMapping
+from tvb.datatypes.sensors import Sensors
+from tvb.datatypes.volumes import Volume
+
+connectivity = Connectivity(
+    region_labels=numpy.array(["a", "b"]),
+    weights=numpy.zeros((2, 2)),
+    # undirected=False,
+    tract_lengths=numpy.zeros((2, 2)),
+    centres=numpy.zeros((2, 2)),
+    cortical=numpy.array([[True, True], [True, True]]),
+    hemispheres=numpy.array([[True, True], [True, True]]),
+    orientations=numpy.zeros((2, 2)),
+    areas=numpy.zeros((2, 2)),
+    number_of_regions=2,
+    number_of_connections=4,
+    # parent_connectivity=""
+)
+
+surface = Surface(
+    vertices=numpy.zeros((5, 3)),
+    triangles=numpy.zeros((3, 3), dtype=int),
+    vertex_normals=numpy.zeros((5, 3)),
+    triangle_normals=numpy.zeros((3, 3)),
+    number_of_vertices=5,
+    number_of_triangles=3,
+    edge_mean_length=1.0,
+    edge_min_length=0.0,
+    edge_max_length=2.0,
+    zero_based_triangles=False,
+    split_triangles=numpy.arange(0),
+    number_of_split_slices=1,
+    split_slices=dict(),
+    bi_hemispheric=False,
+    surface_type="surface",
+    valid_for_simulations=True
+)
+
+region_mapping = RegionMapping(
+    array_data=numpy.arange(5),
+    connectivity=connectivity,
+    surface=surface
+)
+
+cortical_surface = CorticalSurface(
+    vertices=numpy.zeros((5, 3)),
+    triangles=numpy.zeros((3, 3), dtype=int),
+    vertex_normals=numpy.zeros((5, 3)),
+    triangle_normals=numpy.zeros((3, 3)),
+    number_of_vertices=5,
+    number_of_triangles=3,
+    edge_mean_length=1.0,
+    edge_min_length=0.0,
+    edge_max_length=2.0,
+    zero_based_triangles=False,
+    split_triangles=numpy.arange(0),
+    number_of_split_slices=1,
+    split_slices=dict(),
+    bi_hemispheric=False,
+    # surface_type="surface",
+    valid_for_simulations=True
+)
+
+sensors = Sensors(
+    sensors_type="SEEG",
+    labels=numpy.array(["s1", "s2", "s3"]),
+    locations=numpy.zeros((3, 3)),
+    number_of_sensors=3,
+    has_orientation=True,
+    orientations=numpy.zeros((3, 3)),
+    usable=numpy.array([True, False, True])
+)
+
+volume = Volume(
+    origin=numpy.zeros((3, 3)),
+    voxel_size=numpy.zeros((3, 3))
+)
+
+projection_matrix = ProjectionMatrix(
+    projection_type="projSEEG",
+    sources=cortical_surface,
+    sensors=sensors,
+    projection_data=numpy.zeros((5, 3))
+)
--- framework_tvb/tvb/tests/framework/core/neotraits/neotraits_h5_test.py
+++ framework_tvb/tvb/tests/framework/core/neotraits/neotraits_h5_test.py
@@ -1,69 +1,21 @@
 import numpy
+from tvb.datatypes.projections import ProjectionMatrix
+from tvb.datatypes.simulation_state import SimulationState
+from tvb.datatypes.structural import StructuralMRI
+from tvb.datatypes.volumes import Volume
+from tvb.core.entities.file.datatypes.projections_h5 import ProjectionMatrixH5
+from tvb.core.entities.file.datatypes.simulation_state_h5 import SimulationStateH5
+from tvb.core.entities.file.datatypes.structural_h5 import StructuralMRIH5
+from tvb.core.entities.file.datatypes.volumes_h5 import VolumeH5
+from .datatypes import connectivity, surface, region_mapping, sensors, volume, projection_matrix
 from tvb.datatypes.region_mapping import RegionMapping
+from tvb.datatypes.sensors import Sensors
 from tvb.core.entities.file.datatypes.connectivity_h5 import ConnectivityH5
 from tvb.datatypes.connectivity import Connectivity
-from tvb.datatypes.surfaces import Surface, CorticalSurface
+from tvb.datatypes.surfaces import Surface
 from tvb.core.entities.file.datatypes.region_mapping_h5 import RegionMappingH5
+from tvb.core.entities.file.datatypes.sensors_h5 import SensorsH5
 from tvb.core.entities.file.datatypes.surface_h5 import SurfaceH5
-
-conn = Connectivity(
-    region_labels=numpy.array(["a", "b"]),
-    weights=numpy.zeros((2, 2)),
-    # undirected=False,
-    tract_lengths=numpy.zeros((2, 2)),
-    centres=numpy.zeros((2, 2)),
-    cortical=numpy.array([[True, True], [True, True]]),
-    hemispheres=numpy.array([[True, True], [True, True]]),
-    orientations=numpy.zeros((2, 2)),
-    areas=numpy.zeros((2, 2)),
-    number_of_regions=2,
-    number_of_connections=4,
-    # parent_connectivity=""
-)
-
-surface = Surface(
-    vertices=numpy.zeros((5, 3)),
-    triangles=numpy.zeros((3, 3), dtype=int),
-    vertex_normals=numpy.zeros((5, 3)),
-    triangle_normals=numpy.zeros((3, 3)),
-    number_of_vertices=5,
-    number_of_triangles=3,
-    edge_mean_length=1.0,
-    edge_min_length=0.0,
-    edge_max_length=2.0,
-    zero_based_triangles=False,
-    split_triangles=numpy.arange(0),
-    number_of_split_slices=1,
-    split_slices=dict(),
-    bi_hemispheric=False,
-    surface_type="surface",
-    valid_for_simulations=True
-)
-
-rm = RegionMapping(
-    array_data=numpy.arange(5),
-    connectivity=conn,
-    surface=surface
-)
-
-cort = CorticalSurface(
-    vertices=numpy.zeros((5, 3)),
-    triangles=numpy.zeros((3, 3), dtype=int),
-    vertex_normals=numpy.zeros((5, 3)),
-    triangle_normals=numpy.zeros((3, 3)),
-    number_of_vertices=5,
-    number_of_triangles=3,
-    edge_mean_length=1.0,
-    edge_min_length=0.0,
-    edge_max_length=2.0,
-    zero_based_triangles=False,
-    split_triangles=numpy.arange(0),
-    number_of_split_slices=1,
-    split_slices=dict(),
-    bi_hemispheric=False,
-    # surface_type="surface",
-    valid_for_simulations=True
-)
 
 
 def test_store_load_configured_surf(tmph5factory):
@@ -112,13 +64,13 @@ def test_stored_conn_load_vertices_slice(tmph5factory):
 
 def test_store_connectivity(tmph5factory):
     conn_h5 = ConnectivityH5(tmph5factory())
-    conn_h5.store(conn)
+    conn_h5.store(connectivity)
     conn_h5.close()
 
 
 def test_store_load_connectivity(tmph5factory):
     conn_h5 = ConnectivityH5(tmph5factory())
-    conn_h5.store(conn)
+    conn_h5.store(connectivity)
     conn_h5.close()
 
     conn_stored = Connectivity()
@@ -170,7 +122,7 @@ def test_store_load_surface(tmph5factory):
 
 def test_store_load_region_mapping(tmph5factory):
     rm_h5 = RegionMappingH5(tmph5factory())
-    rm_h5.store(rm)
+    rm_h5.store(region_mapping)
     rm_h5.close()
 
     rm_stored = RegionMapping()
@@ -180,29 +132,113 @@ def test_store_load_region_mapping(tmph5factory):
 
 
 def test_store_load_complete_region_mapping(tmph5factory):
-    conn_h5 = ConnectivityH5(tmph5factory('Connectivity_{}.h5'.format(conn.gid)))
+    conn_h5 = ConnectivityH5(tmph5factory('Connectivity_{}.h5'.format(connectivity.gid)))
     surf_h5 = SurfaceH5(tmph5factory('Surface_{}.h5'.format(surface.gid)))
-    rm_h5 = RegionMappingH5(tmph5factory('RegionMapping_{}.h5'.format(rm.gid)))
+    rm_h5 = RegionMappingH5(tmph5factory('RegionMapping_{}.h5'.format(region_mapping.gid)))
 
-    conn_h5.store(conn)
+    conn_h5.store(connectivity)
     conn_h5.close()  # use with
 
     surf_h5.store(surface)
     surf_h5.close()
 
-    rm_h5.store(rm)
+    rm_h5.store(region_mapping)
     rm_h5.close()
 
-    # conn_stored = Connectivity()
-    # surf_stored = Surface()
-    # rm_stored = RegionMapping()
-    #
-    # conn_h5.load_into(conn_stored)
-    # surf_h5.load_into(surf_stored)
-    # rm_h5.load_into(rm_stored)
-    # assert rm_stored.connectivity is None
-    # assert rm_stored.surface is None
-    # rm_stored.connectivity = conn_stored
-    # rm_stored.surface = surf_stored
-    # assert rm_stored.connectivity is not None
-    # assert rm_stored.surface is not None
+    conn_stored = Connectivity()
+    surf_stored = Surface()
+    rm_stored = RegionMapping()
+
+    conn_h5.load_into(conn_stored)
+    surf_h5.load_into(surf_stored)
+    rm_h5.load_into(rm_stored)
+    assert rm_stored.connectivity is None
+    assert rm_stored.surface is None
+    rm_stored.connectivity = conn_stored
+    rm_stored.surface = surf_stored
+    assert rm_stored.connectivity is not None
+    assert rm_stored.surface is not None
+
+
+def test_store_load_sensors(tmph5factory):
+    tmp_file = tmph5factory("Sensors_{}.h5".format(sensors.gid))
+    with SensorsH5(tmp_file) as f:
+        f.store(sensors)
+
+    sensors_stored = Sensors()
+    assert sensors_stored.labels is None
+    with SensorsH5(tmp_file) as f:
+        f.load_into(sensors_stored)
+        assert sensors_stored.labels is not None
+
+
+def test_store_load_partial_sensors(tmph5factory):
+    sensors = Sensors(
+        sensors_type="SEEG",
+        labels=numpy.array(["s1", "s2", "s3"]),
+        locations=numpy.zeros((3, 3)),
+        number_of_sensors=3
+    )
+
+    tmp_file = tmph5factory("Sensors_{}.h5".format(sensors.gid))
+    with SensorsH5(tmp_file) as f:
+        f.store(sensors)
+
+    sensors_stored = Sensors()
+    assert sensors_stored.labels is None
+    with SensorsH5(tmp_file) as f:
+        f.load_into(sensors_stored)
+    assert sensors_stored.labels is not None
+
+
+def test_store_load_volume(tmph5factory):
+    tmp_file = tmph5factory("Volume_{}.h5".format(volume.gid))
+
+    with VolumeH5(tmp_file) as f:
+        f.store(volume)
+
+    volume_stored = Volume()
+    assert volume_stored.origin is None
+    with VolumeH5(tmp_file) as f:
+        f.load_into(volume_stored)
+    assert volume_stored.origin is not None
+
+
+def test_store_load_structuralMRI(tmph5factory):
+    structural_mri = StructuralMRI(
+        array_data=numpy.zeros((3, 3)),
+        weighting="T1",
+        volume=volume
+    )
+
+    tmp_file = tmph5factory("StructuralMRI_{}.h5".format(volume.gid))
+
+    with StructuralMRIH5(tmp_file) as f:
+        f.store(structural_mri)
+
+    structural_mri_stored = StructuralMRI()
+    assert structural_mri_stored.array_data is None
+    assert structural_mri_stored.volume is None
+    with StructuralMRIH5(tmp_file) as f:
+        f.load_into(structural_mri_stored)
+    assert structural_mri_stored.array_data.shape == (3, 3)
+    assert structural_mri_stored.volume is None
+
+
+def test_store_load_simulation_state(tmph5factory):
+    simulation_state = SimulationState(
+        history=numpy.arange(4),
+        current_state=numpy.arange(4),
+        current_step=1
+    )
+
+    tmp_file = tmph5factory("SimulationState_{}.h5".format(simulation_state.gid))
+
+    with SimulationStateH5(tmp_file) as f:
+        f.store(simulation_state)
+
+    simulation_state_stored = SimulationState()
+    assert simulation_state_stored.history is None
+    with SimulationStateH5(tmp_file) as f:
+        f.load_into(simulation_state_stored)
+    assert simulation_state_stored.history is not None
