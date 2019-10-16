52380ca17fb4df192c559c96e470f5864081a7ba
liadomide <lia_domide@yahoo.com>
2019-10-16 09:36:36 +0000

    neotraits.h5 add a sparse matrix accessor
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@9142 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- framework_tvb/tvb/core/neotraits/h5.py
+++ framework_tvb/tvb/core/neotraits/h5.py
@@ -1,4 +1,5 @@
 import json
+import scipy.sparse
 import uuid
 
 import typing
@@ -75,10 +76,10 @@ class DataSetMetaData(object):
 
     @classmethod
     def from_dict(cls, dikt):
-        return cls(min=dikt['min'], max=dikt['max'])
+        return cls(min=dikt['Minimum'], max=dikt['Maximum'])
 
     def to_dict(self):
-        return dict(self.__dict__)
+        return {'Minimum': self.min, 'Maximum': self.max}
 
 
 
@@ -109,13 +110,15 @@ class DataSet(Accessor):
     def store(self, data):
         # type: (numpy.ndarray) -> None
         data = self.trait_attribute._validate_set(None, data)
-        if data is not None:
-            self.owner.storage_manager.store_data(self.trait_attribute.field_name, data)
-            # cache some array information
-            self.owner.storage_manager.set_metadata(
-                DataSetMetaData.from_array(data).to_dict(),
-                self.trait_attribute.field_name
-            )
+        if data is None:
+            return
+
+        self.owner.storage_manager.store_data(self.trait_attribute.field_name, data)
+        # cache some array information
+        self.owner.storage_manager.set_metadata(
+            DataSetMetaData.from_array(data).to_dict(),
+            self.trait_attribute.field_name
+        )
 
     def load(self):
         # type: () -> numpy.ndarray
@@ -138,6 +141,117 @@ class DataSet(Accessor):
         """
         meta = self.owner.storage_manager.get_metadata(self.trait_attribute.field_name)
         return DataSetMetaData.from_dict(meta)
+
+
+class SparseMatrixMetaData(DataSetMetaData):
+    """
+    Essential metadata for interpreting a sparse matrix stored in h5
+    """
+    def __init__(self, minimum, maximum, format, dtype, shape):
+        super(SparseMatrixMetaData, self).__init__(minimum, maximum)
+        self.dtype = dtype
+        self.format = format
+        self.shape = shape
+
+    @staticmethod
+    def parse_shape(shapestr):
+        if not shapestr or shapestr[0] != '(' or shapestr[-1] != ')':
+            raise ValueError('can not parse shape "{}"'.format(shapestr))
+        frags = shapestr[1:-1].split(',')
+        return tuple(long(e) for e in frags)
+
+    @classmethod
+    def from_array(cls, mtx):
+        return cls(
+            minimum=mtx.data.min(),
+            maximum=mtx.data.max(),
+            format=mtx.format,
+            dtype=mtx.dtype,
+            shape=mtx.shape,
+        )
+
+    @classmethod
+    def from_dict(cls, dikt):
+        return cls(
+            minimum=dikt['Minimum'],
+            maximum=dikt['Maximum'],
+            format=dikt['format'],
+            dtype=numpy.dtype(dikt['dtype']),
+            shape=cls.parse_shape(dikt['Shape']),
+        )
+
+    def to_dict(self):
+        return {
+            'Minimum': self.min,
+            'Maximum': self.max,
+            'format': self.format,
+            'dtype': self.dtype.str,
+            'Shape': str(self.shape),
+        }
+
+
+
+class SparseMatrix(Accessor):
+    """
+    Stores and loads a scipy.sparse csc or csr matrix in h5.
+    """
+    constructors = {'csr': scipy.sparse.csr_matrix, 'csc': scipy.sparse.csc_matrix}
+
+    def store(self, mtx):
+        # type: (scipy.sparse.spmatrix) -> None
+        mtx = self.trait_attribute._validate_set(None, mtx)
+        if mtx is None:
+            return
+        if mtx.format not in self.constructors:
+            raise ValueError('sparse format {} not supported'.format(mtx.format))
+
+        if not isinstance(mtx, scipy.sparse.spmatrix):
+            raise TypeError("expected scipy.sparse.spmatrix, got {}".format(type(mtx)))
+
+        self.owner.storage_manager.store_data(
+            'data',
+            mtx.data,
+            where=self.trait_attribute.field_name
+        )
+        self.owner.storage_manager.store_data(
+            'indptr',
+            mtx.indptr,
+            where=self.trait_attribute.field_name
+        )
+        self.owner.storage_manager.store_data(
+            'indices',
+            mtx.indices,
+            where=self.trait_attribute.field_name
+        )
+        self.owner.storage_manager.set_metadata(
+            SparseMatrixMetaData.from_array(mtx).to_dict(),
+            where=self.trait_attribute.field_name
+        )
+
+    def get_metadata(self):
+        meta = self.owner.storage_manager.get_metadata(self.trait_attribute.field_name)
+        return SparseMatrixMetaData.from_dict(meta)
+
+    def load(self):
+        meta = self.get_metadata()
+        if meta.format not in self.constructors:
+            raise ValueError('sparse format {} not supported'.format(meta.format))
+        constructor = self.constructors[meta.format]
+        data = self.owner.storage_manager.get_data(
+            'data',
+            where=self.trait_attribute.field_name,
+        )
+        indptr = self.owner.storage_manager.get_data(
+            'indptr',
+            where=self.trait_attribute.field_name,
+        )
+        indices = self.owner.storage_manager.get_data(
+            'indices',
+            where=self.trait_attribute.field_name,
+        )
+        mtx = constructor((data, indices, indptr), shape=meta.shape, dtype=meta.dtype)
+        mtx.sort_indices()
+        return mtx
 
 
 
@@ -215,7 +329,7 @@ class H5File(object):
 
 
     def store(self, datatype, scalars_only=False):
-        # type: (HasTraits) -> None
+        # type: (HasTraits, bool) -> None
         for name, accessor in self.__dict__.iteritems():
             if isinstance(accessor, Accessor):
                 f_name = accessor.trait_attribute.field_name
