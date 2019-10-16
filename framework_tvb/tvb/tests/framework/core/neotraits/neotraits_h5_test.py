4b60ea2524499a6b9227b9da89aab0b62baadfc4
liadomide <lia_domide@yahoo.com>
2019-10-16 09:36:11 +0000

    neotraits.h5 test updates for new api changes
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@9130 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- framework_tvb/tvb/tests/framework/core/neotraits/neotraits_h5_test.py
+++ framework_tvb/tvb/tests/framework/core/neotraits/neotraits_h5_test.py
@@ -6,57 +6,108 @@ from tvb.datatypes.surfaces import Surface, CorticalSurface
 from tvb.core.entities.file.datatypes.region_mapping_h5 import RegionMappingH5
 from tvb.core.entities.file.datatypes.surface_h5 import SurfaceH5, CorticalSurfaceH5
 
-conn = Connectivity(region_labels=numpy.array(["a", "b"]),
-                    weights=numpy.zeros((2, 2)),
-                    # undirected=False,
-                    tract_lengths=numpy.zeros((2, 2)),
-                    centres=numpy.zeros((2, 2)),
-                    cortical=numpy.array([[True, True], [True, True]]),
-                    hemispheres=numpy.array([[True, True], [True, True]]),
-                    orientations=numpy.zeros((2, 2)),
-                    areas=numpy.zeros((2, 2)),
-                    number_of_regions=long(2),
-                    number_of_connections=long(4),
-                    # parent_connectivity=""
-                    )
+conn = Connectivity(
+    region_labels=numpy.array(["a", "b"]),
+    weights=numpy.zeros((2, 2)),
+    # undirected=False,
+    tract_lengths=numpy.zeros((2, 2)),
+    centres=numpy.zeros((2, 2)),
+    cortical=numpy.array([[True, True], [True, True]]),
+    hemispheres=numpy.array([[True, True], [True, True]]),
+    orientations=numpy.zeros((2, 2)),
+    areas=numpy.zeros((2, 2)),
+    number_of_regions=2,
+    number_of_connections=4,
+    # parent_connectivity=""
+)
 
-surface = Surface(vertices=numpy.zeros((5, 3)),
-                  triangles=numpy.zeros((3, 3), dtype=int),
-                  vertex_normals=numpy.zeros((5, 3)),
-                  triangle_normals=numpy.zeros((3, 3)),
-                  number_of_vertices=long(5),
-                  number_of_triangles=long(3),
-                  edge_mean_length=1.0,
-                  edge_min_length=0.0,
-                  edge_max_length=2.0,
-                  zero_based_triangles=False,
-                  split_triangles=numpy.arange(0),
-                  number_of_split_slices=1,
-                  split_slices=dict(),
-                  bi_hemispheric=False,
-                  surface_type="surface",
-                  valid_for_simulations=True)
+surface = Surface(
+    vertices=numpy.zeros((5, 3)),
+    triangles=numpy.zeros((3, 3), dtype=int),
+    vertex_normals=numpy.zeros((5, 3)),
+    triangle_normals=numpy.zeros((3, 3)),
+    number_of_vertices=5,
+    number_of_triangles=3,
+    edge_mean_length=1.0,
+    edge_min_length=0.0,
+    edge_max_length=2.0,
+    zero_based_triangles=False,
+    split_triangles=numpy.arange(0),
+    number_of_split_slices=1,
+    split_slices=dict(),
+    bi_hemispheric=False,
+    surface_type="surface",
+    valid_for_simulations=True
+)
 
-rm = RegionMapping(array_data=numpy.arange(5),
-                   connectivity=conn,
-                   surface=surface)
+rm = RegionMapping(
+    array_data=numpy.arange(5),
+    connectivity=conn,
+    surface=surface
+)
 
-cort = CorticalSurface(vertices=numpy.zeros((5, 3)),
-                       triangles=numpy.zeros((3, 3), dtype=int),
-                       vertex_normals=numpy.zeros((5, 3)),
-                       triangle_normals=numpy.zeros((3, 3)),
-                       number_of_vertices=long(5),
-                       number_of_triangles=long(3),
-                       edge_mean_length=1.0,
-                       edge_min_length=0.0,
-                       edge_max_length=2.0,
-                       zero_based_triangles=False,
-                       split_triangles=numpy.arange(0),
-                       number_of_split_slices=1,
-                       split_slices=dict(),
-                       bi_hemispheric=False,
-                       # surface_type="surface",
-                       valid_for_simulations=True)
+cort = CorticalSurface(
+    vertices=numpy.zeros((5, 3)),
+    triangles=numpy.zeros((3, 3), dtype=int),
+    vertex_normals=numpy.zeros((5, 3)),
+    triangle_normals=numpy.zeros((3, 3)),
+    number_of_vertices=5,
+    number_of_triangles=3,
+    edge_mean_length=1.0,
+    edge_min_length=0.0,
+    edge_max_length=2.0,
+    zero_based_triangles=False,
+    split_triangles=numpy.arange(0),
+    number_of_split_slices=1,
+    split_slices=dict(),
+    bi_hemispheric=False,
+    # surface_type="surface",
+    valid_for_simulations=True
+)
+
+
+def test_store_load_configured_surf(tmph5factory):
+    surface = Surface(
+        vertices=numpy.zeros((5, 3)),
+        triangles=numpy.zeros((3, 3), dtype=int),
+        vertex_normals=numpy.zeros((5, 3)),
+        triangle_normals=numpy.zeros((3, 3)),
+        zero_based_triangles=False,
+        surface_type="surface",
+        valid_for_simulations=True
+    )
+    surface.configure()
+    assert surface.number_of_vertices == 5
+
+    tmp_path = tmph5factory()
+
+    with SurfaceH5(tmp_path) as f:
+        f.store(surface)
+
+    surf_stored = Surface()
+
+    with SurfaceH5(tmp_path) as f:
+        f.load_into(surf_stored)
+        assert surf_stored.split_slices['0']['triangles']['start_idx'] == 0
+
+
+def test_stored_conn_load_vertices_slice(tmph5factory):
+    surface = Surface(
+        vertices=numpy.zeros((5, 3)),
+        triangles=numpy.zeros((3, 3), dtype=int),
+        vertex_normals=numpy.zeros((5, 3)),
+        triangle_normals=numpy.zeros((3, 3)),
+        zero_based_triangles=False,
+        surface_type="surface",
+        valid_for_simulations=True
+    )
+    surface.configure()
+    tmp_path = tmph5factory()
+
+    with SurfaceH5(tmp_path) as f:
+        f.store(surface)
+        a = f.get_vertices_slice(0)
+        numpy.testing.assert_array_equal(a, numpy.zeros((5, 3)))
 
 
 def test_store_connectivity(tmph5factory):
