ba14a80521072bfbc49a2201c732dab3df3f1a98
liadomide <lia_domide@yahoo.com>
2019-10-16 09:42:08 +0000

    Keep visible=False on SimulatorIndex entities.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@9283 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- framework_tvb/tvb/core/entities/model/simulator/simulator.py
+++ framework_tvb/tvb/core/entities/model/simulator/simulator.py
@@ -1,7 +1,10 @@
 from sqlalchemy import Column, Integer, ForeignKey
-
 from tvb.core.entities.model.model_datatype import DataType
 
 
 class SimulatorIndex(DataType):
     id = Column(Integer, ForeignKey(DataType.id), primary_key=True)
+
+    def __init__(self, gid=None, **kwargs):
+        super(SimulatorIndex, self).__init__(gid, **kwargs)
+        self.visible = False
