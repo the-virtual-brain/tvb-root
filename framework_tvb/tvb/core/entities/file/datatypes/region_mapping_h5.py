7afb315240473675a32192cc1c553708cdac9e09
liadomide <lia_domide@yahoo.com>
2019-10-16 09:36:30 +0000

    Add H5 wrapper for RegionVolumeMapping datatype.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@9139 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- framework_tvb/tvb/core/entities/file/datatypes/region_mapping_h5.py
+++ framework_tvb/tvb/core/entities/file/datatypes/region_mapping_h5.py
@@ -1,5 +1,5 @@
 from tvb.core.neotraits.h5 import H5File, DataSet, Reference
-from tvb.datatypes.region_mapping import RegionMapping
+from tvb.datatypes.region_mapping import RegionMapping, RegionVolumeMapping
 
 
 class RegionMappingH5(H5File):
@@ -9,3 +9,12 @@ class RegionMappingH5(H5File):
         self.array_data = DataSet(RegionMapping.array_data, self)
         self.connectivity = Reference(RegionMapping.connectivity, self)
         self.surface = Reference(RegionMapping.surface, self)
+
+
+class RegionVolumeMappingH5(H5File):
+
+    def __init__(self, path):
+        super(RegionVolumeMappingH5, self).__init__(path)
+        self.array_data = DataSet(RegionVolumeMapping.array_data, self)
+        self.connectivity = Reference(RegionVolumeMapping.connectivity, self)
+        self.volume = Reference(RegionVolumeMapping.volume, self)
