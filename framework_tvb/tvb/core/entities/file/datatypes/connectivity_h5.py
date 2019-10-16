f044d5391324475f79aece9abdadc3b903f05d2d
liadomide <lia_domide@yahoo.com>
2019-10-16 09:40:16 +0000

    Move library methods to ConnectivityH5.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@9233 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- framework_tvb/tvb/core/entities/file/datatypes/connectivity_h5.py
+++ framework_tvb/tvb/core/entities/file/datatypes/connectivity_h5.py
@@ -19,3 +19,35 @@ class ConnectivityH5(H5File):
         self.number_of_connections = Scalar(Connectivity.number_of_connections, self)
         self.parent_connectivity = Scalar(Connectivity.parent_connectivity, self)
         self.saved_selection = Json(Connectivity.saved_selection, self)
+
+    def get_grouped_space_labels(self):
+        """
+        :return: A list [('left', [lh_labels)], ('right': [rh_labels])]
+        """
+        hemispheres = self.hemispheres.load()
+        region_labels = self.region_labels.load()
+        if hemispheres is not None and hemispheres.size:
+            l, r = [], []
+
+            for i, (is_right, label) in enumerate(zip(hemispheres, region_labels)):
+                if is_right:
+                    r.append((i, label))
+                else:
+                    l.append((i, label))
+            return [('left', l), ('right', r)]
+        else:
+            return [('', list(enumerate(region_labels)))]
+
+    def get_default_selection(self):
+        # should this be sub-selection or all always?
+        sel = self.saved_selection.load()
+        if sel is not None and len(sel) > 0:
+            return sel
+        else:
+            return range(len(self.region_labels.load()))
+
+    def get_measure_points_selection_gid(self):
+        """
+        :return: the associated connectivity gid
+        """
+        return self.gid
