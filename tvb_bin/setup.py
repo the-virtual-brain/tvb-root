4858081a640be48682eaa39b4fc91ba58f4ac735
liadomide <lia_domide@yahoo.com>
2019-10-17 10:27:08 +0000

    TVB-2521 Update version 2.0 in remaining modules
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk@9328 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb_bin/setup.py
+++ tvb_bin/setup.py
@@ -33,7 +33,7 @@ from setuptools import setup, find_packages
 setup(
     name='tvb_bin',
     packages=find_packages(),
-    version="1.5.10",
+    version="2.0.0",
     license="GPL v3",
     author="TVB Team"
 )
