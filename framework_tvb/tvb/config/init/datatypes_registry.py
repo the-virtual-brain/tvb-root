21738d03135f5467a0e4466e423cb5330e7b9a75
robert.vincze <vinczerobert7@gmail.com>
2019-10-28 17:38:15 +0200

    TVB-2524: To make the Dummy objects work.

--- framework_tvb/tvb/config/init/datatypes_registry.py
+++ framework_tvb/tvb/config/init/datatypes_registry.py
@@ -98,6 +98,11 @@ from tvb.core.neocom.h5 import REGISTRY
 
 # an alternative approach is to make each h5file declare if it has a corresponding datatype
 # then in a metaclass hook each class creation and populate a map
+from tvb.tests.framework.test_datatype import DummyDataType
+from tvb.tests.framework.test_datatype_h5 import DummyDataTypeH5
+from tvb.tests.framework.test_datatype_index import DummyDataTypeIndex
+
+
 def populate_datatypes_registry():
     REGISTRY.register_datatype(Connectivity, ConnectivityH5, ConnectivityIndex)
     REGISTRY.register_datatype(LocalConnectivity, LocalConnectivityH5, LocalConnectivityIndex)
@@ -134,3 +139,4 @@ def populate_datatypes_registry():
     REGISTRY.register_datatype(ConnectivityAnnotations, ConnectivityAnnotationsH5, ConnectivityAnnotationsIndex)
     REGISTRY.register_datatype(None, ValueWrapperH5, ValueWrapperIndex)
     REGISTRY.register_datatype(Cortex, CortexH5, None)
+    REGISTRY.register_datatype(DummyDataType, DummyDataTypeH5, DummyDataTypeIndex)
