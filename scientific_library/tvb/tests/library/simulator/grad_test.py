af589a532aaf0a6d371879459db0cd31e4fbd63f
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2019-11-13 15:10:49 +0100

    implement first fitting test

--- scientific_library/tvb/tests/library/simulator/grad_test.py
+++ scientific_library/tvb/tests/library/simulator/grad_test.py
@@ -24,3 +24,41 @@ class TestGradients:
 
     def test_grad(self):
         assert self.grad_func(0.4) == -0.96
+
+
+class TestHasGradient:
+
+    def setup_method(self):
+        class TestClass(HasGradient):
+            pass
+        self.test_class = TestClass()
+
+    def test_has_class(self):
+        assert has_gradient(self.test_class)
+
+
+class TestTimeSeriesGradient:
+
+    def setup_method(self):
+        self.dfun = Linear().make_dfun(numpy=anp)
+        self.scheme = EulerDeterministic(dt=0.1).make_scheme(self.dfun)
+        self.init = anp.random.randn(1, 5, 1)
+        self.trace = [self.init]
+        for i in range(10):
+            self.trace.append(self.scheme(self.trace[-1], self.trace[-1]))
+        self.trace = anp.array(self.trace)
+        def fn(init):
+            trace = [init]
+            for i in range(10):
+                trace.append(self.scheme(trace[-1], trace[-1]))
+            trace = anp.array(trace)
+            return anp.sum(anp.square(trace - self.trace))
+        self.fn = fn
+        self.grad_func = grad(self.fn)
+
+    def test_grad(self):
+        init = anp.random.randn(1, 5, 1)
+        assert anp.sum(anp.square(init - self.init)) > 0.1
+        for i in range(20):
+            init += -0.1 * self.grad_func(init)
+        assert anp.sum(anp.square(init - self.init)) < 0.1
