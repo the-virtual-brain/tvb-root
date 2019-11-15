a4e33db6a1e4e62b5a9f3ff1a41739da9d51ffc6
popaula937 <paula.popa@codemart.ro>
2019-11-15 11:32:47 +0200

    TVB-2536. Implement fill_from_trait() for equation forms and  extract constants.

--- framework_tvb/tvb/adapters/simulator/equation_forms.py
+++ framework_tvb/tvb/adapters/simulator/equation_forms.py
@@ -4,19 +4,35 @@ from tvb.datatypes.equations import Equation, Linear, Gaussian, DoubleGaussian, 
 from tvb.core.neotraits.forms import Form, ScalarField, SimpleFloatField
 
 
+LINEAR_EQUATION = 'Linear'
+GAUSSIAN_EQUATION = 'Gaussian'
+DOUBLE_GAUSSIAN_EQUATION = 'DoubleGaussian'
+SIGMOID_EQUATION = 'Sigmoid'
+GENRALIZED_SIGMOID_EQUATION = 'GeneralizedSigmoid'
+SINUSOID_EQUATION = 'Sinusoid'
+COSINE_EQUATION = 'Cosine'
+ALPHA_EQUATION = 'Alpha'
+PULSE_TRAIN_EQUATION = 'PulseTrain'
+
+
 def get_ui_name_to_equation_dict():
     eq_name_to_class = {
-        'Linear': Linear,
-        'Gaussian': Gaussian,
-        'DoubleGaussian': DoubleGaussian,
-        'Sigmoid': Sigmoid,
-        'GeneralizedSigmoid': GeneralizedSigmoid,
-        'Sinusoid': Sinusoid,
-        'Cosine': Cosine,
-        'Alpha': Alpha,
-        'PulseTrain': PulseTrain
+        LINEAR_EQUATION: Linear,
+        GAUSSIAN_EQUATION: Gaussian,
+        DOUBLE_GAUSSIAN_EQUATION: DoubleGaussian,
+        SIGMOID_EQUATION: Sigmoid,
+        GENRALIZED_SIGMOID_EQUATION: GeneralizedSigmoid,
+        SINUSOID_EQUATION: Sinusoid,
+        COSINE_EQUATION: Cosine,
+        ALPHA_EQUATION: Alpha,
+        PULSE_TRAIN_EQUATION: PulseTrain
     }
     return eq_name_to_class
+
+
+def get_ui_name_for_equation(equation_class):
+    equation_to_ui_name = dict((v, k) for k, v in get_ui_name_to_equation_dict().items())
+    return equation_to_ui_name.get(equation_class)
 
 
 def get_ui_name_to_monitor_equation_dict():
@@ -77,6 +93,9 @@ class LinearEquationForm(EquationForm):
         datatype.parameters['a'] = self.a.data
         datatype.parameters['b'] = self.b.data
 
+    def fill_from_trait(self, trait):
+        self.a.data = trait.parameters['a']
+        self.b.data = trait.parameters['b']
 
 class GaussianEquationForm(EquationForm):
 
@@ -91,6 +110,12 @@ class GaussianEquationForm(EquationForm):
         datatype.parameters['sigma'] = self.sigma.data
         datatype.parameters['midpoint'] = self.midpoint.data
         datatype.parameters['offset'] = self.offset.data
+
+    def fill_from_trait(self, trait):
+        self.amp.data = trait.parameters['amp']
+        self.sigma.data = trait.parameters['sigma']
+        self.midpoint.data = trait.parameters['midpoint']
+        self.offset.data = trait.parameters['offset']
 
 
 class DoubleGaussianEquationForm(EquationForm):
@@ -109,6 +134,14 @@ class DoubleGaussianEquationForm(EquationForm):
         datatype.parameters['midpoint_1'] = self.midpoint_1.data
         datatype.parameters['midpoint_2'] = self.midpoint_2.data
 
+    def fill_from_trait(self, trait):
+        self.amp_1.data = trait.parameters['amp_1']
+        self.amp_2.data = trait.parameters['amp_2']
+        self.sigma_1.data = trait.parameters['sigma_1']
+        self.sigma_2.data = trait.parameters['sigma_2']
+        self.midpoint_1.data = trait.parameters['midpoint_1']
+        self.midpoint_2.data = trait.parameters['midpoint_2']
+
 
 class SigmoidEquationForm(EquationForm):
 
@@ -123,6 +156,12 @@ class SigmoidEquationForm(EquationForm):
         datatype.parameters['radius'] = self.radius.data
         datatype.parameters['sigma'] = self.sigma.data
         datatype.parameters['offset'] = self.offset.data
+
+    def fill_from_trait(self, trait):
+        self.amp.data = trait.parameters['amp']
+        self.radius.data = trait.parameters['radius']
+        self.sigma.data = trait.parameters['sigma']
+        self.offset.data = trait.parameters['offset']
 
 
 class GeneralizedSigmoidEquationForm(EquationForm):
@@ -139,6 +178,12 @@ class GeneralizedSigmoidEquationForm(EquationForm):
         datatype.parameters['midpoint'] = self.midpoint.data
         datatype.parameters['sigma'] = self.sigma.data
 
+    def fill_from_trait(self, trait):
+        self.low.data = trait.parameters['low']
+        self.high.data = trait.parameters['high']
+        self.midpoint.data = trait.parameters['midpoint']
+        self.sigma.data = trait.parameters['sigma']
+
 
 class SinusoidEquationForm(EquationForm):
 
@@ -151,6 +196,10 @@ class SinusoidEquationForm(EquationForm):
     def fill_trait(self, datatype):
         datatype.parameters['amp'] = self.amp.data
         datatype.parameters['frequency'] = self.frequency.data
+
+    def fill_from_trait(self, trait):
+        self.amp.data = trait.parameters['amp']
+        self.frequency.data = trait.parameters['frequency']
 
 
 class CosineEquationForm(EquationForm):
@@ -165,6 +214,10 @@ class CosineEquationForm(EquationForm):
         datatype.parameters['amp'] = self.amp.data
         datatype.parameters['frequency'] = self.frequency.data
 
+    def fill_from_trait(self, trait):
+        self.amp.data = trait.parameters['amp']
+        self.frequency.data = trait.parameters['frequency']
+
 
 class AlphaEquationForm(EquationForm):
 
@@ -178,6 +231,11 @@ class AlphaEquationForm(EquationForm):
         datatype.parameters['onset'] = self.onset.data
         datatype.parameters['alpha'] = self.alpha.data
         datatype.parameters['beta'] = self.beta.data
+
+    def fill_from_trait(self, trait):
+        self.onset.data = trait.parameters['onset']
+        self.alpha.data = trait.parameters['alpha']
+        self.beta.data = trait.parameters['beta']
 
 
 class PulseTrainEquationForm(EquationForm):
@@ -194,6 +252,12 @@ class PulseTrainEquationForm(EquationForm):
         datatype.parameters['amp'] = self.amp.data
         datatype.parameters['onset'] = self.onset.data
 
+    def fill_from_trait(self, trait):
+        self.T.data = trait.parameters['T']
+        self.tau.data = trait.parameters['tau']
+        self.amp.data = trait.parameters['amp']
+        self.onset.data = trait.parameters['onset']
+
 
 class GammaEquationForm(EquationForm):
 
@@ -208,6 +272,12 @@ class GammaEquationForm(EquationForm):
         datatype.parameters['n'] = self.n.data
         datatype.parameters['factorial'] = self.factorial.data
         datatype.parameters['a'] = self.a.data
+
+    def fill_from_trait(self, trait):
+        self.tau.data = trait.parameters['tau']
+        self.n.data = trait.parameters['n']
+        self.factorial.data = trait.parameters['factorial']
+        self.a.data = trait.parameters['a']
 
 
 class DoubleExponentialEquationForm(EquationForm):
@@ -228,6 +298,16 @@ class DoubleExponentialEquationForm(EquationForm):
         datatype.parameters['amp_1'] = self.amp_1.data
         datatype.parameters['amp_2'] = self.amp_2.data
 
+    def fill_from_trait(self, trait):
+        self.tau_1.data = trait.parameters['tau_1']
+        self.tau_2.data = trait.parameters['tau_2']
+        self.a.data = trait.parameters['a']
+        self.f_1.data = trait.parameters['f_1']
+        self.f_2.data = trait.parameters['f_2']
+        self.pi.data = trait.parameters['pi']
+        self.amp_1.data = trait.parameters['amp_1']
+        self.amp_2.data = trait.parameters['amp_2']
+
 
 class FirstOrderVolterraEquationForm(EquationForm):
 
@@ -242,6 +322,12 @@ class FirstOrderVolterraEquationForm(EquationForm):
         datatype.parameters['tau_f'] = self.tau_f.data
         datatype.parameters['k_1'] = self.k_1.data
         datatype.parameters['V_0'] = self.V_0.data
+
+    def fill_from_trait(self, trait):
+        self.tau_s.data = trait.parameters['tau_s']
+        self.tau_f.data = trait.parameters['tau_f']
+        self.k_1.data = trait.parameters['k_1']
+        self.V_0.data = trait.parameters['V_0']
 
 
 class MixtureOfGammasEquationForm(EquationForm):
@@ -259,3 +345,11 @@ class MixtureOfGammasEquationForm(EquationForm):
         datatype.parameters['c'] = self.c.data
         datatype.parameters['gamma_a_1'] = self.gamma_a_1.data
         datatype.parameters['gamma_a_2'] = self.gamma_a_2.data
+
+    def fill_from_trait(self, trait):
+        self.a_1.data = trait.parameters['a_1']
+        self.a_2.data = trait.parameters['a_2']
+        self.l.data = trait.parameters['l']
+        self.c.data = trait.parameters['c']
+        self.gamma_a_1.data = trait.parameters['gamma_a_1']
+        self.gamma_a_2.data = trait.parameters['gamma_a_2']
