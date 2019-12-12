68d7cf40c49e244477b5fb4d16326dd6f457de82
popaula937 <paula.popa@codemart.ro>
2019-12-12 11:30:05 +0200

    TVB-2535. Simplify ViewModel-H5 serialization process.

--- framework_tvb/tvb/tests/framework/core/neotraits/view_model_test.py
+++ framework_tvb/tvb/tests/framework/core/neotraits/view_model_test.py
@@ -27,17 +27,15 @@
 #   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
 #
 #
-import numpy
+
 from tvb.adapters.visualizers.fourier_spectrum import FourierSpectrumModel
 from tvb.basic.neotraits.api import Attr
-from tvb.core.neocom import h5
-from tvb.core.neocom.h5 import h5_file_for_view_model
+from tvb.core.neotraits.h5 import ViewModelH5
 from tvb.core.neotraits.view_model import ViewModel, UploadAttr
 from tvb.datatypes.spectral import FourierSpectrum
 
 
 class DummyImporterViewModel(ViewModel):
-
     uploaded = UploadAttr(
         field_type=str,
         label='File to upload'
@@ -56,37 +54,31 @@ def test_dummy_importer_mv_to_h5(tmph5factory):
 
     divm = DummyImporterViewModel(uploaded=dummy_file_name, dummy_scalar=dummy_scalar)
     path = tmph5factory()
-    h5_file = h5_file_for_view_model(divm, path)
+    h5_file = ViewModelH5(path, divm)
     h5_file.store(divm)
     h5_file.close()
 
     loaded_divm = DummyImporterViewModel()
     assert not hasattr(loaded_divm, 'uploaded')
     assert loaded_divm.dummy_scalar is None
-    h5_file = h5_file_for_view_model(loaded_divm, path)
+    h5_file = ViewModelH5(path, loaded_divm)
     h5_file.load_into(loaded_divm)
     assert loaded_divm.uploaded == dummy_file_name
     assert loaded_divm.dummy_scalar == dummy_scalar
 
 
 def test_fourier_spectrum_model_to_h5(tmph5factory, time_series_index_factory):
-    time_series_index = time_series_index_factory()
-    time_series = h5.load_from_index(time_series_index)
-    data = numpy.random.random((10, 10))
-    fs = FourierSpectrum(array_data=data, source=time_series, segment_length=10)
+    fs = FourierSpectrum()
 
-    fsm = FourierSpectrumModel(input_data=fs)
-    fsm.input_data = FourierSpectrum()
+    fsm = FourierSpectrumModel(input_data=fs.gid)
     path = tmph5factory()
-    h5_file = h5_file_for_view_model(type(fsm), path)
+    h5_file = ViewModelH5(path, fsm)
 
     h5_file.store(fsm)
-    h5_file.input_data.store(fs.gid)
     h5_file.close()
 
     loaded_dt = FourierSpectrumModel()
-    h5_file = h5_file_for_view_model(type(loaded_dt), path)
+    h5_file = ViewModelH5(path, loaded_dt)
     h5_file.load_into(loaded_dt)
-    input_data_gid = h5_file.input_data.load()
-    assert input_data_gid == fs.gid
+    assert loaded_dt.input_data == fs.gid
     assert loaded_dt.gid == fsm.gid
