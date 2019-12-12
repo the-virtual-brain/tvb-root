4fda8b67e6f97d8036a0ee359a634bd70d75040b
robert.vincze <vinczerobert7@gmail.com>
2019-12-12 14:22:14 +0200

    TVB-2534: Server side of fire_operation function is done (with copying files in the right folder and then deleting the temp folder as well) but a few 'beauty' aspects should still be reviewed.

--- framework_tvb/tvb/interfaces/rest/server/resources/simulator/fire_simulation.py
+++ framework_tvb/tvb/interfaces/rest/server/resources/simulator/fire_simulation.py
@@ -1,18 +1,13 @@
 import os
 import tempfile
-
 from flask import request, jsonify
 from flask_restful import Resource
-from tvb.config.init.introspector_registry import IntrospectionRegistry
 from tvb.core.entities.file.files_helper import FilesHelper
 from tvb.core.services.exceptions import BurstServiceException
-from tvb.core.services.flow_service import FlowService
 from tvb.core.services.project_service import ProjectService
 from tvb.core.services.simulator_service import SimulatorService
 from werkzeug.utils import secure_filename
 from tvb.basic.profile import TvbProfile
-import threading
-
 
 UPLOAD_FOLDER = TvbProfile.current.TVB_TEMP_FOLDER
 
@@ -45,22 +40,12 @@ class FireSimulationResource(Resource):
         zip_path = os.path.join(destination_folder, filename)
         file.save(zip_path)
         FilesHelper().unpack_zip(zip_path, destination_folder)
-        # this introspection should go toqwards the servuice
-        simulator_algo = FlowService().get_algorithm_by_module_and_class(IntrospectionRegistry.SIMULATOR_MODULE,
-                                                                         IntrospectionRegistry.SIMULATOR_CLASS)
         project = self.project_service.find_project(project_id)
         user_id = project.fk_admin
 
         try:
-            # TODO make the asynch call in the service not here
-            thread = threading.Thread(target=self.simulator_service.prepare_simulation_on_server,
-                                      kwargs={'burst_config': None,
-                                              # TODO should we also create a burst config ?
-                                              'user_id': user_id,
-                                              'project': project,
-                                              'simulator_algo': simulator_algo,
-                                              'zip_folder_path': zip_path[:-4]})
-            thread.start()
+            self.simulator_service.prepare_simulation_on_server(burst_config=None, user_id=user_id, project=project,
+                                                                zip_folder_path=zip_path[:-4])
         except BurstServiceException as e:
             self.logger.exception('Could not launch burst!')
             resp = jsonify({'message': 'Some unexpected error happened!'})
@@ -69,7 +54,6 @@ class FireSimulationResource(Resource):
 
         resp = jsonify({'message': 'File succesfully uploaded!'})
         resp.status_code = 201
-        # TODO remove zip_path
         return resp
 
     def allowed_file(self, filename):
