99005955f0434a43fa57ab5c9b5a5021cbcdc621
bvalean <bogdan.valean@codemart.ro>
2019-12-18 10:50:49 +0200

    implement basic error handling mechanism

---
+++ framework_tvb/tvb/interfaces/rest/server/rest_api.py
@@ -0,0 +1,18 @@
+from flask_restful import Api
+from tvb.interfaces.rest.server.resources.exceptions import BaseRestException
+
+
+class RestApi(Api):
+    def handle_error(self, e):
+        if not isinstance(e, BaseRestException):
+            super().handle_error(e)
+
+        code = getattr(e, 'code', 500)
+        message = getattr(e, 'message', 'Internal Server Error')
+        to_dict = getattr(e, 'to_dict', None)
+
+        if to_dict:
+            data = to_dict()
+        else:
+            data = {'message': message}
+        return self.make_response(data, code)
