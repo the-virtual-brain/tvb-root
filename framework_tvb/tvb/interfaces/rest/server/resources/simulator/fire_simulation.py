79905dd042cf6bc8f475fe231bcbd9eaeaf7b4fc
bvalean <bogdan.valean@codemart.ro>
2019-12-19 15:22:06 +0200

    TVB-2534 Load view model from h5 file when an operation is launched

--- framework_tvb/tvb/interfaces/rest/server/resources/simulator/fire_simulation.py
+++ framework_tvb/tvb/interfaces/rest/server/resources/simulator/fire_simulation.py
@@ -4,18 +4,11 @@ import tempfile
 from flask import request
 from tvb.basic.profile import TvbProfile
 from tvb.core.entities.file.files_helper import FilesHelper
-from tvb.core.services.exceptions import BurstServiceException
 from tvb.core.services.project_service import ProjectService
 from tvb.core.services.simulator_service import SimulatorService
 from tvb.interfaces.rest.server.resources.exceptions import BaseRestException
 from tvb.interfaces.rest.server.resources.rest_resource import RestResource
 from werkzeug.utils import secure_filename
-
-UPLOAD_FOLDER = TvbProfile.current.TVB_TEMP_FOLDER
-
-
-def _allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() == 'zip'
 
 
 class FireSimulationResource(RestResource):
@@ -29,25 +22,19 @@ class FireSimulationResource(RestResource):
         if 'file' not in request.files:
             raise BaseRestException('No file part in the request!', 400)
         file = request.files['file']
-        if file.filename == '':
-            raise BaseRestException('No file selected for uploading!', 400)
-        if not (file and _allowed_file(file.filename)):
+        if not file.filename.endswith(FilesHelper.TVB_ZIP_FILE_EXTENSION):
             raise BaseRestException('Only ZIP files are allowed!', 400)
 
         filename = secure_filename(file.filename)
-        temp_name = tempfile.mkdtemp(dir=UPLOAD_FOLDER)
-        destination_folder = os.path.join(UPLOAD_FOLDER, temp_name)
+        temp_name = tempfile.mkdtemp(dir=TvbProfile.current.TVB_TEMP_FOLDER)
+        destination_folder = os.path.join(TvbProfile.current.TVB_TEMP_FOLDER, temp_name)
         zip_path = os.path.join(destination_folder, filename)
         file.save(zip_path)
         FilesHelper().unpack_zip(zip_path, destination_folder)
         project = self.project_service.find_project(project_id)
         user_id = project.fk_admin
 
-        try:
-            self.simulator_service.prepare_simulation_on_server(burst_config=None, user_id=user_id, project=project,
-                                                                zip_folder_path=zip_path[:-4])
-        except BurstServiceException as e:
-            self.logger.exception('Could not launch burst!')
-            raise BaseRestException(e.message, 500)
+        self.simulator_service.prepare_simulation_on_server(burst_config=None, user_id=user_id, project=project,
+                                                            zip_folder_path=zip_path[:-4])
 
         return {'message': 'File succesfully uploaded!'}, 201
