14bfbe2a509392d7609246c5193cef5c0bd54d15
robert.vincze <vinczerobert7@gmail.com>
2019-12-20 15:36:41 +0200

    TVB-2567: Added API's which are called from the main class (simulation_api is yet to be added because it takes a little more time to implement it)

--- framework_tvb/tvb/interfaces/rest/client/project/project_api.py
+++ framework_tvb/tvb/interfaces/rest/client/project/project_api.py
@@ -1,19 +1,14 @@
 import requests
+from tvb.interfaces.rest.client.main_api import MainApi
 
-BASE_PATH = "http://127.0.0.1:9090/api/"
 
+class ProjectApi(MainApi):
 
-class ProjectApi:
-
-    def get_projects_list(self, user_id):
-        response = requests.get(BASE_PATH + 'projects/' + user_id)
+    def get_data_in_project(self, project_gid):
+        response = requests.get(self.server_url + '/projects/' + project_gid + "/data")
         return response.content
 
-    def get_data_in_project(self, project_id):
-        response = requests.get(BASE_PATH + 'datatypes/project/' + project_id)
-        return response.content
-
-    def get_operations_in_project(self, project_id):
-        response = requests.get(BASE_PATH + 'operations/' + project_id)
+    def get_operations_in_project(self, project_gid):
+        response = requests.get(self.server_url + '/projects/' + project_gid + "/operations")
         return response.content
 
