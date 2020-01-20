35a62cd3085ad60c171bbdd6128a84de1fc66b30
gabiflorea10 <floreagabi_fga@yahoo.com>
2020-01-20 17:43:43 +0200

    TVB-2569 Create files for packaging tvb-rest-client on pypi (or pypi test)

---
+++ framework_tvb/setup_client.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+#
+#
+# TheVirtualBrain-Framework Package. This package holds all Data Management, and
+# Web-UI helpful to run brain-simulations. To use it, you also need do download
+# TheVirtualBrain-Scientific Package (for simulators). See content of the
+# documentation-folder for more details. See also http://www.thevirtualbrain.org
+#
+# (c) 2012-2020, Baycrest Centre for Geriatric Care ("Baycrest") and others
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this
+# program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#   CITATION:
+# When using The Virtual Brain for scientific publications, please cite it as follows:
+#
+#   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
+#   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
+#       The Virtual Brain: a simulator of primate brain network dynamics.
+#   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
+#
+#
+
+"""
+Install TVB Framework package for developers.
+
+Execute:
+    python setup.py install/develop
+
+"""
+
+import os
+import shutil
+import setuptools
+
+
+VERSION = "2.0a1"
+
+TVB_TEAM = "Mihai Andrei, Lia Domide, Stuart Knock, Bogdan Neacsa, Paula Sansz Leon, Marmaduke Woodman"
+
+TVB_INSTALL_REQUIREMENTS = ["allensdk", "BeautifulSoup4", "cherrypy", "flask", "flask-restplus", "formencode", "Jinja2",
+                            "h5py", "networkx", "nibabel", "numpy", "Pillow", "psutil", "scipy",
+                            "simplejson", "sqlalchemy", "sqlalchemy-migrate", "tvb-data", "tvb-gdist", "tvb-library"]
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fd:
+    DESCRIPTION = fd.read()
+
+setuptools.setup(name="tvb-rest-client-test27",
+                 version=VERSION,
+                 packages=setuptools.find_packages(where="./framework_tvb/tvb/interfaces/rest/client"),
+                 include_package_data=True,
+                 install_requires=TVB_INSTALL_REQUIREMENTS,
+                 extras_require={'postgres': ["psycopg2"],
+                                 'test': ["pytest", "pytest-benchmark"]},
+                 description='A package for performing whole brain simulations',
+                 long_description=DESCRIPTION,
+                 license="GPL v3",
+                 author=TVB_TEAM,
+                 author_email='tvb.admin@thevirtualbrain.org',
+                 url='http://www.thevirtualbrain.org',
+                 download_url='https://github.com/the-virtual-brain/tvb-framework',
+                 keywords='tvb brain simulator neuroscience human animal neuronal dynamics models delay')
+
+# Clean after install
+shutil.rmtree('tvb_framework.egg-info', True)
---
+++ tvb_build/package_for_pip_client.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+cd ..
+
+rm -Rf dist
+mkdir dist
+
+declare -a folders2pack=("framework_tvb" "scientific_library" "externals/tvb_gdist")
+if [[ "$1" != "" ]]; then
+    echo "Received param: " "$1"
+    folders2pack=("$1")
+fi
+
+
+for pipPackage in "${folders2pack[@]}"; do
+
+    echo "============================="
+    echo " Packing: " $pipPackage
+    echo "============================="
+
+    cd $pipPackage
+    python setup_client.py sdist
+    python setup_client.py bdist_wheel
+
+    if [ -d "../dist/" ]; then
+        mv dist/* ../dist/
+    else
+        mv dist/* ../../dist/
+    fi
+
+    rm -R dist
+    rm -R build
+
+    if [ -d "../dist/" ]; then
+        cd ..
+    else
+        cd ../..
+    fi
+done
+
+## After manual check, do the actual deploy on Pypi
+# twine upload dist/*
