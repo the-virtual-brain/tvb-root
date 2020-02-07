21aa1e594a6fdb18b365f4fec5d4a25a3a3b5a0d
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2020-02-07 12:47:19 +0100

    WIP first shot at updated script for setting up
    The existing scripts don't work for me but can't mess
    with the CI builds.  At some point it may be better
    to write this in Python since Conda is Python and
    APIs would be cleaner that various shell & batch
    scripts.

---
+++ tvb_build/setup_conda_env_sci_lib.bat
@@ -0,0 +1,2 @@
+conda install scipy numpy networkx scikit-learn cython h5py pip numexpr psutil ipython sympy matplotlib numba scikit-image pytest pytest-cov pytest-xdist pytest-benchmark
+pip install formencode cfflib jinja2 nibabel sqlalchemy-migrate allensdk BeautifulSoup4 cherrypy
