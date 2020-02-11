52be5fb8857c950b5fd09cb26ff8c8a89982bdf2
gabiflorea10 <floreagabi_fga@yahoo.com>
2020-02-11 10:27:18 +0200

    TVB-2531 Drop unused imports

--- framework_tvb/tvb/adapters/datatypes/db/region_mapping.py
+++ framework_tvb/tvb/adapters/datatypes/db/region_mapping.py
@@ -27,7 +27,6 @@
 #   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
 #
 #
-import numpy
 from sqlalchemy import Column, Integer, ForeignKey, Float, String
 from sqlalchemy.orm import relationship
 from tvb.core.entities.model.model_datatype import DataType, DataTypeMatrix
--- framework_tvb/tvb/adapters/datatypes/db/structural.py
+++ framework_tvb/tvb/adapters/datatypes/db/structural.py
@@ -27,7 +27,6 @@
 #   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
 #
 #
-import numpy
 from sqlalchemy import Column, Integer, ForeignKey, String, Float
 from sqlalchemy.orm import relationship
 from tvb.datatypes.structural import StructuralMRI
