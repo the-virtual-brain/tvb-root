4cb48889aef77d3bd8bd30dde8e2a9ea54b41838
liadomide <lia_domide@yahoo.com>
2020-02-28 16:35:18 +0200

    TVB-2594 When the POST call comes from the the DT Overlay, fill default values from traited ViewModel

--- framework_tvb/tvb/interfaces/web/controllers/flow_controller.py
+++ framework_tvb/tvb/interfaces/web/controllers/flow_controller.py
@@ -455,8 +455,9 @@ class FlowController(BaseController):
         try:
             form = adapter_instance.get_form()(project_id=project_id)
             if 'fill_defaults' in data:
-                form.fill_defaults()
-            form.fill_from_post(data)
+                form.fill_from_post_plus_defaults(data)
+            else:
+                form.fill_from_post(data)
             view_model = None
             if form.validate():
                 try:
