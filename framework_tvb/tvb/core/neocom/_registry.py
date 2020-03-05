8ad591c78eb3f3f5ed8356636540703738dc638f
popaula937 <paula.popa@codemart.ro>
2020-03-05 15:30:27 +0200

    HBP-65. Keep index to h5file mapping inside Registry for entities that do not have an associated datatype.

--- framework_tvb/tvb/core/neocom/_registry.py
+++ framework_tvb/tvb/core/neocom/_registry.py
@@ -47,6 +47,7 @@ class Registry(object):
         self._h5file_for_index = {}
         self._index_for_datatype = {}
         self._datatype_for_index = {}
+        self._index_for_h5file = {}
 
     def get_h5file_for_datatype(self, datatype_class):
         # type: (typing.Type[HasTraits]) -> typing.Type[H5File]
@@ -81,7 +82,8 @@ class Registry(object):
 
     def get_index_for_h5file(self, h5file_class):
         # type: (typing.Type[H5File]) -> typing.Type[DataType]
-        return self._index_for_datatype[self._datatype_for_h5file[h5file_class]]
+
+        return self._index_for_h5file[h5file_class]
 
     def register_datatype(self, datatype_class, h5file_class, datatype_index):
         # type: (HasTraits, H5File, DataType) -> None
@@ -90,3 +92,4 @@ class Registry(object):
         self._index_for_datatype[datatype_class] = datatype_index
         self._datatype_for_h5file[h5file_class] = datatype_class
         self._datatype_for_index[datatype_index] = datatype_class
+        self._index_for_h5file[h5file_class] = datatype_index
