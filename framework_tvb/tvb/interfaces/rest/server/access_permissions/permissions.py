cf229eb9a45113a8bd302a772f164462aa0a192f
bvalean <bogdan.valean@codemart.ro>
2020-03-12 11:25:14 +0200

    TVB-2628 Refactor access permissions

--- framework_tvb/tvb/interfaces/rest/server/access_permissions/permissions.py
+++ framework_tvb/tvb/interfaces/rest/server/access_permissions/permissions.py
@@ -36,28 +36,22 @@ from tvb.interfaces.rest.server.request_helper import get_current_user
 
 
 class ResourceAccessPermission:
-    def __init__(self, resource_identifier):
+    def __init__(self, resource_identifier, required_role=None):
         self.resource_identifier = resource_identifier
+        self.required_role = required_role
 
     def has_access(self):
         current_user = get_current_user()
-        return current_user.id in self.get_resource_owners()
+        if self.required_role is not None and current_user.role != self.required_role:
+            return False
+        return self._check_permission(current_user.id)
 
     @abstractmethod
-    def get_resource_owners(self):
+    def _check_permission(self, logged_user_id):
         """
         :return: a list of users id who can access the requested resource
         """
         raise RuntimeError("Not implemented.")
-
-
-class OperationAccessPermission(ResourceAccessPermission):
-    def __init__(self, operation_gid):
-        super(OperationAccessPermission, self).__init__(operation_gid)
-
-    def get_resource_owners(self):
-        operation = ProjectService.load_operation_by_gid(self.resource_identifier)
-        return [operation.fk_launched_by] if operation is not None else []
 
 
 class ProjectAccessPermission(ResourceAccessPermission):
@@ -65,20 +59,43 @@ class ProjectAccessPermission(ResourceAccessPermission):
         super(ProjectAccessPermission, self).__init__(project_gid)
         self.project_dao = CaseDAO()
 
-    def get_resource_owners(self):
+    def _check_permission(self, logged_user_id):
         try:
             project = self.project_dao.get_project_lazy_by_gid(self.resource_identifier)
         except ProjectServiceException:
-            return []
-        project_members = self.project_dao.get_members_of_project(project.id)
-        return [project_member.id for project_member in project_members]
+            return False
+        return self.check_project_permission(logged_user_id, project.id)
+
+    def check_project_permission(self, logged_user_id, project_id):
+        project_members = self.project_dao.get_members_of_project(project_id)
+        return logged_user_id in [project_member.id for project_member in project_members]
 
 
-class DataTypeAccessPermission(ResourceAccessPermission):
+class OperationAccessPermission(ProjectAccessPermission):
+    def __init__(self, operation_gid):
+        super(OperationAccessPermission, self).__init__(operation_gid)
+
+    def _check_permission(self, logged_user_id):
+        operation = ProjectService.load_operation_by_gid(self.resource_identifier)
+        if operation is None:
+            return False
+        return self.check_project_permission(logged_user_id, operation.fk_launched_in)
+
+
+class DataTypeAccessPermission(ProjectAccessPermission):
     def __init__(self, datatype_gid):
         super(DataTypeAccessPermission, self).__init__(datatype_gid)
         self.datatype_dao = DatatypeDAO()
 
-    def get_resource_owners(self):
+    def _check_permission(self, logged_user_id):
         datatype = self.datatype_dao.get_datatype_by_gid(self.resource_identifier)
-        return [datatype.parent_operation.user.id] if datatype is not None else []
+        if datatype is None:
+            return False
+        if logged_user_id in self.project_dao.get_members_of_project(datatype.parent_operation.fk_launched_in):
+            return True
+        links = self.datatype_dao.get_links_for_datatype(datatype.id)
+        if links is not None:
+            for link in links:
+                if self.check_project_permission(logged_user_id, link.fk_to_project):
+                    return True
+        return False
