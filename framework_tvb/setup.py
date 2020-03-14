b71c92192dcc5e681096e66fc0019793c2fb39c1
liadomide <lia_domide@yahoo.com>
2020-03-14 18:34:54 +0200

    TVB-2523 scikit-learn needs to be a dependency of tvb-framework as it is used in FCD Adapter

--- framework_tvb/setup.py
+++ framework_tvb/setup.py
@@ -46,8 +46,8 @@ TVB_TEAM = "Mihai Andrei, Lia Domide, Stuart Knock, Bogdan Neacsa, Paula Popa, P
 
 TVB_INSTALL_REQUIREMENTS = ["allensdk", "BeautifulSoup4", "cherrypy", "flask", "flask-restplus", "formencode",
                             "gevent", "h5py", "Jinja2", "networkx", "nibabel", "numpy", "Pillow", "psutil",
-                            "python-keycloak", "scipy", "simplejson", "sqlalchemy", "sqlalchemy-migrate",
-                            "tvb-data", "tvb-gdist", "tvb-library"]
+                            "python-keycloak", "scikit-learn", "scipy", "simplejson", "sqlalchemy", "sqlalchemy-migrate"
+                            , "tvb-data", "tvb-gdist", "tvb-library"]
 
 # Packaging tvb-framework with REST server inside
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fd:
