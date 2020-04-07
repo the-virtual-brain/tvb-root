ce110e6a9ed3a8c7d2f4fe2beb871a8dc2ca8d86
dionperd <dionperd@gmail.com>
2020-04-07 20:49:29 +0200

    Added if check to not try to store None reference datatypes in _h5loader.store

--- framework_tvb/tvb/core/neocom/_h5loader.py
+++ framework_tvb/tvb/core/neocom/_h5loader.py
@@ -135,7 +135,8 @@ class DirLoader(object):
         for traited_attr, sub_gid in sub_dt_refs:
             if sub_gid is not None:
                 subdt = self.load(sub_gid)
-                setattr(datatype, traited_attr.field_name, subdt)
+                if subdt is not None:
+                    setattr(datatype, traited_attr.field_name, subdt)
 
         return datatype
 
