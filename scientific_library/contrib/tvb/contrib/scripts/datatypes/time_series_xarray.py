ea9beeadca6724edf4d2f148bcfaf3a734c7b652
bvalean <bogdan.valean@codemart.ro>
2020-04-15 11:36:36 +0300

    HBP-75 Remove time_series_xarray header

--- scientific_library/contrib/tvb/contrib/scripts/datatypes/time_series_xarray.py
+++ scientific_library/contrib/tvb/contrib/scripts/datatypes/time_series_xarray.py
@@ -1,47 +1,13 @@
 # -*- coding: utf-8 -*-
-#
-#
-#  TheVirtualBrain-Scientific Package. This package holds all simulators, and 
-# analysers necessary to run brain-simulations. You can use it stand alone or
-# in conjunction with TheVirtualBrain-Framework Package. See content of the
-# documentation-folder for more details. See also http://www.thevirtualbrain.org
-#
-# (c) 2012-2017, Baycrest Centre for Geriatric Care ("Baycrest") and others
-#
-# This program is free software: you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software Foundation,
-# either version 3 of the License, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License along with this
-# program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
-#   CITATION:
-# When using The Virtual Brain for scientific publications, please cite it as follows:
-#
-#   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
-#   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
-#       The Virtual Brain: a simulator of primate brain network dynamics.
-#   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
-#
-#
 
-"""
-The TimeSeries datatypes.
-
-.. moduleauthor:: Stuart A. Knock <Stuart@tvb.invalid>
-
-"""
 from copy import deepcopy
 
 import numpy as np
 import xarray as xr
-from tvb.contrib.scripts.datatypes.time_series import TimeSeries as TimeSeriesTVB
-from tvb.contrib.scripts.utils.data_structures_utils import is_integer
 from six import string_types
 from tvb.basic.neotraits.api import HasTraits, Attr, List, narray_summary_info
+from tvb.contrib.scripts.datatypes.time_series import TimeSeries as TimeSeriesTVB
+from tvb.contrib.scripts.utils.data_structures_utils import is_integer
 from tvb.datatypes import sensors, surfaces, volumes, region_mapping, connectivity
 
 
