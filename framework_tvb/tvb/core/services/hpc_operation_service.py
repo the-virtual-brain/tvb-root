f38a222307c61ed2a11f57d1301668f51e4c69a3
bvalean <bogdan.valean@codemart.ro>
2020-09-08 13:43:33 +0300

    HBP-113 Add finally statement

--- framework_tvb/tvb/core/services/hpc_operation_service.py
+++ framework_tvb/tvb/core/services/hpc_operation_service.py
@@ -94,10 +94,10 @@ class HPCOperationService(object):
             HPCOperationService.LOGGER.error(exception)
             HPCOperationService._operation_error(operation)
 
-        if TvbProfile.current.web.ENCRYPT_STORAGE:
-            encryption_handler.sync_folders(folder)
-            encryption_handler.dec_project_usage_count(folder)
-            encryption_handler.set_project_inactive(operation.project)
+        finally:
+            if TvbProfile.current.web.ENCRYPT_STORAGE:
+                encryption_handler.sync_folders(folder)
+                encryption_handler.set_project_inactive(operation.project)
 
     @staticmethod
     def handle_hpc_status_changed(operation, simulator_gid, new_status):
