c01568104d1f083cb267bfb4c5ef80713dea9443
liadomide <lia_domide@yahoo.com>
2020-09-11 03:39:00 +0300

    TVB-2783 Prepare version numbers for the next release

--- scientific_library/setup.py
+++ scientific_library/setup.py
@@ -39,7 +39,7 @@ import os
 import shutil
 import setuptools
 
-LIBRARY_VERSION = "2.0.9"
+LIBRARY_VERSION = "2.0.10"
 
 LIBRARY_TEAM = "Marmaduke Woodman, Stuart Knock, Paula Sanz Leon, Viktor Jirsa"
 
