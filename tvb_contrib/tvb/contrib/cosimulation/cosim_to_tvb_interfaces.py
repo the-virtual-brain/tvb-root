60352b09a13d29e763a85cb05b25bf0ee2935ddf
dionperd <dionperd@gmail.com>
2020-09-24 18:53:45 +0200

    Modified CosimHistoryUpdate to update a CosimHistory class. Added update_from_cosimulator() methods.

--- tvb_contrib/tvb/contrib/cosimulation/cosim_to_tvb_interfaces.py
+++ tvb_contrib/tvb/contrib/cosimulation/cosim_to_tvb_interfaces.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 import os
 import numpy
-from tvb.basic.neotraits.api import HasTraits, Attr, Float, NArray, List
+from tvb.basic.neotraits.api import HasTraits, Attr, NArray, List
 from tvb.simulator.common import iround
-from tvb.simulator.history import BaseHistory, SparseHistory
+from tvb.contrib.cosimulation.history import CosimHistory
 
 
 class CosimUpdate(HasTraits):
 
-    """Base CosimUpdate class"""
+    """Base class to update TVB state or history from data from co-simulator"""
 
     proxy_inds = NArray(
         dtype=numpy.int,
         label="Indices of TVB proxy nodes",
-        required=True,
-    )
+        required=True)
 
-    number_of_proxy_nodes = Attr(field_type=int, required=True,
-                                 default=0)
+    number_of_proxy_nodes = Attr(field_type=int,
+                                 required=True,
+                                 default=0,
+                                 label="Number of TVB proxy nodes")
 
     voi = NArray(
         dtype=int,
@@ -26,41 +27,33 @@ class CosimUpdate(HasTraits):
              "should be updated (i.e., overwriten) during cosimulation."),
         required=False)
 
-    def configure(self, simulator):
-        """Method to compute the number_of_proxy_nodes, from user defined proxy_inds"""
-        self.number_of_proxy_nodes = len(self.proxy_inds)
-
-
-class CosimStateUpdate(CosimUpdate):
-
-    """Class for updating the current TVB state from co-simulator at each time step."""
-
     exclusive = Attr(
         field_type=bool,
         default=False, required=False,
         doc="1, when the proxy nodes substitute TVB nodes and their mutual connections should be removed.")
 
     def configure(self, simulator):
-        """Method to compute the number_of_proxy_nodes, from user defined proxy_inds,
-           and to set the default indices of the variables to be updated by the cosimulator,
-           if user input is not provided."""
-        super(CosimStateUpdate, self).configure()
+        """Method to compute the number_of_proxy_nodes, from user defined proxy_inds"""
+        self.number_of_proxy_nodes = len(self.proxy_inds)
         if self.voi is None or self.voi.size == 0:
             self.voi = numpy.r_[:len(simulator.model.variables_of_interest)]
+        super(CosimUpdate, self).configure()
 
-    def update(self, state, update=None):
-        """This method will update the input state for
-           - state variables of indices voi,
-           - and region nodes of indices proxy_inds,
-           with update data, if the latter is not None"""
-        if update:
-            state[self.voi, self.proxy_inds] = update
+
+class CosimStateUpdate(CosimUpdate):
+
+    def update_from_cosimulator(self):
+        """Method to get update data from co-simulator."""
+        raise NotImplementedError
+
+    def update(self, state):
+        state[self.voi, self.proxy_inds] = self.update_from_cosimulator()
         return state
 
 
-class CosimStateUpdateFromFile(CosimStateUpdate):
+class CosimStateUpdateFromFile(CosimUpdate):
 
-    """Class for reading data from a file
+    """Class for reading data to update state from a file
        and updating the current TVB state from co-simulator at each time step."""
 
     path = Attr(field_type=os.PathLike, required=False, default="")
@@ -69,18 +62,17 @@ class CosimStateUpdateFromFile(CosimStateUpdate):
         """Method to read update data from file."""
         raise NotImplementedError
 
-    def update(self, state, update=None):
+    def update(self, state):
         """This method will update the input state for
            - state variables of indices voi,
            - and region nodes of indices proxy_inds,
            with data read from file by update_from_file() method"""
-        state[self.voi, self.proxy_inds] = self.update_from_file()
-        return state
+        super(CosimStateUpdateFromFile, self).update(state, *self.update_from_file())
 
 
-class CosimStateUpdateFromMPI(CosimStateUpdate):
+class CosimStateUpdateFromMPI(CosimUpdate):
 
-    """Class for receving data from a MPI port
+    """Class for receving data to update state from a MPI port
        and updating the current TVB state from co-simulator at each time step."""
 
     path = Attr(field_type=os.PathLike, required=False, default="")
@@ -89,81 +81,48 @@ class CosimStateUpdateFromMPI(CosimStateUpdate):
         """Method to receive update data from a MPI port."""
         raise NotImplementedError
 
-    def update(self, state, update=None):
+    def update(self, state):
         """This method will update the input state for
            - state variables of indices voi,
            - and region nodes of indices proxy_inds,
            with data received from a MPI port by update_from_mpi() method"""
-        state[self.voi, self.proxy_inds] = self.update_from_mpi()
-        return state
+        super(CosimStateUpdateFromMPI, self).update(state, *self.update_from_mpi())
 
 
-class CosimHistoryUpdate(CosimUpdate):
+class CosimHistoryUpdate(HasTraits):
 
-    """Class for updating the history of TVB state from co-simulator every period time."""
+    """CosimHistoryUpdate class"""
 
     history = Attr(
-        field_type=BaseHistory,
-        label="Simulator history",
-        default=SparseHistory(),
+        field_type=CosimHistory,
+        label="Cosimulation history",
+        default=None,
         required=True,
-        doc="""A tvb.simulator.history""")
-
-    period = Float(
-        label="Updating period (ms)",
-        required=False,
-        default=0.9765625,  # ms. 0.9765625 => 1024Hz #ms, 0.5 => 2000Hz
-        doc="""Updating period in milliseconds, must be an integral multiple
-                    of integration-step size. As a guide: 2048 Hz => 0.48828125 ms ;  
-                    1024 Hz => 0.9765625 ms ; 512 Hz => 1.953125 ms.""")
-
-    dt = Float(
-        label="Integration step (ms)",  # order = 10
-        default=None,  # ms. 0.9765625 => 1024Hz #ms, 0.5 => 2000Hz
-        required=False,
-        doc="""Sampling period in milliseconds, must be an integral multiple
-                    of integration-step size. As a guide: 2048 Hz => 0.48828125 ms ;  
-                    1024 Hz => 0.9765625 ms ; 512 Hz => 1.953125 ms.""")
+        doc="""A history of the whole TVB state for the cosimulation synchronization time.""")
 
     def configure(self, simulator):
-        """Method to
-             - compute the number_of_proxy_nodes, from user defined proxy_inds,
-             - set the default indices of the variables to be updated by the cosimulator,
-               if user input is not provided,
-             - set history from simulator.history,
-             - set integration time step dt from simulator.integrator.dt
-             - set default period of update equal to dt if user has not defined it,
-             - compute the istep time steps of update as iround(period / dt),
-             - and confirm that istep is not greater than history.n_time."""
-        super(CosimHistoryUpdate, self).configure()
-        if self.voi is None or self.voi.size == 0:
-            self.voi = numpy.r_[:len(simulator.model.cvar)]
+        """Method to compute the number_of_proxy_nodes, from user defined proxy_inds"""
         self.history = simulator.history
-        self.dt = simulator.integrator.dt
-        if not self.period:
-            self.period = self.dt
-        self.istep = iround(self.period / self.dt)
-        if self.istep > self.history.n_time:
-            raise ValueError("Synchronization time %g for cosimulation update cannot "
-                             "be longer than the history buffer time length %g!"
-                             % (self.period, self.dt * self.n_time))
+        super(CosimHistoryUpdate, self).configure()
 
-    def update(self, step, update=None):
-        """This method will update the last istep steps of history for
-            - coupling variables of indices voi,
-            - and region nodes of indices proxy_inds,
-            with update data,
-            if the latter is not None, and time step is modulo the period istep."""
-        if update and step % self.istep == 0:
-            self.history.buffer[(step - self.istep + 1) % self.n_time:step % self.n_time + 1,
-                                self.voi,
-                                self.proxy_inds] = update
+    def update_from_cosimulator(self):
+        """Method to get update data from co-simulator."""
+        pass
+
+    def update(self, times=None, new_states=None):
+        if times is None or new_states is None:
+            times, new_states = self.update_from_cosimulator()
+        # First convert times into simulation steps and then update history
+        # for steps, vois and proxy_inds:
+        self.history.update_from_cosim([iround(time / self.dt) for time in times],
+                                       new_states,
+                                       self.voi, self.proxy_inds)
 
 
-class CosimHistoryUpdateFromFile(CosimHistoryUpdate):
+class CosimHistoryUpdateFromFile(CosimUpdate):
 
-    """Class for reading data from a file and
-       updating the history of TVB state from co-simulator every period time."""
+    """Class for reading data to update history from a file
+       and updating the current TVB state from co-simulator at each time step."""
 
     path = Attr(field_type=os.PathLike, required=False, default="")
 
@@ -171,49 +130,69 @@ class CosimHistoryUpdateFromFile(CosimHistoryUpdate):
         """Method to read update data from file."""
         raise NotImplementedError
 
-    def update(self, step, update=None):
-        """This method will update the last istep steps of history for
-            - coupling variables of indices voi,
-            - and region nodes of indices proxy_inds,
-            with update data
-           that are read from file by update_from_file method,
-           if time step is modulo the period istep."""
-        if step % self.istep == 0:
-            self.history.buffer[(step - self.istep + 1) % self.n_time:step % self.n_time + 1,
-                                self.voi,
-                                self.proxy_inds] = self.update_from_file()
+    def update(self, state, new_state=None):
+        """This method will update the input state for
+           - state variables of indices voi,
+           - and region nodes of indices proxy_inds,
+           with data read from file by update_from_file() method"""
+        super(CosimHistoryUpdateFromFile, self).update(state, *self.update_from_file())
 
 
-class CosimHistoryUpdateFromMPI(CosimHistoryUpdate):
+class CosimUpdateFromMPI(CosimUpdate):
 
-    """Class for getting data from a MPI port and
-       updating the history of TVB state from co-simulator every period time."""
+    """Class for receving data to update history from a MPI port
+       and updating the current TVB state from co-simulator at each time step."""
 
     path = Attr(field_type=os.PathLike, required=False, default="")
 
-    def update_from_mpi(self, data=None):
+    def update_from_mpi(self):
         """Method to receive update data from a MPI port."""
         raise NotImplementedError
 
-    def update(self, step, update=None):
-        """This method will update the last istep steps of history for
-            - coupling variables of indices voi,
-            - and region nodes of indices proxy_inds,
-            with update data
-           that are received from a MPI port by update_from_mpi method,
-           if time step is modulo the period istep."""
-        if step % self.istep == 0:
-            self.history.buffer[(step - self.istep + 1) % self.n_time:step % self.n_time + 1,
-                                self.voi,
-                                self.proxy_inds] = self.update_from_mpi()
+    def update(self, state, new_state=None):
+        """This method will update the input state for
+           - state variables of indices voi,
+           - and region nodes of indices proxy_inds,
+           with data received from a MPI port by update_from_mpi() method"""
+        super(CosimUpdateFromMPI, self).update(state, *self.update_from_mpi())
 
 
 class CosimToTVBInterfaces(HasTraits):
 
-    """This class holds lists of
-           - CosimStateUpdate,
-           - CosimHistoryUpdate,
-           class instances"""
+    """This class holds lists of CosimStateUpdate and CosimHistoryUpdate class instances"""
 
     state_interfaces = List(of=CosimStateUpdate)
     history_interfaces = List(of=CosimHistoryUpdate)
+
+    @property
+    def interfaces(self):
+        return self.state_interfaces + self.history_interfaces
+
+    @property
+    def state_vois(self):
+        return [state_interface.voi for state_interface in self.state_interfaces]
+
+    @property
+    def state_proxy_inds(self):
+        return [state_interface.proxy_inds for state_interface in self.state_interfaces]
+
+    @property
+    def history_vois(self):
+        return [history_interface.voi for history_interface in self.history_interfaces]
+
+    @property
+    def history_proxy_inds(self):
+        return [history_interface.proxy_inds for history_interface in self.history_interfaces]
+
+    @property
+    def vois(self):
+        return self.state_vois + self.history_vois
+
+    @property
+    def proxy_inds(self):
+        return self.state_proxy_inds + self.history_proxy_inds
+
+    def configure(self, simulator):
+        for interfaces in self.interfacess:
+            interfaces.configure(simulator)
+        super(CosimToTVBInterfaces, self).configure()
