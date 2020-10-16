426aedf8b53fd32c0b39972d29f3c361a7d8919a
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2020-10-16 07:31:32 +0200

    minor tweaks to clean up

--- scientific_library/tvb/simulator/models/fast_montbrio.py
+++ scientific_library/tvb/simulator/models/fast_montbrio.py
@@ -43,45 +43,52 @@ def fmri(node_bold, x, dt):
     node_bold[3] = q
     return V0 * (k1 * (1.0 - q) + k2 * (1.0 - q / v) + k3 * (1.0 - v))
 
-
-@nb.njit(boundscheck=False, fastmath=True)
-def loop(r, V, wrV, w, d, tavg, bold_state, bold_out, dt, I, Delta, eta, tau, J, cr, cv, pi):
-    o_tau = nb.float32(1 / tau)
+def make_loop(nh, nn, dt):
+    # for now assume dt=0.1ms, speed 10m/s, max delay 25.6ms ~ gamma band
+    assert nh == 256
+    assert dt == 0.1
+    nh, nn = [nb.uint32(_) for _ in (nh, nn)]
+    dt, pi = [nb.float32(_) for _ in (dt, np.pi)]
     sqrt_dt = nb.float32(np.sqrt(dt))
-    # assume dt=0.1ms, speed 10m/s, max delay 25.6ms ~ gamma band
-    nh = nb.uint32(256)
     o_nh = nb.float32(1 / nh)
     nh_dt = nb.float32(nh * dt)
-    assert r.shape[0] == V.shape[0] == nh
-    for t0 in range(-1, r.shape[0] - 1):
-        t = nh-1 if t0==0 else t0
-        t1 = t0 + 1
+    @nb.njit(boundscheck=False, fastmath=True)
+    def loop(r, V, wrV, w, d, tavg, bold_state, bold_out, dt, I, Delta, eta, tau, J, cr, cv):
+        o_tau = nb.float32(1 / tau)
+        assert r.shape[0] == V.shape[0] == nh  # shape asserts help numba optimizer
+        assert r.shape[1] == V.shape[1] == nn
+        for t0 in range(-1, nh - 1):
+            t = nh-1 if t0==0 else t0
+            t1 = t0 + 1
+            for i in range(nn):
+                rc = nb.float32(0)
+                Vc = nb.float32(0)
+                for j in range(nn):
+                    dij = (t - d[i, j] + nh) & (nh-1)
+                    rc += w[i, j] * r[dij, j]
+                    Vc += w[i, j] * r[dij, j]
+                dr = o_tau * (Delta / (pi * tau) + 2 * V[t, i] * r[t, i])
+                dV = o_tau * (V[t, i] ** 2 - pi ** 2 * tau ** 2 * r[t, i] ** 2 + eta + J * tau * r[t, i] + I + cr * rc + cv * Vc)
+                r[t1, i] = r[t, i] + dr * dt + sqrt_dt * 0.1 * wrV[0, t, i]
+                V[t1, i] = V[t, i] + dV * dt + sqrt_dt * 0.01 * wrV[1, t, i]
+                # t avg over 25.6 ms ~39 Hz sampling; TODO use hanning window
+                tavg[0, i] += r[t1, i] * o_nh
+                tavg[1, i] += V[t1, i] * o_nh
+        # update bold (from r?)
         for i in range(r.shape[1]):
-            rc = nb.float32(0)
-            Vc = nb.float32(0)
-            for j in range(r.shape[1]):
-                dij = (t - d[i, j] + nh) & (nh-1)
-                rc += w[i, j] * r[dij, j]
-                Vc += w[i, j] * r[dij, j]
-            dr = o_tau * (Delta / (pi * tau) + 2 * V[t, i] * r[t, i])
-            dV = o_tau * (V[t, i] ** 2 - pi ** 2 * tau ** 2 * r[t, i] ** 2 + eta + J * tau * r[t, i] + I + cr * rc + cv * Vc)
-            r[t1, i] = r[t, i] + dr * dt + sqrt_dt * 0.1 * wrV[0, t, i]
-            V[t1, i] = V[t, i] + dV * dt + sqrt_dt * 0.01 * wrV[1, t, i]
-            # t avg over 25.6 ms ~39 Hz sampling; TODO use hanning window
-            tavg[0, i] += r[t1, i] * o_nh
-            tavg[1, i] += V[t1, i] * o_nh
-    # update bold (from r?)
-    for i in range(r.shape[1]):
-        bold_out[i] = fmri(bold_state[i], tavg[0, i], nh_dt)
+            bold_out[i] = fmri(bold_state[i], tavg[0, i], nh_dt)
+    return loop
 
-nt, nn = 256, 96                                            # likely dimensions
+# TODO turn the following into a class w/ all vars as attributes
+# inner loop setup dimensions, constants, buffers
+nh, nn = 256, 96                                            # likely dimensions
 dt = 0.1                                                    # fixed dt
 w = (np.random.randn(nn, nn)**2).astype(np.float32)         # random weights
-d = (np.random.rand(nn, nn)**2 * 256).astype(np.uint32)     # random delays
-r, V = np.zeros((2, nt, nn), 'f')                           # buffers for r & V
+d = (np.random.rand(nn, nn)**2 * 256).astype(np.uint32)     # random delays (in units of dt)
+r, V = np.zeros((2, nh, nn), 'f')                           # buffers for r & V
 V -= 2.0
-I, Delta, eta, tau, J, cr, cv, pi, dt = [nb.float32(_) for _ in (0.0, 1.0, -5.0, 100.0, 15.0, 1.0/nn, 0.0, np.pi, dt)]
-wrV = np.empty((2, nt, nn), 'f')                            # buffer for noise
+I, Delta, eta, tau, J, cr, cv = [nb.float32(_) for _ in (0.0, 1.0, -5.0, 100.0, 15.0, 1.0/nn, 0.0)]
+wrV = np.empty((2, nh, nn), 'f')                            # buffer for noise
 tavg = np.zeros((2, nn), 'f')                               # buffer for temporal average
 bold_state = np.zeros((nn, 4), 'f')                         # buffer for bold state
 bold_state[:,1:] = 1.0
@@ -89,11 +96,27 @@ bold_out = np.zeros((nn,), 'f')                             # buffer for bold ou
 rng = np.random.default_rng(SFC64(42))                      # create RNG w/ known seed
 
 # first call to jit the function
-loop(r, V, wrV, w, d, tavg, bold_state, bold_out, dt, I, Delta, eta, tau, J, cr, cv, pi)
+loop = make_loop(nh, nn, dt)
+loop(r, V, wrV, w, d, tavg, bold_state, bold_out, dt, I, Delta, eta, tau, J, cr, cv)
 
-# 10 iters of 1 second each; ~10x over realtime
-for i in tqdm.trange(10):
-    # 40 iters of 25.6ms jit'ed inner loop = 1s
-    for j in range(40):
-        rng.standard_normal(size=(2, nt, nn), dtype='f', out=wrV)  # ~15% time here
-        loop(r, V, wrV, w, d, tavg, bold_state, bold_out, dt, I, Delta, eta, tau, J, cr, cv, pi)
+# outer loop setup
+win_len = nh * dt
+total_time = 60e3 # 1 min
+total_wins = int(total_time / win_len)
+bold_tr = 1800  # in ms
+bold_skip = int(bold_tr / win_len)
+tavg_trace = []  # TODO memmap
+bold_trace = []
+
+# start time stepping
+for t in tqdm.trange(total_wins):
+    rng.standard_normal(size=(2, nh, nn), dtype='f', out=wrV)  # ~15% time here
+    loop(r, V, wrV, w, d, tavg, bold_state, bold_out, dt, I, Delta, eta, tau, J, cr, cv)
+    tavg_trace.append(tavg.copy())
+    if t % bold_skip == 0:
+        bold_trace.append(bold_out)
+
+tavg_trace = np.array(tavg_trace)
+bold_trace = np.array(bold_trace)
+print('tavg', tavg_trace.shape)
+print('bold', bold_trace.shape)
