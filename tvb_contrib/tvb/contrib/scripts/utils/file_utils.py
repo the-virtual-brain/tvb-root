d30192d98787aa6f3fd24bbb436a967fb4f008c8
dionperd <dionperd@gmail.com>
2020-11-20 20:42:31 +0100

    Added truncate_ascii_file_after_header to file_utils.py of tvb.contrib.scripts

--- tvb_contrib/tvb/contrib/scripts/utils/file_utils.py
+++ tvb_contrib/tvb/contrib/scripts/utils/file_utils.py
@@ -163,3 +163,14 @@ def safe_makedirs(folder):
                         raise
                 except:
                     pass
+
+
+def truncate_ascii_file_after_header(filepath, header_chars="#"):
+    n_header_chars = len(header_chars)
+    with open("/Users/dionperd/Desktop/test.txt", "r+") as file:
+        line = file.readline()
+        while len(line) >= n_header_chars and line[:n_header_chars] == header_chars:
+            line = file.readline()
+        file.seek(file.tell() - len(line), os.SEEK_SET)
+        file.truncate()
+        file.close()
