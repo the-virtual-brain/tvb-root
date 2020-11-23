87ebfe2f1ed5fce6b562c834a8f137575d651d2a
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2020-11-23 16:51:32 +0100

    rewrite Montbrio GPU kernel, requires debugging

--- scientific_library/tvb/simulator/_numba/montbrio.py
+++ scientific_library/tvb/simulator/_numba/montbrio.py
@@ -2,116 +2,114 @@ import math
 
 import numba as nb
 import numpy as np
+from numba import cuda
+from numba.cuda.random import xoroshiro128p_uniform_float32
 
 from .monitors import fmri
 
 
-def make_gpu_loop(nh, nn, nt, dt):
-    from numba import cuda
-    from numba.cuda.random import xoroshiro128p_uniform_float32
-    assert nh == 32
-    nh, nn = [nb.uint32(_) for _ in (nh, nn)]
-    dt, pi = [nb.float32(_) for _ in (dt, np.pi)]
-    sqrt_dt = nb.float32(np.sqrt(dt))
-    o_6 = nb.float32(1 / 6)
-    pi_2 = nb.float32(np.pi * 2)
-    o_nt = nb.float32(1.0 / nt)
-    nl, nc = nb.int32(32), nb.int32(nn // 32)
-    @cuda.jit(inline='always',device=True)
-    def dr(r, V, o_tau, pi, tau, Delta):
-        return o_tau * (Delta / (pi * tau) + 2 * V * r)
-    @cuda.jit(inline='always',device=True)
-    def dV(r, V, o_tau, pi, tau, eta, J, I, cr, rc, cv, Vc):
-        return o_tau * (V ** 2 - (pi ** 2) * (tau ** 2) * (r ** 2) + eta + J * tau * r + I + cr * rc + cv * Vc)
-    @cuda.jit(fastmath=True)
-    def loop(r, V, w, d, tavg, I, Delta, eta, tau, J, cr, cv, r_sigma, V_sigma, rng_states):
-        it = cuda.threadIdx.x
-        o_tau = nb.float32(1 / tau)
-        assert r.shape[0] == V.shape[0] == nn  # shape asserts help numba optimizer
-        assert r.shape[1] == V.shape[1] == nh
-        # 48 KB shared memory
-        # 96 nn x 16 nh x 4 f32 x 2 svar = 12 KB
-        # 96^2*2 * 4f32 = 73 KB so we can't do everything in shared memory
-        # coupling
-        aff = cuda.shared.array((3, 32), nb.float32)
-        # current state
-        rt = cuda.shared.array((3, 32), nb.float32)
-        Vt = cuda.shared.array((3, 32), nb.float32)
-        nrt = cuda.shared.array((3, 32), nb.float32)
-        nVt = cuda.shared.array((3, 32), nb.float32)
-        # shared mem usage 640 per block, nvcc says
-        # used 106 registers, 32 stack, 1920 bytes smem, 856 bytes cmem[0], 24 bytes cmem[2], 0 bytes lmem
-        for i in range(nc):
-            rt[i, it] = r[i * nl + it, 0]
-            Vt[i, it] = V[i * nl + it, 0]
-        for i in range(nc):
-            tavg[0, i, it] = nb.float32(0.0)
-            tavg[1, i, it] = nb.float32(0.0)
-        for t in range(nt):
-            for i in range(nc):
-                aff[i, it] = nb.float32(0)
-            for j in range(nn):
-                for i in range(nc):
-                    aff[i, it] += w[i,j,it] * cuda.shfl_sync(nb.int32(1), r[j,it], d[j,i,it])
-            for i in range(nc):
-                r_ = rt[i, it]  # we need history stride 1 in time, but state stride 1 in space..
-                V_ = Vt[i, it]
-                dr_0 = dr(r_, V_, o_tau, pi, tau, Delta)
-                dV_0 = dV(r_, V_, o_tau, pi, tau, eta, J, I, cr, aff[i,it], cv, nb.float32(0))
-                kh = nb.float32(0.5)
-                dr_1 = dr(r_ + dt*kh*dr_0, V_ + dt*kh*dV_0, o_tau, pi, tau, Delta)
-                dV_1 = dV(r_ + dt*kh*dr_0, V_ + dt*kh*dV_0, o_tau, pi, tau, eta, J, I, cr, aff[i,it], cv, nb.float32(0))
-                dr_2 = dr(r_ + dt*kh*dr_1, V_ + dt*kh*dV_1, o_tau, pi, tau, Delta)
-                dV_2 = dV(r_ + dt*kh*dr_1, V_ + dt*kh*dV_1, o_tau, pi, tau, eta, J, I, cr, aff[i,it], cv, nb.float32(0))
-                kh = nb.float32(1.0)
-                dr_3 = dr(r_ + dt*kh*dr_2, V_ + dt*kh*dV_2, o_tau, pi, tau, Delta)
-                dV_3 = dV(r_ + dt*kh*dr_2, V_ + dt*kh*dV_2, o_tau, pi, tau, eta, J, I, cr, aff[i,it], cv, nb.float32(0))
-                # drift
-                nrt[i, it] = r_ + o_6 * dt * (dr_0 + 2 * (dr_1 + dr_2) + dr_3)
-                nVt[i, it] = V_ + o_6 * dt * (dV_0 + 2 * (dV_1 + dV_2) + dV_3)
-                # box-muller
-                u1 = xoroshiro128p_uniform_float32(rng_states, t*nn*2 + i*nl*2 + it)
-                u2 = xoroshiro128p_uniform_float32(rng_states, t*nn*2 + i*nl*2 + nl + it)
-                z0 = math.sqrt(-nb.float32(2.0) * math.log(u1)) * math.cos(pi_2 * u2)
-                z1 = math.sqrt(-nb.float32(2.0) * math.log(u1)) * math.sin(pi_2 * u2)
-                nrt[i, it] += sqrt_dt * r_sigma * z0
-                nVt[i, it] += sqrt_dt * V_sigma * z1
-                nrt[i, it] *= nrt[i, it] >= 0
-            for i in range(nc):
-                rt[i, it] = nrt[i, it]
-                Vt[i, it] = nVt[i, it]
-            # global memory writes
-            for i in range(nc):
-                # shift history
-                for l in range(nl):
-                    r[i*nl+l, it] = cuda.shfl_up_sync(1, r[i*nl+l, it], 1) # [0, 1, 2] -> [0, 0, 1]
-                    V[i*nl+l, it] = cuda.shfl_up_sync(1, V[i*nl+l, it], 1)
-                # insert current state at 0
-                r[i*nl+it, 0] = rt[i, it]
-                V[i*nl+it, 0] = Vt[i, it]
-                # update tavg
-                tavg[0, i, it] += rt[i, it] * o_nt
-                tavg[1, i, it] += Vt[i, it] * o_nt
-            # TODO fmri
-    return loop
+def dr(r, V, o_tau, pi, tau, Delta):
+    "Time-derivative of r(t) in the Montbrio model."
+    return o_tau * (Delta / (pi * tau) + 2 * V * r)
 
 
-def make_loop(nh, nto, nn, dt, cfpre, cfpost):
+def dV(r, V, o_tau, pi, tau, eta, J, I, cr, rc, cv, Vc):
+    "Time-derivative of V(t) in the Montbrio model."
+    return o_tau * (V ** 2 - (pi ** 2) * (tau ** 2) * (r ** 2) + eta + J * tau * r + I + cr * rc + cv * Vc)
+
+
+def make_rk4_rV(dt, sqrt_dt, o_6, use_cuda=False):
+    if use_cuda:
+        jit = cuda.jit(inline='always',device=True)
+    else:
+        jit = nb.njit(fastmath=True,boundscheck=False,inline='always')
+    dr_ = jit(dr)
+    dV_ = jit(dV)
+    def rk4_rV(it, nrV, rti, Vti,
+               o_tau, pi, tau, Delta, eta, J, I, cr, rc, cv, Vc,
+               r_sigma, V_sigma, z0, z1):
+        dr_0 = dr_(rti, Vti, o_tau, pi, tau, Delta)
+        dV_0 = dV_(rti, Vti, o_tau, pi, tau, eta, J, I, cr, rc, cv, Vc)
+        kh = nb.float32(0.5)
+        dr_1 = dr_(rti + dt * kh * dr_0, Vti + dt * kh * dV_0, o_tau, pi, tau, Delta)
+        dV_1 = dV_(rti + dt * kh * dr_0, Vti + dt * kh * dV_0, o_tau, pi, tau, eta, J, I, cr, rc, cv, Vc)
+        dr_2 = dr_(rti + dt * kh * dr_1, Vti + dt * kh * dV_1, o_tau, pi, tau, Delta)
+        dV_2 = dV_(rti + dt * kh * dr_1, Vti + dt * kh * dV_1, o_tau, pi, tau, eta, J, I, cr, rc, cv, Vc)
+        kh = nb.float32(1.0)
+        dr_3 = dr_(rti + dt * kh * dr_2, Vti + dt * kh * dV_2, o_tau, pi, tau, Delta)
+        dV_3 = dV_(rti + dt * kh * dr_2, Vti + dt * kh * dV_2, o_tau, pi, tau, eta, J, I, cr, rc, cv, Vc)
+        nrV[0, it] = rti + o_6 * dt * (dr_0 + 2 * (dr_1 + dr_2) + dr_3) + sqrt_dt * r_sigma * z0
+        nrV[0, it] *= nrV[0, it] > 0
+        nrV[1, it] = Vti + o_6 * dt * (dV_0 + 2 * (dV_1 + dV_2) + dV_3) + sqrt_dt * V_sigma * z1
+    return jit(rk4_rV)
+
+# def make_box_muller():
+#     import numba.cuda.random as cr
+#     @cuda.jit(inline='always')
+#     def box_muller(rngs, offset, out):
+#         it = cuda.threadIdx.x
+#     return box_muller
+
+def setup_const(nh, nto, nn, dt):
     nh, nn = [nb.uint32(_) for _ in (nh, nn)]
     dt, pi = [nb.float32(_) for _ in (dt, np.pi)]
     sqrt_dt = nb.float32(np.sqrt(dt))
     o_nh = nb.float32(1 / nh * nto)
     o_6 = nb.float32(1 / 6)
-    @nb.njit(fastmath=True,boundscheck=False,inline='always')
-    def dr(r, V, o_tau, pi, tau, Delta):
-        return o_tau * (Delta / (pi * tau) + 2 * V * r)
-    @nb.njit(fastmath=True,boundscheck=False,inline='always')
-    def dV(r, V, o_tau, pi, tau, eta, J, I, cr, rc, cv, Vc):
-        return o_tau * (V ** 2 - (pi ** 2) * (tau ** 2) * (r ** 2) + eta + J * tau * r + I + cr * rc + cv * Vc)
-    @nb.njit(boundscheck=False, fastmath=True)
-    def loop(r, V, wrV, w, d, tavg, bold_state, bold_out, I, Delta, eta, tau, J, cr, cv, r_sigma, V_sigma):
+    return nh, nn, dt, pi, sqrt_dt, o_nh, o_6
+
+
+def make_gpu_loop(nh, nto, nn, dt, cfpre, cfpost):
+    nh, nn, dt, pi, sqrt_dt, o_nh, o_6 = setup_const(nh, nto, nn, dt)
+    rk4_rV = make_rk4_rV(dt, sqrt_dt, o_6, use_cuda=True)
+    @cuda.jit(fastmath=True)
+    def loop(_, r, V, rngs, w, d, tavg, bold_state, bold_out, I, Delta, eta, tau, J, cr, cv, r_sigma, V_sigma):
+        it = cuda.threadIdx.x
+        NT = cuda.blockDim.x * cuda.gridDim.x # for now
         o_tau = nb.float32(1 / tau)
-        # np.seterr(invalid='raise')
+        assert r.shape[0] == V.shape[0] == nn  # shape asserts help numba optimizer
+        assert r.shape[1] == V.shape[1] == nh
+        nrV = cuda.shared.array((2, 32), nb.float32)
+        for j in range(nto):
+            for i in range(nn):
+                tavg[j, 0, i, it] = nb.float32(0.0)
+                tavg[j, 1, i, it] = nb.float32(0.0)
+        for t0 in range(-1, nh - 1):
+            t = nh-1 if t0<0 else t0
+            t1 = t0 + 1
+            t0_nto = t0 // (nh // nto)
+            for i in range(nn):
+                rc = nb.float32(0) # using array here costs 50%+
+                Vc = nb.float32(0)
+                for j in range(nn):
+                    dij = (t - d[i, j] + nh) & (nh-1)
+                    rc += w[i, j] * cfpre(r[dij, j, it], r[t, i, it])
+                    Vc += w[i, j] * cfpre(V[dij, j, it], V[t, i, it])
+                rc = cfpost(rc)
+                Vc = cfpost(Vc)
+                # can't put it in a device function..
+                pi_2 = nb.float32(np.pi * 2)
+                u1 = xoroshiro128p_uniform_float32(rngs, t1*NT*nn*2 + i*NT*2 + it)
+                u2 = xoroshiro128p_uniform_float32(rngs, t1*NT*nn*2 + i*NT*2 + it + NT)
+                z0 = math.sqrt(-nb.float32(2.0) * math.log(u1)) * math.cos(pi_2 * u2)
+                z1 = math.sqrt(-nb.float32(2.0) * math.log(u1)) * math.sin(pi_2 * u2)
+                rk4_rV(it, nrV, r[t, i, it], V[t, i, it],
+                       o_tau, pi, tau, Delta, eta, J, I, cr, rc, cv, Vc,
+                       r_sigma, V_sigma, z0, z1)
+                r[t1, i, it] = nrV[0, it]
+                V[t1, i, it] = nrV[1, it]
+                tavg[t0_nto, 0, i, it] += r[t1, i, it] * o_nh
+                tavg[t0_nto, 1, i, it] += V[t1, i, it] * o_nh
+                bold_out[i, it] = fmri(bold_state[i, it], tavg[0, 0, i, it], dt)
+    return loop
+
+
+def make_loop(nh, nto, nn, dt, cfpre, cfpost):
+    nh, nn, dt, pi, sqrt_dt, o_nh, o_6 = setup_const(nh, nto, nn, dt)
+    rk4_rV = make_rk4_rV(dt, sqrt_dt, o_6)
+    @nb.njit(boundscheck=False, fastmath=True)
+    def loop(nrV, r, V, wrV, w, d, tavg, bold_state, bold_out, I, Delta, eta, tau, J, cr, cv, r_sigma, V_sigma):
+        o_tau = nb.float32(1 / tau)
         assert r.shape[0] == V.shape[0] == nh  # shape asserts help numba optimizer
         assert r.shape[1] == V.shape[1] == nn
         for j in range(nto):
@@ -131,19 +129,11 @@ def make_loop(nh, nto, nn, dt, cfpre, cfpost):
                     Vc += w[i, j] * cfpre(V[dij, j], V[t, i])
                 rc = cfpost(rc)
                 Vc = cfpost(Vc)
-                dr_0 = dr(r[t, i], V[t, i], o_tau, pi, tau, Delta)
-                dV_0 = dV(r[t, i], V[t, i], o_tau, pi, tau, eta, J, I, cr, rc, cv, Vc)
-                kh = nb.float32(0.5)
-                dr_1 = dr(r[t, i] + dt*kh*dr_0, V[t, i] + dt*kh*dV_0, o_tau, pi, tau, Delta)
-                dV_1 = dV(r[t, i] + dt*kh*dr_0, V[t, i] + dt*kh*dV_0, o_tau, pi, tau, eta, J, I, cr, rc, cv, Vc)
-                dr_2 = dr(r[t, i] + dt*kh*dr_1, V[t, i] + dt*kh*dV_1, o_tau, pi, tau, Delta)
-                dV_2 = dV(r[t, i] + dt*kh*dr_1, V[t, i] + dt*kh*dV_1, o_tau, pi, tau, eta, J, I, cr, rc, cv, Vc)
-                kh = nb.float32(1.0)
-                dr_3 = dr(r[t, i] + dt*kh*dr_2, V[t, i] + dt*kh*dV_2, o_tau, pi, tau, Delta)
-                dV_3 = dV(r[t, i] + dt*kh*dr_2, V[t, i] + dt*kh*dV_2, o_tau, pi, tau, eta, J, I, cr, rc, cv, Vc)
-                r[t1, i] = r[t, i] + o_6*dt*(dr_0 + 2*(dr_1 + dr_2) + dr_3) + sqrt_dt * r_sigma * wrV[0, t1, i]
-                r[t1, i] *= r[t1, i] > 0
-                V[t1, i] = V[t, i] + o_6*dt*(dV_0 + 2*(dV_1 + dV_2) + dV_3) + sqrt_dt * V_sigma * wrV[1, t1, i]
+                rk4_rV(0, nrV, r[t, i], V[t, i],
+                       o_tau, pi, tau, Delta, eta, J, I, cr, rc, cv, Vc,
+                       r_sigma, V_sigma, wrV[0, t1, i], wrV[1, t1, i])
+                r[t1, i] = nrV[0, 0]
+                V[t1, i] = nrV[1, 0]
                 tavg[t0_nto, 0, i] += r[t1, i] * o_nh
                 tavg[t0_nto, 1, i] += V[t1, i] * o_nh
                 bold_out[i] = fmri(bold_state[i], tavg[0, 0, i], dt)
--- scientific_library/tvb/simulator/models/fast_montbrio.py
+++ scientific_library/tvb/simulator/models/fast_montbrio.py
@@ -2,6 +2,7 @@ import tqdm
 import numpy as np
 import numba as nb
 from numpy.random import SFC64
+from numba.cuda.random import create_xoroshiro128p_states
 
 from tvb.simulator._numba.montbrio import make_gpu_loop, make_loop
 from tvb.simulator._ispc.montbrio import run_ispc_montbrio
@@ -39,9 +40,9 @@ def run_loop(weights, delays,
     assert d.max() < nh
     # inner loop setup dimensions, constants, buffers
     r, V = rV = np.zeros((2, nh, nn), 'f')
+    nrV = np.zeros((2, 1), 'f')  # no stack arrays in Numba
     icfun(-np.r_[:nh]*dt, rV)
     I, Delta, eta, tau, J, cr, cv, r_sigma, V_sigma = [nb.float32(_) for _ in (I, Delta, eta, tau, J, cr, cv, r_sigma, V_sigma)]
-    #wrV = np.empty((2, nh, nn), 'f')                            # buffer for noise
     tavg = np.zeros((nto, 2, nn), 'f')                               # buffer for temporal average
     bold_state = np.zeros((nn, 4), 'f')                         # buffer for bold state
     bold_state[:,1:] = 1.0
@@ -61,7 +62,7 @@ def run_loop(weights, delays,
     # start time stepping
     for t in (tqdm.trange if progress else range)(total_wins):
         wrV = rng.standard_normal(size=(2, nh, nn), dtype='f') #, out=wrV)  # ~15% time here
-        loop(r, V, wrV, w, d, tavg, bold_state, bold_out, I, Delta, eta, tau, J, cr, cv, r_sigma, V_sigma)
+        loop(nrV, r, V, wrV, w, d, tavg, bold_state, bold_out, I, Delta, eta, tau, J, cr, cv, r_sigma, V_sigma)
         tavg_trace[t] = tavg
         if t % bold_skip == 0:
             bold_trace[t//bold_skip] = bold_out
@@ -69,52 +70,58 @@ def run_loop(weights, delays,
 
 
 def run_gpu_loop(weights, delays,
-             total_time=60e3, coupling_scaling=0.01,
+             total_time=60e3, bold_tr=1800, coupling_scaling=0.01,
              r_sigma=1e-3, V_sigma=1e-3,
              I=1.0, Delta=1.0, eta=-5.0, tau=100.0, J=15.0, cr=0.01, cv=0.0,
              dt=1.0,
-             nh=32,  # history buf len, must be power of 2 & greater than delays.max()/dt
-             progress=False, nt=16,
+             nh=256,  # history buf len, must be power of 2 & greater than delays.max()/dt
+             nto=16,   # num parts of nh for tavg, e.g. nh=256, nto=4: tavg over 64 steps
+             progress=False,
              icfun=default_icfun):
-    assert nh==32, 'only nh=32 supported'
-    assert weights.shape == delays.shape and weights.shape[0] == weights.shape[1], 'weights delays square plz'
+    assert weights.shape == delays.shape and weights.shape[0] == weights.shape[1]
     nn = weights.shape[0]
-    assert nn%32==0, 'only nn multiple of 32 supported'
-    nc, nl = nn // 32, 32
     w = weights.astype(np.float32)
-    d = (delays / dt).astype(np.uint32)
-    # reshape for j,chunk,lane indexing
-    w_, d_ = [_.T.copy().reshape((nn,nc,nl)) for _ in (w, d)]
-    assert d.max() <= nh, 'max delay must be <= nh'
+    d = (delays / dt).astype(np.int32)
+    assert d.max() < nh
+    nt = 32
     # inner loop setup dimensions, constants, buffers
-    r, V = rV = np.zeros((2, nn, nh), 'f')
+    r, V = rV = np.zeros((2, nh, nn, nt), 'f')
+    nrV = np.zeros((2, nt), 'f')  # no stack arrays in Numba
     icfun(-np.r_[:nh]*dt, rV)
     I, Delta, eta, tau, J, cr, cv, r_sigma, V_sigma = [nb.float32(_) for _ in (I, Delta, eta, tau, J, cr, cv, r_sigma, V_sigma)]
-    tavg = np.zeros((2, nc, nl), 'f')                               # buffer for temporal average
-    bold_state = np.zeros((nn, 4), 'f')                         # buffer for bold state
+    tavg = np.zeros((nto, 2, nn, nt), 'f')                               # buffer for temporal average
+    bold_state = np.zeros((nn, 4, nt), 'f')                         # buffer for bold state
     bold_state[:,1:] = 1.0
-    bold_out = np.zeros((nn,), 'f')                             # buffer for bold output
-    from numba.cuda.random import create_xoroshiro128p_states
-    rng_states = create_xoroshiro128p_states(nn*2*nt, seed=1)
-    loop = make_gpu_loop(nh, nn, nt, dt)
-    sloop = loop.specialize(r, V, w_, d_, tavg, I, Delta, eta, tau, J, cr, cv, r_sigma, V_sigma, rng_states)
-    print(sloop._func.ccinfos[0])
+    bold_out = np.zeros((nn, nt), 'f')                             # buffer for bold output
+    # first call to jit the function
+    cfpre, cfpost = make_linear_cfun(coupling_scaling)
+    loop = make_gpu_loop(nh, nto, nn, dt, cfpre, cfpost)
+    #loop(r, V, wrV, w, d, tavg, bold_state, bold_out, I, Delta, eta, tau, J, cr, cv, r_sigma, V_sigma)
     # outer loop setup
-    win_len = nt * dt
+    win_len = nh * dt
     total_wins = int(total_time / win_len)
-    print(total_time, win_len, total_wins)
-    tavg_trace = np.empty((total_wins, ) + tavg.shape, 'f')
-    # manage gpu memory
+    bold_skip = int(bold_tr / win_len)
+    rngs = create_xoroshiro128p_states(nt * nn * 2 * nh, 42)
+    # pinned memory for speeding up kernel invocations
     from numba.cuda import to_device, pinned_array, device_array_like
-    g_r, g_V, g_w_, g_d_, g_rng_states = [to_device(_) for _ in (r, V, w_, d_, rng_states)]
-    p_tavg = pinned_array(tavg.shape)
-    g_tavg = device_array_like(p_tavg)
+    g_nrV, g_r, g_V, g_rngs, g_w, g_d, g_tavg, g_bold_state, g_bold_out = [
+        to_device(_) for _ in (nrV, r, V, rngs, w, d, tavg, bold_state, bold_out)]
+    p_tavg = pinned_array(tavg.shape, dtype=np.float32)
+    p_bold_out = pinned_array(bold_out.shape, dtype=np.float32)
+    tavg_trace = np.zeros((total_wins, ) + tavg.shape, 'f')
+    bold_trace = np.zeros((total_wins//bold_skip + 1, ) + bold_out.shape, 'f')
     # start time stepping
     for t in (tqdm.trange if progress else range)(total_wins):
-        loop[1, 32](g_r, g_V, g_w_, g_d_, g_tavg, I, Delta, eta, tau, J, cr, cv, r_sigma, V_sigma, g_rng_states)
+        loop[1, 32](g_nrV, g_r, g_V, g_rngs, g_w, g_d, tavg, g_bold_state, g_bold_out,
+                     I, Delta, eta, tau, J, cr, cv, r_sigma, V_sigma)
         g_tavg.copy_to_host(p_tavg)
+        print(tavg)
         tavg_trace[t] = p_tavg
-    return tavg_trace.reshape((total_wins, 2, nn)), None
+        if t % bold_skip == 0:
+            g_bold_out.copy_to_host(p_bold_out)
+            bold_trace[t//bold_skip] = p_bold_out
+    return tavg_trace.reshape((total_wins * nto, 2, nn, nt)), bold_trace
+
 
 
 def grid_search(loop, **params):
@@ -135,21 +142,6 @@ if __name__ == '__main__':
     w = np.random.randn(nn, nn)**2
     d = np.random.rand(nn, nn)**2 * 15
     ns = 60
-    params = dict(dt=1.0, I=1.0, r_sigma=3e-3, V_sigma=1e-3, tau=10.0)
+    params = dict(dt=1.0, total_time=5*60e3, I=1.0, r_sigma=3e-3, V_sigma=1e-3, tau=10.0, progress=True)
 
-    # set the ISA for ISPC, one of sse4 avx1 avx2 or avx512
-    params['isa'] = 'avx2'
-
-    # tavg0, _ = run_loop(w, d, nh=16, nto=1, **params)
-    # tavg1, _ = run_ispc_montbrio(w, d, **params)
-    # from numpy.testing import assert_allclose
-    # assert_allclose(tavg0.reshape((-1, 192)), tavg1, 1e-3, 0.1)
-
-    params = {key: [val] for key, val in params.items()}
-    args, mons = grid_search(run_ispc_montbrio, n_jobs=4,
-        weights=[w], delays=[d], total_time=[5e3],  # non-varying into single elem list
-        cr=np.r_[:0.1:16j], cv=np.r_[:0.1:16j],        # varying as arrays/lists
-        **params
-    )
-    # for args, (tavg, bold) in zip(args, mons):
-    #     print('cr/cv: ', args['cr'], '/', args['cv'], ', tavg std:', tavg[-100:].std())
+    run_gpu_loop(w, d, **params)
