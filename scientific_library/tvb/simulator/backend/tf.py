9c79c9febfe153df3bc6cb0fc1df99b930c9fe8f
Marmaduke Woodman <mmwoodman@gmail.com>
2020-12-02 16:17:43 +0100

    restructure with tf modules

--- scientific_library/tvb/simulator/backend/tf.py
+++ scientific_library/tvb/simulator/backend/tf.py
@@ -6,65 +6,86 @@ import numpy as np
 import tensorflow as tf
 import tqdm
 
-# mainly testing TF for parsweeps here, not yet developed
-# into a proper backend
+# quick switch from compiled to not
+jit = tf.function if 1 else lambda f: f
 
-def make_odes(tau, Delta, eta, J, I, cr, cv):
-    o_tau, pi = 1/tau, np.pi
-    o_tau, pi, tau, Delta, eta, J, I, cr, cv = [tf.constant(_) for _ in (o_tau, pi, tau, Delta, eta, J, I, cr, cv)]
-    @tf.function
-    def odes(x, rc):
+# TF modules play an infrastructure/instrumenting role in TF like traits in TVB
+# Structuring code with modules should allow considerable flexiblity afterwards.
+
+class MontbrioDfun(tf.Module):
+    def __init__(self, tau=100.0, Delta=1.0, eta=-5.0, J=15.0, I=1.0, cr=0.01, cv=0.0):
+        super().__init__()
+        self.pi = tf.constant(np.pi, tf.float32)
+        self.tau = tf.Variable(tau, dtype=tf.float32)
+        self.Delta = tf.Variable(Delta, dtype=tf.float32)
+        self.eta = tf.Variable(eta, dtype=tf.float32)
+        self.J = tf.Variable(J, dtype=tf.float32)
+        self.I = tf.Variable(I, dtype=tf.float32)
+        self.cr = tf.Variable(cr, dtype=tf.float32)
+        self.cv = tf.Variable(cv, dtype=tf.float32)
+    @jit
+    def __call__(self, x, c):
+        o_tau = 1.0 / self.tau
         r, V = x[0], x[1]
-        Vc = tf.constant(0.0)
-        dr = o_tau * (Delta / (pi * tau) + 2 * V * r)
-        dV = o_tau * (V ** 2 - (pi ** 2) * (tau ** 2) * (r ** 2) + eta + J * tau * r + I + cr * rc + cv * Vc)
+        rc, Vc = c[0], c[1]
+        dr = o_tau * (self.Delta / (self.pi * self.tau) + 2 * V * r)
+        dV = o_tau * (V ** 2 - (self.pi ** 2) * (self.tau ** 2) * (r ** 2) + self.eta +
+                      self.J * self.tau * r + self.I + self.cr * rc + self.cv * Vc)
         return tf.stack([dr, dV])
-    return odes
-
-def make_rk4(dt, f, n_steps=10):
-    dt = tf.constant(dt)
-    sqrt_dt = tf.sqrt(dt)
-    @tf.function
-    def rk4_step(x, z, w, sigma):
-        nsv, nt1, nt2, nn, nb = x.shape
-        rc = tf.reduce_sum(w * tf.reshape(x[0], (nt1, nt2, 1, nn, nb)), axis=-2)
-        k1 = f(x, rc)
-        k2 = f(x + dt/2*k1, rc)
-        k3 = f(x + dt/2*k2, rc)
-        k4 = f(x + dt*k3, rc)
-        nx = x + dt/6*(k1 + 2*(k2 + k3) + k4) + sqrt_dt*sigma*z
-        return nx
-    @tf.function
-    def rk4(x, z, w, sigma):
-        for i in range(n_steps):
-            x = rk4_step(x, z[i], w, sigma)
-        return x
-    return rk4
 
 
-odes = make_odes(tau=100.0, Delta=1.0, eta=-5.0, J=15.0, I=1.0, cr=0.01, cv=0.0)
-rk4 = make_rk4(0.1, odes)
+class RK4(tf.Module):
+    def __init__(self, dt, dfun):
+        super().__init__()
+        self.dt = tf.constant(dt, tf.float32)
+        self.dfun = dfun
+    @jit
+    def __call__(self, x, c):
+        k1 = self.dfun(x, c)
+        k2 = self.dfun(x + self.dt/2*k1, c)
+        k3 = self.dfun(x + self.dt/2*k2, c)
+        k4 = self.dfun(x + self.dt*k3, c)
+        return x + self.dt/6*(k1 + 2*(k2 + k3) + k4)
 
-# dims & grid
-nn = 96
-nt1, nt2 = 2, 2
-nb = 1
 
-# doesn't hit linear scaling until 32 threads, but then CPU's using multiple cores
-# for 16x16, using all 4 cores, around 4 hours for 10 min bold, single subjects
+class LinearCfun(tf.Module):
+    def __init__(self, weights, a, b):
+        super().__init__()
+        self.weights = tf.Variable(weights, tf.float32)
+        self.a = tf.Variable(a, tf.float32)
+        self.b = tf.Variable(b, tf.float32)
+    @jit
+    def __call__(self, x):
+        gx = tf.matmul(w, x, transpose_b=True)
+        nsv, nb, nn, _ = gx.shape
+        gx = tf.reshape(gx, (nsv, nb, 1, nn))
+        assert x.shape == gx.shape
+        return self.a * gx + self.b
 
-# state buffer
-x = tf.zeros((2, nt1, nt2, nn, nb)) - tf.reshape(tf.constant([0.0, 2.0]), (2,1,1,1,1))
-# generate random log normal weights then max normalized
-w = tf.exp(tf.random.normal((nt1, nt2, nn, nn, nb)))
-w = w / tf.reduce_max(tf.reduce_max(w, axis=-1, keepdims=True), axis=-1, keepdims=True)
 
-# parameters to sweep, need to broadcast to grid above
-cr = tf.convert_to_tensor(10**np.r_[-3:-1:1j*nt1], tf.float32)
-cr = tf.reshape(cr, (nt1, 1, 1, 1))
-sigma = tf.convert_to_tensor(10**np.r_[-3:-1:1j*nt2], tf.float32)
-sigma = tf.reshape(sigma, (nt2, 1, 1))
+class Stepper(tf.Module):
+    def __init__(self, x0, dt, n_steps, dfun, cfun, scheme=RK4):
+        super().__init__()
+        self.n_steps = tf.constant(n_steps)
+        self.scheme = scheme(dt, dfun)
+        self.cfun = cfun
+        self.tavg = tf.Variable(tf.zeros_like(x0))
+    @jit
+    def __call__(self, x):
+        self.tavg.assign(tf.zeros_like(x))
+        for i in range(self.n_steps):
+            x = self.scheme(x, self.cfun(x))
+            self.tavg.assign_add(x)
+        return x, self.tavg/tf.cast(self.n_steps, tf.float32)
+
+nn = 96  # network size
+nb = 16  # batch size
+w = tf.random.normal((nb, nn, nn))
+x = tf.zeros(     (2, nb,  1, nn), tf.float32)
+
+dfun = MontbrioDfun()
+cfun = LinearCfun(w, 0.1, 0.0)
+stepper = Stepper(x, 0.1, 10, dfun, cfun)
 
 for i in tqdm.trange(int(600*1e3)):
-    z = tf.random.normal((10, 2, nt1, nt2, nn, nb))
-    r, V = rk4(x, z, w, sigma)
+    x, tavg = stepper(x)
