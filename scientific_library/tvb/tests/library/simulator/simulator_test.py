99fd0da4503758c82bff7671c5a1d7230a797cc1
dionperd <dionperd@gmail.com>
2020-12-08 20:15:56 +0100

    Corrected simulator_test.py after last corrections

--- scientific_library/tvb/tests/library/simulator/simulator_test.py
+++ scientific_library/tvb/tests/library/simulator/simulator_test.py
@@ -272,9 +272,7 @@ class TestSimulator(BaseTestCase):
         test_simulator.integrator.configure()
         test_simulator._configure_integrator_next_step()
         assert test_simulator.integrate_next_step == test_simulator.integrator.integrate_with_update
-        assert test_simulator.model.state_variables_mask == \
-               [var in test_simulator.model.integration_variables for var in test_simulator.model.state_variables]
-        assert test_simulator.model.state_variables_mask == [True, True, True, False, False]
+        assert test_simulator.model.nintvar == len(test_simulator.model.integration_variables)
 
     @pytest.mark.parametrize('default_connectivity', [True, False])
     def test_simulator_regional_stimulus(self, default_connectivity):
