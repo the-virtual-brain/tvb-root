ddfaf2ef009ee312081a81c0c2e46fbd5d299873
dionperd <dionperd@gmail.com>
2020-12-21 17:32:27 +0100

    Substitued model n_nonintvar with n_intvar

--- scientific_library/tvb/simulator/models/base.py
+++ scientific_library/tvb/simulator/models/base.py
@@ -48,6 +48,7 @@ class Model(HasTraits):
     non_integrated_variables = None  # type: typing.Tuple[str]
     variables_of_interest = ()
     _nvar = None   # todo make this a prop len(state_variables)
+    _n_intvar = _nvar
     _n_nonintvar = 0
     number_of_modes = 1
     cvar = None
@@ -103,7 +104,7 @@ class Model(HasTraits):
         if self.non_integrated_variables is not None:
             for var in self.non_integrated_variables:
                 self.state_variable_mask[self.state_variables.index(var)] = False
-        self._n_nonintvar = self.nvar - numpy.sum(self.state_variable_mask)
+        self._n_intvar = numpy.sum(self.state_variable_mask)
         self.update_derived_parameters()
         self._build_observer()
 
@@ -113,9 +114,9 @@ class Model(HasTraits):
         return self._nvar
 
     @property
-    def n_nonintvar(self):
+    def n_intvar(self):
         """ The number of integrated state variables in this model. """
-        return self._n_nonintvar
+        return self._n_intvar
 
     def update_derived_parameters(self):
         """
