552c9e13e5b705c03f2ad0b552d7a9b5d9bf6e51
popaula937 <paula.popa@codemart.ro>
2020-12-22 21:56:10 +0200

    TVB-2856. Prepare version numbers and release notes.

--- scientific_library/tvb/basic/config/settings.py
+++ scientific_library/tvb/basic/config/settings.py
@@ -45,7 +45,7 @@ class VersionSettings(object):
     """
 
     # Current release number
-    BASE_VERSION = "2.0.10"
+    BASE_VERSION = "2.1a1"
 
     # Current DB version. Increment this and create a new xxx_update_db.py migrate script
     DB_STRUCTURE_VERSION = 18
