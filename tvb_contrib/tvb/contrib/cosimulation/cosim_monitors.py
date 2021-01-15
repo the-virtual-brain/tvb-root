f994a8f4ca068056607c770db8013fe2db00b04d
dionperd <dionperd@gmail.com>
2021-01-15 17:06:31 +0100

    Refactored. CosimMonitor inheriting from Raw. Removed RawVoiDelayed

--- tvb_contrib/tvb/contrib/cosimulation/cosim_monitors.py
+++ tvb_contrib/tvb/contrib/cosimulation/cosim_monitors.py
@@ -5,27 +5,14 @@ import abc
 import numpy
 
 from tvb.basic.neotraits.api import Attr, NArray
-from tvb.simulator.coupling import Coupling,Linear
-from tvb.simulator.monitors import Monitor, Raw, RawVoi
+from tvb.simulator.coupling import Coupling, Linear
+from tvb.simulator.monitors import Raw
 
 
-class CosimMonitor(Monitor):
+class CosimMonitor(Raw):
     """
     Abstract base class for monitor implementations.
     """
-
-    def config_for_sim(self, simulator):
-        """Configure monitor for given simulator.
-
-        Grab the Simulator's integration step size. Set the monitor's variables
-        of interest based on the Monitor's 'variables_of_interest' attribute, if
-        it was specified, otherwise use the 'variables_of_interest' specified
-        for the Model. This method is called from within
-        the the Simulator's configure() method.
-
-        """
-        self._config_vois(simulator)
-        self.dt = simulator.integrator.dt
 
     @abc.abstractmethod
     def sample(self, start_step, n_steps,history_incomplete,history_delayed):
@@ -36,7 +23,7 @@ class CosimMonitor(Monitor):
         pass
 
 
-class RawDelayed(CosimMonitor, Raw):
+class RawDelayed(CosimMonitor):
     """
     A monitor that records the output raw data from the full history of a tvb simulation:
     It collects:
@@ -49,12 +36,6 @@ class RawDelayed(CosimMonitor, Raw):
 
     _ui_name = "Cosimulation Raw Delayed recording"
 
-    def _config_vois(self, simulator):
-        Raw._config_vois(self, simulator)
-
-    def config_for_sim(self, simulator):
-        CosimMonitor.config_for_sim(self, simulator)
-
     def sample(self, start_step, n_steps, cosim_history, history):
         " Return the states of the delayed (by synchronization time) TVB history "
         times = []
@@ -65,37 +46,7 @@ class RawDelayed(CosimMonitor, Raw):
         return [numpy.array(times),numpy.array(values)]
 
 
-class RawVoiDelayed(CosimMonitor, RawVoi):
-    """
-        A monitor that records selected vois of output raw data from the full history of a tvb simulation:
-        It collects:
-
-            - selected state variables and all modes from class :Model:
-            - all nodes of a region or surface based
-            - all the integration time steps
-
-        """
-
-    _ui_name = "Cosimulation RawVoi Delayed recording"
-
-    def _config_vois(self, simulator):
-        RawVoi._config_vois(self, simulator)
-
-    def config_for_sim(self, simulator):
-        CosimMonitor.config_for_sim(self, simulator)
-
-    def sample(self, start_step, n_steps, cosim_history, history):
-        " Return the states of the delayed (by synchronization time) TVB history "
-        times = []
-        values = []
-        for step in range(start_step, start_step + n_steps):
-            times.append(step * self.dt)
-            values.append(history.query_sparse(step)[0][:, self.voi])
-        return [numpy.array(times), numpy.array(values)]
-
-
-
-class RawCosim(CosimMonitor, Raw):
+class RawCosim(CosimMonitor):
     """
     A monitor that records the output raw data from the incomplete history of tvb simulation:
     It collects:
@@ -107,12 +58,6 @@ class RawCosim(CosimMonitor, Raw):
     """
 
     _ui_name = "Cosimulation Raw recording"
-
-    def _config_vois(self, simulator):
-        Raw._config_vois(self, simulator)
-
-    def config_for_sim(self, simulator):
-        CosimMonitor.config_for_sim(self, simulator)
 
     def sample(self, start_step, n_steps, cosim_history, history):
         "Return the states of the incomplete (up to synchronization time) cosimulation history "
@@ -140,7 +85,7 @@ class CosimCoupling(CosimMonitor):
 
     variables_of_interest = NArray(
         dtype=int,
-        label="See only the coupling variable!!! Resistance is futile...",
+        label="Set only the coupling variable!!! Resistance is futile...",
         required=False)
 
     coupling = Attr(
