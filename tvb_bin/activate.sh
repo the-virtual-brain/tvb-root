60b2f56b02c7ef1993d3b005ef396de47bddf50c
liadomide <lia_domide@yahoo.com>
2021-02-16 15:30:43 +0200

    TVB-2867 Small adjustment

--- tvb_bin/activate.sh
+++ tvb_bin/activate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export PYTHONPATH=$(pwd)/../scientific_library:$(pwd)/../framework_tvb:$(pwd)/../tvb_data:$(pwd):$PYTHONPATH
+export PYTHONPATH=$(pwd)/../scientific_library:$(pwd)/../framework_tvb:$(pwd)/../tvb_contrib:$(pwd):$PYTHONPATH
