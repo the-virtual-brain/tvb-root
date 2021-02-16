425392935f7ef1c29ac7d12786edb2a7e00536d0
bvalean <bogdan.valean@codemart.ro>
2021-02-16 13:14:42 +0200

    TVB-2880 Code review

--- framework_tvb/tvb/adapters/exporters/export_manager.py
+++ framework_tvb/tvb/adapters/exporters/export_manager.py
@@ -68,6 +68,7 @@ class ExportManager(object):
         self._register_exporter(TVBExporter())
         self._register_exporter(TVBLinkedExporter())
         self.export_folder = os.path.join(TvbProfile.current.TVB_STORAGE, self.EXPORT_FOLDER_NAME)
+        self.files_helper = FilesHelper()
 
     def _register_exporter(self, exporter):
         """
@@ -144,7 +145,6 @@ class ExportManager(object):
         return export_data
 
     def _export_linked_datatypes(self, project, zip_file):
-        files_helper = FilesHelper()
         linked_paths = ProjectService().get_linked_datatypes_storage_path(project)
 
         if not linked_paths:
@@ -160,7 +160,7 @@ class ExportManager(object):
         op.start_now()
         op.mark_complete(model_operation.STATUS_FINISHED)
 
-        op_folder = files_helper.get_operation_folder(op.project.name, op.id)
+        op_folder = self.files_helper.get_operation_folder(op.project.name, op.id)
         op_folder_name = os.path.basename(op_folder)
 
         # add linked datatypes to archive in the import operation
@@ -169,7 +169,7 @@ class ExportManager(object):
             zip_file.write(pth, zip_pth)
 
         # remove these files, since we only want them in export archive
-        files_helper.remove_folder(op_folder)
+        self.files_helper.remove_folder(op_folder)
 
     def export_project(self, project, optimize_size=False):
         """
@@ -180,8 +180,7 @@ class ExportManager(object):
         if project is None:
             raise ExportException("Please provide project to be exported")
 
-        files_helper = FilesHelper()
-        project_folder = files_helper.get_project_folder(project)
+        project_folder = self.files_helper.get_project_folder(project)
         project_datatypes = dao.get_datatypes_in_project(project.id, only_visible=optimize_size)
         to_be_exported_folders = []
         considered_op_ids = []
@@ -192,7 +191,7 @@ class ExportManager(object):
             for dt in project_datatypes:
                 op_id = dt.fk_from_operation
                 if op_id not in considered_op_ids:
-                    to_be_exported_folders.append({'folder': files_helper.get_project_folder(project, str(op_id)),
+                    to_be_exported_folders.append({'folder': self.files_helper.get_project_folder(project, str(op_id)),
                                                    'archive_path_prefix': str(op_id) + os.sep,
                                                    'exclude': folders_to_exclude})
                     considered_op_ids.append(op_id)
@@ -221,7 +220,7 @@ class ExportManager(object):
             # Make sure the Project.xml file gets copied:
             if optimize_size:
                 self.logger.debug("Done linked, now we write the project xml")
-                zip_file.write(files_helper.get_project_meta_file_path(project.name), files_helper.TVB_PROJECT_FILE)
+                zip_file.write(self.files_helper.get_project_meta_file_path(project.name), self.files_helper.TVB_PROJECT_FILE)
             self.logger.debug("Done, closing")
 
         return result_path
@@ -247,8 +246,7 @@ class ExportManager(object):
                                              now.minute, now.second, now.microsecond)
         tmp_str = date_str + "@" + data.gid
         data_export_folder = os.path.join(self.export_folder, tmp_str)
-        files_helper = FilesHelper()
-        files_helper.check_created(data_export_folder)
+        self.files_helper.check_created(data_export_folder)
 
         return data_export_folder
 
@@ -257,7 +255,7 @@ class ExportManager(object):
         if burst is None:
             raise InvalidExportDataException("Could not find burst with ID " + str(burst_id))
 
-        op_folder = FilesHelper().get_project_folder(burst.project, str(burst.fk_simulation))
+        op_folder = self.files_helper.get_project_folder(burst.project, str(burst.fk_simulation))
         tmp_export_folder = self._build_data_export_folder(burst)
         tmp_sim_folder = os.path.join(tmp_export_folder, self.EXPORTED_SIMULATION_NAME)
 
@@ -271,7 +269,7 @@ class ExportManager(object):
 
         for vm_path in all_view_model_paths:
             dest = os.path.join(tmp_sim_folder, os.path.basename(vm_path))
-            FilesHelper().copy_file(vm_path, dest)
+            self.files_helper.copy_file(vm_path, dest)
 
         main_vm_path = h5.determine_filepath(burst.simulator_gid, tmp_sim_folder)
         H5File.remove_metadata_param(main_vm_path, 'history_gid')
@@ -284,5 +282,5 @@ class ExportManager(object):
         with TvbZip(result_path, "w") as zip_file:
             for filename in os.listdir(tmp_sim_folder):
                 zip_file.write(os.path.join(tmp_sim_folder, filename), filename)
-        FilesHelper().remove_folder(tmp_sim_folder)
+        self.files_helper.remove_folder(tmp_sim_folder)
         return result_path
