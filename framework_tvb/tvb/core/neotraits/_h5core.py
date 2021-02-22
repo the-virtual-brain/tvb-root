3d914dab30c430f0d3e211b0f1dd1b32f3b46286
bvalean <bogdan.valean@codemart.ro>
2021-02-22 15:55:38 +0200

    HBP-113 Code review

--- framework_tvb/tvb/core/neotraits/_h5core.py
+++ framework_tvb/tvb/core/neotraits/_h5core.py
@@ -40,9 +40,7 @@ from tvb.basic.logger.builder import get_logger
 from tvb.basic.neotraits.api import Final
 from tvb.basic.neotraits.api import HasTraits, Attr, List, NArray, Range
 from tvb.basic.neotraits.ex import TraitFinalAttributeError
-from tvb.core.entities.file.data_encryption_handler import encryption_handler
 from tvb.core.entities.file.exceptions import MissingDataSetException
-from tvb.core.entities.file.files_helper import FilesHelper
 from tvb.core.entities.file.hdf5_storage_manager import HDF5StorageManager
 from tvb.core.entities.generic_attributes import GenericAttributes
 from tvb.core.neotraits.h5 import EquationScalar, SparseMatrix, ReferenceList
