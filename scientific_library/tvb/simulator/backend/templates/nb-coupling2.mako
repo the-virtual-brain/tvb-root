c874ed23bbb65f6cc0a4fdbeefe3a9a3a590add1
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2021-03-30 10:17:08 +0200

    tmpl per cvar fn with foward in time scheme

---
+++ scientific_library/tvb/simulator/backend/templates/nb-coupling2.mako
@@ -0,0 +1,22 @@
+import math
+import numpy as np
+import numba as nb
+
+sin, cos, exp = math.sin, math.cos, math.exp
+
+% for cvar, cterm in zip(sim.model.cvar, sim.model.coupling_terms):
+@nb.jit(boundscheck=True, inline='always')
+def cx_${cterm}(t, i, weights, state, di):
+% for par in sim.coupling.parameter_names:
+    ${par} = nb.float32(${getattr(sim.coupling, par)[0]})
+% endfor
+    gx = nb.float32(0.0)
+    for j in range(weights.shape[0]):
+        wij = nb.float32(weights[i, j])
+        if (wij == nb.float32(0.0)):
+            continue
+        x_i = state[${cvar}, i, t]
+        x_j = state[${cvar}, j, t - di[i, j]]
+	gx += wij * ${sim.coupling.pre_expr}
+    return ${sim.coupling.post_expr}
+% endfor
--- scientific_library/tvb/tests/library/simulator/backend/nbbackend_test.py
+++ scientific_library/tvb/tests/library/simulator/backend/nbbackend_test.py
@@ -47,23 +47,30 @@ class TestNbCoupling(BaseTestCoupling):
         "This variant tests per node cfun kernel."
         sim = self._prep_sim(cfun)
         # prep & invoke kernel
-        template = f'''import numpy as np
-<%include file="nb-coupling2.py.mako"/>
+        template = '''import numpy as np
+<%include file="nb-coupling2.mako"/>
+def kernel(t, cx, weights, state, di):
+    for i in range(weights.shape[0]):
+% for cvar, cterm in zip(sim.model.cvar, sim.model.coupling_terms):
+        cx[${loop.index},i] = cx_${cterm}(t, i, weights, state, di)
+% endfor
 '''
         kernel = NbBackend().build_py_func(template, dict(sim=sim, np=np), 
-            name='coupling', print_source=True)
+            print_source=True)
         fill = np.r_[:sim.history.buffer.size]
         fill = np.reshape(fill, sim.history.buffer.shape[:-1])
         sim.history.buffer[..., 0] = fill
         sim.current_state[:] = fill[0,:,:,None]
-        buf = sim.history.buffer[...,0]
-        # kernel has history in reverse order except 1st element 🤕
-        rbuf = np.concatenate((buf[0:1], buf[1:][::-1]), axis=0)
-        state = np.transpose(rbuf, (1, 0, 2)).astype('f')
+        # [current_state, oldest, next oldest, ..., current - 1]
+        buf = sim.history.buffer[...,0] # (horizon, svar, node)
+        horizon, nsvar, nnode = buf.shape
+        # switch to Jan's history layout here, time increasing with index
+        buf2 = np.zeros((nsvar, nnode, horizon + 10))
+        buf2[:,:,:horizon-1] = np.transpose(buf[1:], (1,2,0))
+        buf2[:,:, horizon-1] = buf[0]
         weights = sim.connectivity.weights.astype('f')
-        cX = np.zeros_like(state[:,0])
-        for i in range(weights.shape[0]):
-            kernel(i, cX[:,i], weights, state, sim.connectivity.idelays)
+        cX = np.zeros((nsvar,nnode))
+        kernel(horizon-1, cX, weights, buf2, sim.connectivity.idelays)
         # do comparison
         (t, y), = sim.run()
         np.testing.assert_allclose(cX, y[0,:,:,0], 1e-5, 1e-6)
