73aec7aadb775cf4344186de65044bbffb353c75
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2021-03-30 09:15:31 +0200

    split out nb backend & fix coupling test w/ delays

---
+++ scientific_library/tvb/tests/library/simulator/backend/nbbackend_test.py
@@ -0,0 +1,43 @@
+"""
+Tests for the Numba backend.
+
+.. moduleauthor:: Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
+
+"""
+
+import numpy as np
+
+from tvb.simulator.coupling import Sigmoidal, Linear
+from tvb.simulator.backend.nb import NbBackend
+
+from .backendtestbase import BaseTestCoupling
+
+
+class TestNbCoupling(BaseTestCoupling):
+
+    def _test_cfun(self, mode, cfun):
+        "Test a Python cfun template."
+        sim = self._prep_sim(cfun)
+        # prep & invoke kernel
+        template = f'''import numpy as np
+<%include file="{mode}-coupling.mako"/>
+'''
+        kernel = NbBackend().build_py_func(template, dict(sim=sim, np=np), 
+            name='coupling', print_source=True)
+        fill = np.r_[:sim.history.buffer.size]
+        fill = np.reshape(fill, sim.history.buffer.shape[:-1])
+        sim.history.buffer[..., 0] = fill
+        sim.current_state[:] = fill[0,:,:,None]
+        buf = sim.history.buffer[...,0]
+        # kernel has history in reverse order except 1st element 🤕
+        rbuf = np.concatenate((buf[0:1], buf[1:][::-1]), axis=0)
+        state = np.transpose(rbuf, (1, 0, 2)).astype('f')
+        weights = sim.connectivity.weights.astype('f')
+        cX = np.zeros_like(state[:,0])
+        kernel(cX, weights, state, sim.connectivity.idelays)
+        # do comparison
+        (t, y), = sim.run()
+        np.testing.assert_allclose(cX, y[0,:,:,0], 1e-5, 1e-6)
+
+    def test_nb_linear(self): self._test_cfun('nb', Linear())
+    def test_nb_sigmoidal(self): self._test_cfun('nb', Sigmoidal())
