4aea419542490bf2885b8ebbbfe352ca3314c171
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2021-04-01 17:09:02 +0200

    add benchmarks for mako generated mpr

--- scientific_library/tvb/tests/library/simulator/backend/nbmprperf_test.py
+++ scientific_library/tvb/tests/library/simulator/backend/nbmprperf_test.py
@@ -4,6 +4,7 @@ import numpy as np
 from tvb.simulator import simulator, models, integrators, monitors, noise
 from tvb.datatypes import connectivity
 from tvb.simulator.backend.nb_mpr import NbMPRBackend
+from tvb.simulator.backend.nb import NbBackend
 
 
 def make_sim(sim_len=1000.0):
@@ -12,7 +13,7 @@ def make_sim(sim_len=1000.0):
         model=models.MontbrioPazoRoxin(),
         integrator=integrators.HeunStochastic(
             dt=0.1,
-            noise=noise.Additive(nsig=np.r_[0.001,0.002])),
+            noise=noise.Additive(nsig=np.r_[0.001])),
         monitors=[monitors.Raw()],
         simulation_length=sim_len)
     sim.configure()
@@ -43,3 +44,19 @@ def test_nb_pdq_100ms(benchmark):
         '<%include file="nb-montbrio.py.mako"/>', {}, name='run_sim')
     nstep = int(sim.simulation_length / sim.integrator.dt)
     benchmark(lambda : run_sim(sim, nstep))
+
+
+def test_nb_mako_10ms(benchmark):
+    sim = make_sim(10.0)
+    template = '<%include file="nb-sim.py.mako"/>'
+    content = dict(sim=sim, np=np, debug_nojit=False)
+    kernel = NbBackend().build_py_func(template, content, print_source=True, name='run_sim')
+    benchmark(lambda : kernel(sim))
+
+
+def test_nb_mako_100ms(benchmark):
+    sim = make_sim(100.0)
+    template = '<%include file="nb-sim.py.mako"/>'
+    content = dict(sim=sim, np=np, debug_nojit=False)
+    kernel = NbBackend().build_py_func(template, content, print_source=True, name='run_sim')
+    benchmark(lambda : kernel(sim))
