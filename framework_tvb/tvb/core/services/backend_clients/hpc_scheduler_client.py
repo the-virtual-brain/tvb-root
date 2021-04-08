1e168e25462a8329dc94a02f3798a911bc076593
bvalean <bogdan.valean@codemart.ro>
2021-04-08 14:53:23 +0300

    HBP-151 Review clear cache

--- framework_tvb/tvb/core/services/backend_clients/hpc_scheduler_client.py
+++ framework_tvb/tvb/core/services/backend_clients/hpc_scheduler_client.py
@@ -51,6 +51,7 @@ from tvb.core.entities.storage import dao, OperationDAO
 from tvb.core.neocom import h5
 from tvb.core.services.backend_clients.backend_client import BackendClient
 from tvb.core.services.burst_service import BurstService
+from tvb.core.services.cache_service import cache
 from tvb.core.services.exceptions import OperationException
 from tvb.storage.storage_interface import StorageInterface
 
@@ -435,4 +436,5 @@ class HPCSchedulerClient(BackendClient):
             while not operation_thread.stopped():
                 LOGGER.info("Thread for operation {} is stopping".format(operation_id))
         BurstService().persist_operation_state(operation, STATUS_CANCELED)
+        cache.clear_cache()
         return True
--- framework_tvb/tvb/core/services/backend_clients/standalone_client.py
+++ framework_tvb/tvb/core/services/backend_clients/standalone_client.py
@@ -50,6 +50,7 @@ from tvb.core.entities.storage import dao
 from tvb.core.services.backend_clients.backend_client import BackendClient
 from tvb.core.services.burst_service import BurstService
 from tvb.storage.storage_interface import StorageInterface
+from tvb.core.services.cache_service import cache
 
 LOGGER = get_logger(__name__)
 
@@ -123,6 +124,7 @@ class OperationExecutor(Thread):
         storage_interface.check_and_delete(project_folder)
 
         # Give back empty spot now that you finished your operation
+        cache.clear_cache()
         CURRENT_ACTIVE_THREADS.remove(self)
         LOCKS_QUEUE.put(1)
 
@@ -241,5 +243,5 @@ class StandAloneClient(BackendClient):
 
         # Mark operation as canceled in DB and on disk
         BurstService().persist_operation_state(operation, STATUS_CANCELED)
-
+        cache.clear_cache()
         return stopped
