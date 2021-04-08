9fe87a11106e761d18b6b93e3ac936d5e335ce10
raghib <raghibshams456@gmail.com>
2021-04-08 18:23:11 +0530

    Done profilling

--- framework_tvb/tvb/interfaces/command/benchmark.py
+++ framework_tvb/tvb/interfaces/command/benchmark.py
@@ -34,6 +34,8 @@ Some standardized simulations are run and a report is generated in the console o
 """
 
 from os import path
+from numba import jit
+import memory_profiler
 
 import tvb_data
 from tvb.adapters.datatypes.db.connectivity import ConnectivityIndex
@@ -102,7 +104,7 @@ class Bench(object):
         self.sim_lengths = sim_lengths
         self.running_times = []
         self.coupling = coupling
-
+    @profile
     def run(self, project_id):
         for model in self.models:
             for conn in self.connectivities:
@@ -121,7 +123,7 @@ class Bench(object):
 
                             operation = _fire_simulation(project_id, simulator_vm)
                             self.running_times.append(operation.completion_date - operation.start_date)
-
+    @profile
     def report(self):
         i = 0
         print(HEADER)
