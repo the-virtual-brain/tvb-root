f3475416a439babec17adb93006df594852f48d9
Robert.Vincze <vinczerobert7@gmail.com>
2021-05-28 11:03:06 +0300

    TVB-2868: Correct license in library files.

--- scientific_library/tvb/simulator/_numba/coupling.py
+++ scientific_library/tvb/simulator/_numba/coupling.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #
-#  TheVirtualBrain-Scientific Package. This package holds all simulators, and
+# TheVirtualBrain-Scientific Package. This package holds all simulators, and
 # analysers necessary to run brain-simulations. You can use it stand alone or
 # in conjunction with TheVirtualBrain-Framework Package. See content of the
 # documentation-folder for more details. See also http://www.thevirtualbrain.org
--- scientific_library/tvb/tests/library/simulator/regionmapping_test.py
+++ scientific_library/tvb/tests/library/simulator/regionmapping_test.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 #
 #
-# TheVirtualBrain-Framework Package. This package holds all Data Management, and
-# Web-UI helpful to run brain-simulations. To use it, you also need do download
-# TheVirtualBrain-Scientific Package (for simulators). See content of the
+# TheVirtualBrain-Scientific Package. This package holds all simulators, and
+# analysers necessary to run brain-simulations. You can use it stand alone or
+# in conjunction with TheVirtualBrain-Framework Package. See content of the
 # documentation-folder for more details. See also http://www.thevirtualbrain.org
 #
 # (c) 2012-2022, Baycrest Centre for Geriatric Care ("Baycrest") and others
@@ -25,6 +25,7 @@
 #   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
 #       The Virtual Brain: a simulator of primate brain network dynamics.
 #   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
+#
 #
 
 """
