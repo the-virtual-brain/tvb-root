1576e9501b1c4b1750922c6aa9b4be2f039725d6
Robert.Vincze <vinczerobert7@gmail.com>
2021-06-03 12:45:46 +0300

    TVB-2911: Wrote new db migration script

--- scientific_library/tvb/basic/config/settings.py
+++ scientific_library/tvb/basic/config/settings.py
@@ -52,7 +52,7 @@ class VersionSettings(object):
     BASE_VERSION = "2.2.2"
 
     # Current DB version. Create a new migration script from command line and copy its gid here
-    DB_STRUCTURE_VERSION = 'ec2859bb9114'
+    DB_STRUCTURE_VERSION = '32d4bf9f8cab'
 
     # This is the version of the data stored in H5 and XML files
     # and should be used by next versions to know how to import
