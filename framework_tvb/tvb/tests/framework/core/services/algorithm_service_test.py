e50def692ad9fea3a997529b13acf6afe9582d35
Robert.Vincze <vinczerobert7@gmail.com>
2021-06-25 15:47:54 +0300

    TVB-2939: Fix test.p

--- framework_tvb/tvb/tests/framework/core/services/algorithm_service_test.py
+++ framework_tvb/tvb/tests/framework/core/services/algorithm_service_test.py
@@ -80,7 +80,7 @@ class TestAlgorithmService(TransactionalTestCase):
     def test_get_uploaders(self):
 
         result = AlgorithmService.get_upload_algorithms()
-        assert 19 == len(result)
+        assert 18 == len(result)
         found = False
         for algo in result:
             if algo.classname == self.algorithm.classname and algo.module == self.algorithm.module:
