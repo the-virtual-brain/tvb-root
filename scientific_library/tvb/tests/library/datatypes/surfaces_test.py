bf1a250cdaecb4fd806356d43c9c2c81e37edfcb
popaula937 <paula.popa@codemart.ro>
2021-08-31 21:17:23 +0300

    TVB-2889. Avoid numpy warnings in dummy surface configuration used in tests.

--- scientific_library/tvb/tests/library/datatypes/surfaces_test.py
+++ scientific_library/tvb/tests/library/datatypes/surfaces_test.py
@@ -51,9 +51,10 @@ class TestSurfaces(BaseTestCase):
         dt = surfaces.Surface(valid_for_simulations=True)
         dt.vertices = numpy.array(list(range(30))).reshape(10, 3).astype(numpy.float64)
         dt.triangles = numpy.array(list(range(9))).reshape(3, 3)
+        dt.triangle_normals = numpy.array(list(range(9))).reshape(3, 3)
+        dt.vertex_normals = numpy.array(list(range(30))).reshape(10, 3).astype('f')
 
-        with numpy.errstate(all='ignore'):
-            dt.configure()
+        dt.configure()
 
         summary_info = dt.summary_info()
         assert summary_info['Number of edges'] == 9
@@ -120,9 +121,9 @@ class TestSurfaces(BaseTestCase):
         dt = surfaces.Surface()
         dt.vertices = numpy.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 2]]).astype(numpy.float64)
         dt.triangles = numpy.array([[0, 2, 1], [0, 1, 3], [0, 3, 2], [1, 2, 3]])
-
-        with numpy.errstate(all='ignore'):
-            dt.configure()
+        dt.triangle_normals = numpy.array([[0, 2, 1], [0, 1, 3], [0, 3, 2], [1, 2, 3]])
+        dt.vertex_normals = numpy.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 2]]).astype(numpy.float64)
+        dt.configure()
 
         euler, isolated, pinched_off, holes = dt.compute_topological_constants()
         assert 3 == euler
