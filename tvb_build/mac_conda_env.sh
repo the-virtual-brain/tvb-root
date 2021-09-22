cc655b16d2f0ae9e17c332c9ea2ba2f13a8c09d0
popaula937 <paula.popa@codemart.ro>
2021-09-22 12:23:10 +0300

    Constrains flask version to be compatible with jinja2 version

--- tvb_build/mac_conda_env.sh
+++ tvb_build/mac_conda_env.sh
@@ -6,10 +6,10 @@ conda env remove --name mac-distribution
 conda create -y --name mac-distribution python=3.7 nomkl numba scipy numpy==1.18.1 networkx scikit-learn cython pip numexpr psutil
 conda install -y --name mac-distribution pytest pytest-cov pytest-benchmark pytest-mock matplotlib-base
 conda install -y --name mac-distribution psycopg2 pytables scikit-image==0.14.2 simplejson cherrypy docutils werkzeug
-conda install -y --name mac-distribution -c conda-forge flask==1.1.4 gevent pillow
+conda install -y --name mac-distribution -c conda-forge gevent pillow
 
 /WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install --upgrade pip
-/WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install h5py==2.10 formencode cfflib jinja2==2.11.3 nibabel sqlalchemy alembic allensdk # h5py and jinja2 versions are constarined by allensdk
+/WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install h5py==2.10 formencode cfflib flask==1.1.4 jinja2==2.11.3 nibabel sqlalchemy alembic allensdk # h5py and jinja2 versions are constarined by allensdk
 /WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install tvb-gdist BeautifulSoup4 subprocess32 flask-restx python-keycloak mako pyAesCrypt pyunicore==0.6.0
 /WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install pyobjc
 /WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install biplist six
