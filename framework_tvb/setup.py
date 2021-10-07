ad19684100b46c007a9dd22fd1944eeff3d71e1d
popaula937 <paula.popa@codemart.ro>
2021-10-07 12:12:13 +0300

    TVB-2978. Prepare release notes and version numbers.

--- framework_tvb/setup.py
+++ framework_tvb/setup.py
@@ -40,11 +40,11 @@ import os
 import shutil
 import setuptools
 
-VERSION = "2.3"
+VERSION = "2.4"
 
 TVB_TEAM = "Mihai Andrei, Lia Domide, Stuart Knock, Bogdan Neacsa, Paula Popa, Paula Sansz Leon, Marmaduke Woodman"
 
-TVB_INSTALL_REQUIREMENTS = ["alembic", "allensdk", "cherrypy", "cryptography", "flask", "flask-restx",
+TVB_INSTALL_REQUIREMENTS = ["alembic", "allensdk", "cherrypy", "cryptography", "flask==1.1.4", "flask-restx",
                             "formencode", "gevent", "h5py<3", "Jinja2<2.12.0", "nibabel", "numpy", "pandas",
                             "Pillow", "psutil", "pyAesCrypt", "python-keycloak", "requests", "scikit-learn",
                             "scipy", "simplejson", "six", "sqlalchemy", "tvb-data", "tvb-gdist",
