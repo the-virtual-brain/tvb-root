e5630c89b0c1061b43313b6a2a0a331830e51806
liadomide <lia_domide@yahoo.com>
2022-05-17 11:28:49 +0300

    TVB-2513 h5py can be 3.* as allenSDK has an upgrade

--- tvb_build/mac_conda_env.sh
+++ tvb_build/mac_conda_env.sh
@@ -3,13 +3,13 @@
 #Install dependencies
 conda update -n base -c defaults conda
 conda env remove --name mac-distribution
-conda create -y --name mac-distribution python=3.7 nomkl numba scipy numpy==1.18.1 networkx scikit-learn cython pip numexpr psutil
+conda create -y --name mac-distribution python=3 nomkl numba scipy numpy networkx scikit-learn cython pip numexpr psutil
 conda install -y --name mac-distribution pytest pytest-cov pytest-benchmark pytest-mock pytest-xdist matplotlib-base
 conda install -y --name mac-distribution psycopg2 pytables scikit-image==0.14.2 simplejson cherrypy docutils werkzeug
 conda install -y --name mac-distribution -c conda-forge gevent pillow
 
 /WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install --upgrade pip
-/WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install h5py==2.10 formencode cfflib flask==1.1.4 jinja2==2.11.3 nibabel sqlalchemy alembic allensdk # h5py and jinja2 versions are constarined by allensdk
+/WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install h5py formencode cfflib flask==1.1.4 jinja2==2.11.3 nibabel sqlalchemy alembic allensdk # h5py and jinja2 versions are constarined by allensdk
 /WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install tvb-gdist BeautifulSoup4 subprocess32 flask-restx python-keycloak mako pyAesCrypt pyunicore==0.6.0
 /WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install pyobjc
 /WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install biplist six
