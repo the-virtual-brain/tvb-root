f29b522311526907755d6e692253a5cfdacc0c33
liadomide <lia_domide@yahoo.com>
2022-06-08 23:17:42 +0300

    TVB-2513 Add missing ipywidgets dependency. Order dependencies alphabetically

--- tvb_library/setup.py
+++ tvb_library/setup.py
@@ -43,8 +43,8 @@ LIBRARY_VERSION = "2.6"
 
 LIBRARY_TEAM = "Marmaduke Woodman, Stuart Knock, Paula Sanz Leon, Viktor Jirsa"
 
-LIBRARY_REQUIRED_PACKAGES = ["mako>=1.1.4", "matplotlib", "networkx", "numba", "numexpr", "numpy", 
-                             "requests", "autopep8", "scipy", "six", "pylems", "lxml"]
+LIBRARY_REQUIRED_PACKAGES = ["autopep8", "ipywidgets", "lxml", "mako>=1.1.4", "matplotlib", "networkx",
+                             "numba", "numexpr", "numpy", "pylems", "requests", "scipy", "six"]
 
 LIBRARY_REQUIRED_EXTRA = ["h5py",  "pytest", "pytest-benchmark", "pytest-xdist", "tvb-gdist", "tvb-data"]
 
