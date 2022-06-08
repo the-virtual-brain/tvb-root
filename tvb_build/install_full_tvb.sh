f01f96a0bf8dffb52c1c351aca27b79ec17c10b0
liadomide <lia_domide@yahoo.com>
2022-06-08 23:14:59 +0300

    TVB-2513 When linking current code into python path, use pip instead of the easy_setup deprecated tool

--- tvb_build/install_full_tvb.sh
+++ tvb_build/install_full_tvb.sh
@@ -3,26 +3,21 @@
 # Use this script to install TVB from the main code sources repo, to the current python installation
 
 # set up external dependencies that we maintain as distutils packages in externals/
-cd ..
 
-cd tvb_framework
-python setup.py develop --no-deps --user
-cd ..
+cd ../tvb_framework
+pip install -e . --no-deps --user
 
-cd tvb_library
-python setup.py develop --user
-cd ..
+cd ../tvb_library
+pip install -e . --user
 
-cd tvb_storage
-python setup.py develop --user
-cd ..
+cd ../tvb_storage
+pip install -e . --user
 
-cd tvb_contrib
-python setup.py develop --no-deps --user
-cd ..
+cd ../tvb_contrib
+pip install -e . --no-deps --user
 
-cd tvb_bin
-python setup.py develop --user
+cd ../tvb_bin
+pip install -e . --user
 
 cd ../tvb_build
-python setup.py develop --no-deps --user
+pip install -e . --no-deps --user
