697c0ae55c047fce6b52aa63e46a298cd0b9ee80
liadomide <lia_domide@yahoo.com>
2022-06-10 00:09:57 +0300

    TVB-2513 Revert style

--- tvb_build/install_full_tvb.sh
+++ tvb_build/install_full_tvb.sh
@@ -3,21 +3,27 @@
 # Use this script to install TVB from the main code sources repo, to the current python installation
 
 # set up external dependencies that we maintain as distutils packages in externals/
+cd ..
 
-cd ../tvb_framework
+cd tvb_framework
 pip install -e . --no-deps --user
+cd ..
 
-cd ../tvb_library
+cd tvb_library
 pip install -e . --user
+cd ..
 
-cd ../tvb_storage
+cd tvb_storage
 pip install -e . --user
+cd ..
 
-cd ../tvb_contrib
+cd tvb_contrib
 pip install -e . --no-deps --user
+cd ..
 
-cd ../tvb_bin
+cd tvb_bin
 pip install -e . --user
+cd ..
 
-cd ../tvb_build
+cd tvb_build
 pip install -e . --no-deps --user
