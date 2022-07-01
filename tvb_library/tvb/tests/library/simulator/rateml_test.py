b7c3ec6b10c17b61b849930d78c8430fc9e0c792
DeLaVlag <michielvandervlag@gmail.com>
2022-07-01 15:51:12 +0200

    added demo and zerlaut to tests

--- tvb_library/tvb/tests/library/simulator/rateml_test.py
+++ tvb_library/tvb/tests/library/simulator/rateml_test.py
@@ -93,7 +93,7 @@ def setup_namespace(model='kuramoto'):
 #This line avoid passing script name as a parameters
 check_input_params()
 class TestRateML():
-	models=["epileptor", "kuramoto", "montbrio", "oscillator", "rwongwang"]
+	models=["epileptor", "kuramoto", "montbrio", "oscillator", "rwongwang", "zerlaut"]
 	python_mods = ["python"]*len(models)
 	languages = ["python", "cuda"]
 
