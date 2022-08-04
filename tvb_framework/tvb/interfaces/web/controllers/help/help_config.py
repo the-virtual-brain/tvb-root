58b28fc5c1c6d4ea4297d2694f220ddad3d67a80
romina1601 <romina.baila16@gmail.com>
2022-08-04 18:09:38 +0300

    TVB-3027. Add initial page for interacting with siibra and extracting structural and functional connectivities from EBRAINS KG

--- tvb_framework/tvb/interfaces/web/controllers/help/help_config.py
+++ tvb_framework/tvb/interfaces/web/controllers/help/help_config.py
@@ -110,6 +110,7 @@ class HelpConfig:
         self._add_mapping(WebStructure.SECTION_CONNECTIVITY, WebStructure.SUB_SECTION_CONNECTIVITY, self.CONNECTIVITY_HELP_PAGE, "long-range-connectivity")
         self._add_mapping(WebStructure.SECTION_CONNECTIVITY, WebStructure.SUB_SECTION_LOCAL_CONNECTIVITY, self.CONNECTIVITY_HELP_PAGE, "local-connectivity")
         self._add_mapping(WebStructure.SECTION_CONNECTIVITY, WebStructure.SUB_SECTION_ALLEN, self.CONNECTIVITY_HELP_PAGE, "allen-connectome-downloader")
+        self._add_mapping(WebStructure.SECTION_CONNECTIVITY, WebStructure.SUB_SECTION_SIIBRA, self.CONNECTIVITY_HELP_PAGE, "siibra-connectivities")
 
 
     def _add_mapping(self, section, subsection, page, paragraph):
