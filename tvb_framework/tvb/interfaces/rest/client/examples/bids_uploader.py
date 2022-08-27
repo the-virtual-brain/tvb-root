3d0e4406398240aea9f7ec3c55b52e33668489fd
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2022-08-27 20:34:48 +0530

    fixed bug

--- tvb_framework/tvb/interfaces/rest/client/examples/bids_uploader.py
+++ tvb_framework/tvb/interfaces/rest/client/examples/bids_uploader.py
@@ -35,7 +35,7 @@ delimiter_mapper = {
     ' ': CSVDelimiterOptionsEnum.SPACE
 }
 
-BIDS_UPLOAD_CONTENT = BIDSUploadDataTypeOptionsEnum.FUNCTIONAL_CONNECTIVITY
+BIDS_UPLOAD_CONTENT = BIDSUploadDataTypeOptionsEnum.TIME_SERIES
 BIDS_DIR_NAME = "BIDS_DEMO_DATSET - Copy"
 BIDS_DIR = "C:/Users/upadh/Documents/GitHub/tvb-root/tvb_framework/tvb/interfaces/rest/client/examples/BIDS_DEMO_DATSET - Copy"
 SUBJECT_PREFIX = 'sub'
@@ -75,16 +75,9 @@ def get_abs_path(bids_root_dir, sub, path):
 
 
 def create_archive(files_list, zip_name, base_dir):
-    print("Creating file ziap nasida")
-    print(base_dir)
     base_dir_name = os.path.dirname(os.path.abspath(base_dir))
-    print(base_dir_name)
-    print(os.path.dirname(os.path.abspath(base_dir)))
     with ZipFile(zip_name, 'w') as myzip:
         for file_name in files_list:
-            print(file_name)
-            print(file_name.split(base_dir_name))
-            print(file_name.split(os.path.dirname(os.path.abspath(base_dir))))
             myzip.write(file_name, arcname=file_name.split(base_dir_name)[1])
 
 
@@ -95,8 +88,6 @@ def create_bids_dataset(bids_data_to_import, bids_root_dir, bids_file_name):
     temp_bids_dir_name = bids_file_name + '-' + str(uuid.uuid4()).split("-")[4]
     temp_bids_zip_dir = os.path.join(bids_file_base_dir, temp_bids_dir_name) + '.zip'
 
-    print("zip dir {}".format(temp_bids_zip_dir))
-    
     files = os.listdir(bids_root_dir)
     subject_folders = []
 
@@ -105,13 +96,10 @@ def create_bids_dataset(bids_data_to_import, bids_root_dir, bids_file_name):
         if os.path.basename(file_name).startswith(SUBJECT_PREFIX) and os.path.isdir(os.path.join(bids_root_dir, file_name)):
             subject_folders.append(file_name)
     
-    print(subject_folders)
     logger.info("Found {} subject folders in the".format(len(subject_folders)))
     # For each subject we read its content and sub-dirs
     for sub in subject_folders:
         sub_contents_path = bids_root_dir + '/' + sub + '/' + bids_data_to_import.value
-        print(sub_contents_path)
-        print(os.path.abspath(os.path.join(bids_root_dir, sub, bids_data_to_import.value)))
         sub_contents_path = os.path.abspath(os.path.join(bids_root_dir, sub, bids_data_to_import.value))
         sub_contents = os.listdir(sub_contents_path)
         if len(sub_contents) == 0:
@@ -126,39 +114,14 @@ def create_bids_dataset(bids_data_to_import, bids_root_dir, bids_file_name):
 
         # Addding path of all files present in the alg(to import) dir
         for file_name in sub_contents:
-            import_dependencies_paths.add(get_abs_path(bids_root_dir, sub, os.path.join(sub_contents_path, file_name)))
-        
-        print(import_dependencies_paths)
-
-        json_files = [file_name for file_name in import_dependencies_paths if '.json' in os.path.splitext(file_name)[1]]
-
-        print(json_files)
-
-        print(import_dependencies_paths)
-
-        # add json files content in abs path in import_dependencies_paths
-        
-        # Reading all json files in the alg(to import) dir and adding all dependencies path found in the json in the set
-        for file in json_files:
-            try:
-                file_info = json.load(open(get_abs_path(bids_root_dir, sub, file)))
-                
-                for possible_path_key in possible_paths[bids_data_to_import]:
-                    if isinstance(file_info[possible_path_key], list):
-                        for path in file_info[possible_path_key]:
-                            import_dependencies_paths.add(get_abs_path(bids_root_dir, sub, path))
-                    else:
-                        import_dependencies_paths.add(get_abs_path(bids_root_dir, sub, file_info[possible_path_key]))
-            except Exception as e:       
-                print(e.__class__.__name__)
-                logger.error("Exception occurred in reading json files: {}".format(e.__class__.__name__))
-
-        for dependency_path in import_dependencies_paths: 
-            if os.path.splitext(dependency_path)[1] != '.json':
+            file_path = get_abs_path(bids_root_dir, sub, os.path.join(sub_contents_path, file_name))
+            import_dependencies_paths.add(file_path)
+            if os.path.splitext(file_path)[1] != '.json':
                 continue
-            paths_queue.append(dependency_path)
-            json_files_processed[dependency_path] = False
-            
+            paths_queue.append(file_path)
+            json_files_processed[file_path] = False
+        
+
         # Now, reading all dependencies json files and adding dependency again if found
         while len(paths_queue)!=0:
             path = paths_queue.pop(0)
@@ -168,19 +131,20 @@ def create_bids_dataset(bids_data_to_import, bids_root_dir, bids_file_name):
                     json_data = json.load(open(get_abs_path(bids_root_dir, sub, path)))
                     
                     for possible_path_key in possible_paths["common_paths"]:
-                        if possible_path_key in json_data.keys() and isinstance(json_data[possible_path_key], list):
-                            for path1 in json_data[possible_path_key]:
-                                computed_path = get_abs_path(bids_root_dir, sub, path1)
+                        if json_data.get(possible_path_key) != None:
+                            if isinstance(json_data[possible_path_key], list):
+                                for path1 in json_data[possible_path_key]:
+                                    computed_path = get_abs_path(bids_root_dir, sub, path1)
+                                    import_dependencies_paths.add(computed_path)
+                                    paths_queue.append(computed_path)
+                                    if computed_path not in json_files_processed:
+                                        json_files_processed[computed_path] = False
+                            else:
+                                computed_path = get_abs_path(bids_root_dir, sub, json_data[possible_path_key])
                                 import_dependencies_paths.add(computed_path)
                                 paths_queue.append(computed_path)
                                 if computed_path not in json_files_processed:
                                     json_files_processed[computed_path] = False
-                        else:
-                            computed_path = get_abs_path(bids_root_dir, sub, json_data[possible_path_key])
-                            import_dependencies_paths.add(computed_path)
-                            paths_queue.append(computed_path)
-                            if computed_path not in json_files_processed:
-                                json_files_processed[computed_path] = False
                             
                 except Exception as e:
                     print(e.__class__.__name__)
@@ -189,35 +153,17 @@ def create_bids_dataset(bids_data_to_import, bids_root_dir, bids_file_name):
 
         data_files = set()
 
-        print('import depedn pathj ais')
         for p in import_dependencies_paths:
-            print(p)
-
-        for p in import_dependencies_paths:
-            print('path is {}'.format(p))
-            print(os.path.normpath(p))
-            print(os.path.abspath(p))
             path_ar = p.split(os.sep)
-            print(path_ar)
             file_dir = os.path.sep.join(path_ar[0:len(path_ar)-1])
-            print(file_dir)
-
-
-
             file_name = path_ar[len(path_ar)-1]
             files_to_copy = [fn for fn in os.listdir(file_dir) if os.path.splitext(file_name)[0] == os.path.splitext(fn)[0]] 
             for f in files_to_copy:
                 data_files.add(get_abs_path(bids_root_dir, sub, os.path.join(file_dir, f)))
 
-        print("data files are")
         for p in data_files:
-            print(p)
             import_dependencies_paths.add(p)
         
-
-        print("final import depednm pathsss")
-        for f in import_dependencies_paths:
-            print(f)
         # Creating zip archive all paths present in the set
         logger.info("Creating ZIP archive of {} files".format(len(import_dependencies_paths)))
         create_archive(import_dependencies_paths, temp_bids_zip_dir, bids_root_dir)
