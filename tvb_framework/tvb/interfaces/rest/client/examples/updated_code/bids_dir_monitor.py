cbcb967d671f34622b47aca5e70142321de94a2e
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2022-08-30 16:07:43 +0530

    Added more logs, and error handling

--- tvb_framework/tvb/interfaces/rest/client/examples/updated_code/bids_dir_monitor.py
+++ tvb_framework/tvb/interfaces/rest/client/examples/updated_code/bids_dir_monitor.py
@@ -50,6 +50,8 @@ class BIDSDirWatcher:
             while True:
                 time.sleep(self.UPLOAD_TRIGGER_INTERVAL)
                 # uploading files currently present in the queue
+                if len(self.added_files) == 0:
+                    continue
                 self.create_bids_dataset(added_files = self.added_files[:])
                 # emptying the queue after uploading them
                 self.added_files = []
@@ -60,36 +62,34 @@ class BIDSDirWatcher:
         watchdog_thread = Thread(target = self.watchdog_thread)
         upload_file_thread = Thread(target = self.uploader_thread)
         
-        print("Starting watchdog thread...")
+        logger.info("Starting watchdog thread...")
         watchdog_thread.start()
         
-        print("Starting file uploader thread...")
+        logger.info("Starting file uploader thread...")
         upload_file_thread.start()
 
         if self.IMPORT_DATA_IN_TVB:
+            logger.info("Performing TVB browser login for importing files in TVB")
             self.tvb_client = TVBClient(compute_rest_url())
             self.tvb_client.browser_login()
 
     def handle_file_added(self, event):
-        print(f"File created! {event.src_path}")
         self.added_files.append(os.path.normpath(event.src_path))
-        print("Current queue length {}".format(len(self.added_files)))
+        logger.info("New file found {}, current file queue length {}".format(event.src_path, len(self.added_files)))
 
     def create_bids_dataset(self, added_files):
-        print("Creating BIDS dataset, with {} as intial json files".format(len(added_files)))
+        logger.info("Creating BIDS dataset, with {} intial json files".format(len(added_files)))
         added_files = set(added_files)
-        print(added_files)
-        
         uploading_files = []
         for path in added_files:
             if self.change_outside_sub_dir(path):
                 continue
             uploading_files.append(path)
         
-        print("json files to upload")
-        print(uploading_files)
-        
-        if len(uploading_files) == 0: return
+
+        if len(uploading_files) == 0: 
+            logger.info("No files are added inside subject folder")
+            return
 
         subs_divided_paths = {}
 
@@ -97,9 +97,7 @@ class BIDSDirWatcher:
         
         for f in uploading_files:
             path_ar = f.split(bids_dir_name)[1].split(os.sep)
-            print(path_ar)
             for i, j in enumerate(path_ar):
-                print("{}{}".format(i,j))
                 if j.startswith(SUBJECT_PREFIX):
                     if subs_divided_paths.get(j) is None:
                         subs_divided_paths[j] = [f]
@@ -107,61 +105,56 @@ class BIDSDirWatcher:
                         subs_divided_paths[j].append(f)
                     break
         
-        print("subject wise pathss")
-        print(subs_divided_paths)
+        logger.info("Running BIDSDataBuilder on these files...")
 
-        bids_data_builder = BIDSDataBuilder(bids_root_dir = self.DIRECTORY_TO_WATCH, init_json_files = subs_divided_paths)
-        bids_zip_file = bids_data_builder.create_dataset_json_files()
-        print(bids_zip_file)
+        try:
+            bids_data_builder = BIDSDataBuilder(bids_root_dir = self.DIRECTORY_TO_WATCH, init_json_files = subs_divided_paths)
+            bids_zip_file = bids_data_builder.create_dataset_json_files()
+            logger.info("Succesfully built BIDS dataset")
+            logger.info("ZIP file location: {}".format(bids_zip_file))
+        except Exception as e:
+            logger.error("Exception occurred while creating BIDS dataset {}".format(e.__class__))
+            logger.error("Unable to create BIDS dataset for these files")
+            return
 
         if self.IMPORT_DATA_IN_TVB:
+            logger.info("Now, importing data into TVB")
             self.upload_to_tvb(bids_zip_file)
 
     def upload_to_tvb(self, file_path):
         
         projects_of_user = self.tvb_client.get_project_list()
-
+        logger.info("Found {} porjects".format(len(projects_of_user)))
         if len(projects_of_user) == 0 :
             return
         
         if self.TVB_PROJECT_ID is None:
+            logger.info("Importing data into first project as provided project id is None")
             self.TVB_PROJECT_ID = projects_of_user[0].gid
         
-        print(self.TVB_PROJECT_ID)
-        print(projects_of_user[0].name)
+        logger.info("Project Name: ".format(projects_of_user[0].name))
         model = BIDSImporterModel()
         model.uploaded = file_path
         operation_gid = self.tvb_client.launch_operation(self.TVB_PROJECT_ID, BIDSImporter, model)
         monitor_operation(self.tvb_client, operation_gid)
-        print("Get the result of import...")
+        logger.info("Get the result of import...")
         res = self.tvb_client.get_operation_results(operation_gid)
         print(res)
      
     def change_outside_sub_dir(self, file_path):
-        print(file_path)
-        print(os.path.abspath(file_path))
-        print(os.path.normpath(file_path))
         bids_dir_name = os.path.split(os.path.normpath(self.DIRECTORY_TO_WATCH))[1]
-        print(bids_dir_name)
         path_ar = file_path.split(bids_dir_name)
-        print(path_ar)
         sub_dir = path_ar[1]
         sub_name = os.path.split(sub_dir)[0]
-        print(sub_name)
-        print(os.path.split(sub_dir))
-        print(sub_dir.split(sub_name))
-        print(os.path.split(sub_dir.split(sub_name)[0]))
         try:
             if SUBJECT_PREFIX in sub_name:
                 sub_dir_ar = sub_dir.split(os.sep)
                 for i, j in enumerate(sub_dir_ar):
-                    print("{}{}".format(i,j))
                     if j.startswith(SUBJECT_PREFIX):
                         if sub_dir_ar[i+1] in ['ts', 'net', 'coord', 'spatial']:
                             return False
                         break
         except Exception as e:
-            print(e)
-            logger.err("Exception occurred in checking if added file is in sub directory")
+            logger.error("Exception: {} occurred in checking if added file is in sub directory".format(e.__class__))
         return True
     
