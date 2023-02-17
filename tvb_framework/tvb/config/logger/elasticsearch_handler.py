0b4f69fb175deb9301b7091b4803113e98bd9d43
liadomide <lia_domide@yahoo.com>
2023-02-17 23:57:12 +0200

    HBP-216 Review code

--- tvb_framework/tvb/config/logger/elasticsearch_handler.py
+++ tvb_framework/tvb/config/logger/elasticsearch_handler.py
@@ -27,74 +27,71 @@
 """
 .. moduleauthor:: Horge Rares <rares.horge@codemart.ro>
 """
-import logging
+
 from logging import Handler, LogRecord
 from logging.handlers import QueueListener, QueueHandler
 from queue import Queue
-
-from elasticsearch import Elasticsearch
-
 from tvb.basic.profile import TvbProfile
 
+if not TvbProfile.current.TRACE_USER_ACTIONS:
 
-class ElasticSendHandler(Handler):
-    def __init__(self):
-        """
-                Initializes the custom http handler
-        """
-        super().__init__()
-        self._client = Elasticsearch(
-            "https://elk-cscs.tc.humanbrainproject.eu:9200",
-            api_key='MzdnQVg0WUJFazFQZTBNTlBuZ3I6dFJuRmNLeThUQm12YmNOc0RLVFdsUQ==',
-            request_timeout=30
-        )
-        self.threshold = 5
-        self.buffer = []
+    class ElasticQueueHandler(Handler):
+        def emit(self, record: LogRecord) -> None:
+            pass
 
-    def _convert_to_bulk_format(self, record):
-        return [{"index": {}}, {"@timestamp": record.asctime, "message": record.message, "user": {"id": "user-id"}}]
+else:
 
-    def emit(self, record: LogRecord):
-        """
-        This function gets called when a log event gets emitted. It recieves a
-        record, formats it and sends it to the url
-        Parameters:
-            record: a log record
-        """
+    from elasticsearch import Elasticsearch
 
-        self.buffer += self._convert_to_bulk_format(record)
 
-        if len(self.buffer) // 2 >= self.threshold:
-            self._client.bulk(
-                index="app_tvb_logging",
-                operations=self.buffer
+    def _convert_to_bulk_format(record):
+        return [{"index": {}},
+                {"@timestamp": record.asctime, "message": record.message, "user": {"id": "user-id"}}]
+
+
+    class ElasticSendHandler(Handler):
+        def __init__(self):
+            """
+            Initializes the custom http handler
+            """
+            super().__init__()
+            self._client = Elasticsearch(
+                "https://elk-cscs.tc.humanbrainproject.eu:9200",
+                api_key='MzdnQVg0WUJFazFQZTBNTlBuZ3I6dFJuRmNLeThUQm12YmNOc0RLVFdsUQ==',
+                request_timeout=30
             )
-            self.buffer.clear()
+            self.threshold = 5
+            self.buffer = []
 
-    def close(self) -> None:
-        self._client.close()
-        self.buffer = []
-        return super().close()
+        def emit(self, record: LogRecord):
+            """
+            This function gets called when a log event gets emitted. It recieves a
+            record, formats it and sends it to the url
+            Parameters:
+                record: a log record
+            """
+            self.buffer += _convert_to_bulk_format(record)
+
+            if len(self.buffer) // 2 >= self.threshold:
+                self._client.bulk(index="app_tvb_logging", operations=self.buffer)
+                self.buffer.clear()
+
+        def close(self) -> None:
+            self._client.close()
+            self.buffer = []
+            return super().close()
 
 
-if TvbProfile.current.TRACE_USER_ACTIONS:
     class ElasticQueueHandler(QueueHandler):
+
         def __init__(self):
             # sets the queue attribute
             super().__init__(Queue(-1))
-
             self.sending_handler = ElasticSendHandler()
-            self._listener = QueueListener(self.queue,
-                                           self.sending_handler)
+            self._listener = QueueListener(self.queue, self.sending_handler)
             self._listener.start()
 
         def close(self) -> None:
             self._listener.stop()
             self.sending_handler.close()
             return super().close()
-else:  # if not TvbProfile.current.TRACE_USER_ACTIONS:
-    class ElasticQueueHandler(Handler):
-        def __init__(self):
-            super.__init__()
-        def emit(self, record: LogRecord) -> None:
-            pass
