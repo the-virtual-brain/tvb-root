0918d3594a29522c9dd181f184830efe4bb9c4d0
abhi_win <f20190041@goa.bits-pilani.ac.in>
2023-08-07 00:06:05 +0530

    revert the change

--- tvb_library/tvb/tests/library/simulator/backend/nbbackend_mpr_test.py
+++ tvb_library/tvb/tests/library/simulator/backend/nbbackend_mpr_test.py
@@ -78,8 +78,8 @@ class TestNbSim(BaseTestSim):
             )
         ).configure()
 
-        (pdq_t, pdq_d) , = NbMPRBackend().run_sim(sim, nstep=1)
-        (raw_t, raw_d) , = sim.run(simulation_length=1)
+        (pdq_t, pdq_d), = NbMPRBackend().run_sim(sim, nstep=1)
+        (raw_t, raw_d), = sim.run(simulation_length=1)
 
         np.testing.assert_allclose(raw_d[0,:], pdq_d[0,:], rtol=1e-5)
 
