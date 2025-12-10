d10ce0c9906274d40ecbb658925315bbc0884580
popaula937 <paula.popa@codemart.ro>
2025-12-10 13:13:31 +0200

    TVB-3144. Fix PSE in UI.

--- tvb_framework/tvb/core/services/backend_clients/standalone_client.py
+++ tvb_framework/tvb/core/services/backend_clients/standalone_client.py
@@ -62,7 +62,7 @@ def get_host_current_host_ip():
     :return: current host ip address
     """
     hostname = socket.gethostname()
-    ip_addr = socket.gethostbyname(hostname)
+    ip_addr = "socket.gethostbyname(hostname)"
     return ip_addr
 
 
