0386719c4c600bd930523552e4d536f3b176be7b
Catriona Osborn Moar <156433390+CatrionaOM@users.noreply.github.com>
2026-02-12 17:50:04 +0000

    Fix Jansen-Rit docstring equations
    Align y4 and y3 to implementation

--- tvb_library/tvb/simulator/models/jansen_rit.py
+++ tvb_library/tvb/simulator/models/jansen_rit.py
@@ -62,8 +62,8 @@ class JansenRit(ModelNumbaDfun):
         \dot{y_0} &= y_3 \\
         \dot{y_3} &= A a\,S[y_1 - y_2] - 2a\,y_3 - a^2\, y_0 \\
         \dot{y_1} &= y_4\\
-        \dot{y_4} &= A a \,[p(t) + \alpha_2 J + S[\alpha_1 J\,y_0]+ c_0]
-                    -2a\,y - a^2\,y_1 \\
+        \dot{y_4} &= A a \,[p(t) + \alpha_2 J\, S[\alpha_1 J\,y_0]+ c_0]
+                    -2a\,y_4 - a^2\,y_1 \\
         \dot{y_2} &= y_5 \\
         \dot{y_5} &= B b (\alpha_4 J\, S[\alpha_3 J \,y_0]) - 2 b\, y_5
                     - b^2\,y_2 \\
@@ -238,10 +238,10 @@ class JansenRit(ModelNumbaDfun):
 
         .. math::
             \dot{y_0} &= y_3 \\
-            \dot{y_3} &= A a\,S[y_1 - y_2] - 2a\,y_3 - 2a^2\, y_0 \\
+            \dot{y_3} &= A a\,S[y_1 - y_2] - 2a\,y_3 - a^2\, y_0 \\
             \dot{y_1} &= y_4\\
-            \dot{y_4} &= A a \,[p(t) + \alpha_2 J S[\alpha_1 J\,y_0]+ c_0]
-                        -2a\,y - a^2\,y_1 \\
+            \dot{y_4} &= A a \,[p(t) + \alpha_2 J\, S[\alpha_1 J\,y_0]+ c_0]
+                        -2a\,y_4 - a^2\,y_1 \\
             \dot{y_2} &= y_5 \\
             \dot{y_5} &= B b (\alpha_4 J\, S[\alpha_3 J \,y_0]) - 2 b\, y_5
                         - b^2\,y_2 \\
