e77c8e81103c1b63124175f67edce91fefb5a867
marmaduke woodman <marmaduke.woodman@univ-amu.fr>
2026-04-08 10:00:20 +0200

    fix: pickle load error

--- tvb_framework/tvb/adapters/uploaders/networkx_importer.py
+++ tvb_framework/tvb/adapters/uploaders/networkx_importer.py
@@ -27,7 +27,8 @@
 """
 .. moduleauthor:: Mihai Andrei <mihai.andrei@codemart.ro>
 """
-import pandas
+
+import pickle
 from tvb.adapters.uploaders.networkx_connectivity.parser import NetworkxParser
 from tvb.basic.neotraits.api import Attr
 from tvb.core.adapters.exceptions import ParseException, LaunchException
@@ -41,64 +42,75 @@ from tvb.core.neotraits.view_model import Str
 
 
 class NetworkxImporterModel(UploaderViewModel):
-    data_file = Str(
-        label='Please select file to import'
-    )
+    data_file = Str(label="Please select file to import")
 
     key_edge_weight = Attr(
         field_type=str,
         required=False,
         default=NetworkxParser.KEY_EDGE_WEIGHT[0],
-        label='Key Edge Weight'
+        label="Key Edge Weight",
     )
 
     key_edge_tract = Attr(
         field_type=str,
         required=False,
         default=NetworkxParser.KEY_EDGE_TRACT[0],
-        label='Key Edge Tract'
+        label="Key Edge Tract",
     )
 
     key_node_coordinates = Attr(
         field_type=str,
         required=False,
         default=NetworkxParser.KEY_NODE_COORDINATES[0],
-        label='Key Node Coordinates'
+        label="Key Node Coordinates",
     )
 
     key_node_label = Attr(
         field_type=str,
         required=False,
         default=NetworkxParser.KEY_NODE_LABEL[0],
-        label='Key Node Label'
+        label="Key Node Label",
     )
 
     key_node_region = Attr(
         field_type=str,
         required=False,
         default=NetworkxParser.KEY_NODE_REGION[0],
-        label='Key Node Region'
+        label="Key Node Region",
     )
 
     key_node_hemisphere = Attr(
         field_type=str,
         required=False,
         default=NetworkxParser.KEY_NODE_HEMISPHERE[0],
-        label='Key Node Hemisphere'
+        label="Key Node Hemisphere",
     )
 
 
 class NetworkxConnectivityImporterForm(ABCUploaderForm):
-
     def __init__(self):
         super(NetworkxConnectivityImporterForm, self).__init__()
-        self.data_file = TraitUploadField(NetworkxImporterModel.data_file, '.gpickle', 'data_file')
-        self.key_edge_weight = StrField(NetworkxImporterModel.key_edge_weight, 'key_edge_weight')
-        self.key_edge_tract = StrField(NetworkxImporterModel.key_edge_tract, name='key_edge_tract')
-        self.key_node_coordinates = StrField(NetworkxImporterModel.key_node_coordinates, name='key_node_coordinates')
-        self.key_node_label = StrField(NetworkxImporterModel.key_node_label, name='key_node_label')
-        self.key_node_region = StrField(NetworkxImporterModel.key_node_region, name='key_node_region')
-        self.key_node_hemisphere = StrField(NetworkxImporterModel.key_node_hemisphere, name='key_node_hemisphere')
+        self.data_file = TraitUploadField(
+            NetworkxImporterModel.data_file, ".gpickle", "data_file"
+        )
+        self.key_edge_weight = StrField(
+            NetworkxImporterModel.key_edge_weight, "key_edge_weight"
+        )
+        self.key_edge_tract = StrField(
+            NetworkxImporterModel.key_edge_tract, name="key_edge_tract"
+        )
+        self.key_node_coordinates = StrField(
+            NetworkxImporterModel.key_node_coordinates, name="key_node_coordinates"
+        )
+        self.key_node_label = StrField(
+            NetworkxImporterModel.key_node_label, name="key_node_label"
+        )
+        self.key_node_region = StrField(
+            NetworkxImporterModel.key_node_region, name="key_node_region"
+        )
+        self.key_node_hemisphere = StrField(
+            NetworkxImporterModel.key_node_hemisphere, name="key_node_hemisphere"
+        )
 
     @staticmethod
     def get_view_model():
@@ -106,15 +118,14 @@ class NetworkxConnectivityImporterForm(ABCUploaderForm):
 
     @staticmethod
     def get_upload_information():
-        return {
-            'data_file': '.gpickle'
-        }
+        return {"data_file": ".gpickle"}
 
 
 class NetworkxConnectivityImporter(ABCUploader):
     """
     Import connectivity data stored in the networkx gpickle format
     """
+
     _ui_name = "Connectivity Networkx gpickle"
     _ui_subsection = "networkx_importer"
     _ui_description = "Import connectivity data stored in the networkx gpickle format"
@@ -130,7 +141,8 @@ class NetworkxConnectivityImporter(ABCUploader):
         # type: (NetworkxImporterModel) -> [ConnectivityIndex]
         try:
             parser = NetworkxParser(view_model)
-            net = pandas.read_pickle(view_model.data_file)
+            with open(view_model.data_file, "rb") as f:
+                net = pickle.load(f, encoding="latin1")
             connectivity = parser.parse(net)
             return self.store_complete(connectivity)
         except ParseException as excep:
